8bc9deb33ab57b104717db9455a2443df8efdb3f
Matthias Ronge <matthias.ronge@zeutschel.de>
2012-07-04 12:01:07 +0200

    International language switch showing entries from faces-config.xml
    • SpracheForm.java: was extended by a getSupportedLocales() method which reads the values from Faces
    • tbl_Kopf.jsp: was changed to iterate over the list retrieved and display the links
    • faces-config.xml: Removed non available language entry “es”
    • default.css: was extended to format the language switch accordingly

--- src/de/sub/goobi/forms/SpracheForm.java
+++ src/de/sub/goobi/forms/SpracheForm.java
@@ -22,7 +22,12 @@
 
 package de.sub.goobi.forms;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.faces.context.FacesContext;
 
@@ -51,6 +56,61 @@ public class SpracheForm {
 		Messages.loadLanguageBundle();
 	}
 
+	/**
+	 * The function getSupportedLocales() returns a list of maps, each
+	 * representing one locale configured in the faces-config.xml file. Each of
+	 * the maps will contain the fields
+	 * 
+	 * 		id							− the locale’s ID String, e.g. “fr” or
+	 * 									  “en_GB”
+	 * 
+	 * 		displayLanguageSelf			− the name of the language in the
+	 * 									  language itself, e.g. “English”,
+	 * 									  “Deutsch”, “français”, “español”,
+	 * 									  “русский”, “日本語”, …
+	 * 
+	 * It’s a good practice to identify a language in its own spelling, since
+	 * this will be most likely what a speaker of that language will recognize.
+	 * See also: http://www.cs.tut.fi/~jkorpela/flags.html
+	 * Note that capitalisation is subject to the respective language. If the
+	 * language is unknown, the id will be returned.
+	 * 
+	 * 		displayLanguageTranslated	− the name of the language in the
+	 * 									  currently selected language, e.g., if
+	 * 									  the current language is English:
+	 * 									  “English”, “German”, “French”, …
+	 * 
+	 * This is just a nice feature because the language names are provided by
+	 * Java; and it’s used in the mouse-over titles, so you can find out what
+	 * e.g. “हिंदी” means, even if you don’t have a clue of the glyphs used. If
+	 * no translations are available, this will fall back to English.
+	 * 
+	 * 		selected					− whether this is the current language
+	 * 
+	 * This can be used for a sophisticated layout.
+	 * 
+	 * @return a list of maps, each with the fields “id”, “displayName” and
+	 *         “selected”
+	 */
+	public List<Map<String, Object>> getSupportedLocales() {
+		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+		Locale currentDisplayLanguage = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		@SuppressWarnings("unchecked") // It seems we have an old Faces API, Faces 2.1’s getSupportedLocales() returns Iterator<Locale> → TODO: Update JSF API
+		Iterator<Locale> localesIterator = FacesContext.getCurrentInstance().getApplication().getSupportedLocales();
+		while (localesIterator.hasNext()) {
+			Locale supportedLocale = localesIterator.next();
+			if (supportedLocale.getLanguage().length() > 0) {
+				Map<String, Object> translation = new HashMap<String, Object>();
+				translation.put("id", supportedLocale.toString());
+				translation.put("displayLanguageSelf", supportedLocale.getDisplayLanguage(supportedLocale));
+				translation.put("displayLanguageTranslated", supportedLocale.getDisplayLanguage(currentDisplayLanguage));
+				translation.put("selected", Boolean.valueOf(supportedLocale.equals(currentDisplayLanguage)));
+				result.add(translation);
+			}
+		}
+		return result;
+	}
+	
 	/**
 	 * The procedure switchLanguage is used to alter the application’s interface
 	 * language.
