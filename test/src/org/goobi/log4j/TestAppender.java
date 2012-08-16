151000c5e73d69f1e3995d2bdb98766d9fb802de
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2012-08-16 13:37:26 +0200

    Adding log4j test appender.

---
+++ test/src/org/goobi/log4j/TestAppender.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the Goobi Application - a Workflow tool for the support of
+ * mass digitization.
+ *
+ * Visit the websites for more information.
+ *     - http://gdz.sub.uni-goettingen.de
+ *     - http://www.goobi.org
+ *     - http://launchpad.net/goobi-production
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.goobi.log4j;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestAppender extends AppenderSkeleton{
+
+	private List<LoggingEvent> log = new ArrayList<LoggingEvent>();
+
+	@Override
+	protected void append(LoggingEvent loggingEvent) {
+		log.add(loggingEvent);
+	}
+
+	@Override
+	public boolean requiresLayout() {
+		return false;
+	}
+
+	@Override
+	public void close() {
+	}
+
+	public LoggingEvent getLastEvent() {
+		return log.get(log.size() - 1);
+	}
+
+}
