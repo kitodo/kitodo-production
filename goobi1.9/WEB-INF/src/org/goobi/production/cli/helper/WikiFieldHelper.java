b40328bc23246bdb29efd616586aa583f45ef5c4
Robert Sehr <Robert.Sehr@intranda.com>
2012-11-06 10:13:31 +0100

    added support of sending mails when adding something to process log

--- goobi1.9/WEB-INF/src/org/goobi/production/cli/helper/WikiFieldHelper.java
+++ goobi1.9/WEB-INF/src/org/goobi/production/cli/helper/WikiFieldHelper.java
@@ -75,4 +75,30 @@ public class WikiFieldHelper {
 		message = message + timestamp + ": " + value + ENDTAG;
 		return message;
 	}
+	
+	
+	public static String getWikiMessage(String currentWikifieldcontent, String type, String value) {
+		String message = "";
+		if (currentWikifieldcontent != null && currentWikifieldcontent.length() > 0) {
+			message += currentWikifieldcontent;
+			message += BREAK;
+		}
+
+		if (type.equals("error")) {
+			message += TAG_ERROR;
+		} else if (type.equals("debug")) {
+			message += TAG_DEBUG;
+		} else if (type.equals("user")) {
+			message += TAG_USER;
+		} else if (type.equals("warn")) {
+			message += TAG_WARN;
+		}else {
+			message += TAG_INFO;
+		}  
+
+		String timestamp = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM).format(new Date());
+		
+		message = message + timestamp + ": " + value + ENDTAG;
+		return message;
+	}
 }
