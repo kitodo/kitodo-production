c2d53a3c5e2ace866dc3625b7d0085d26e79bb75
Robert Sehr <Robert.Sehr@intranda.com>
2012-11-23 17:35:05 +0100

    fixes lp:1029578 language selection breaks in meta data editor
    http://bazaar.launchpad.net/~slub.team/goobi-production/1.8.0-mergebase/revision/81

--- goobi1.9/src/de/sub/goobi/forms/SpracheForm.java
+++ goobi1.9/src/de/sub/goobi/forms/SpracheForm.java
@@ -1,25 +1,30 @@
-/*
- * This file is part of the Goobi Application - a Workflow tool for the support of
- * mass digitization.
- *
- * Visit the websites for more information.
- *     - http://gdz.sub.uni-goettingen.de
- *     - http://www.goobi.org
- *     - http://launchpad.net/goobi-production
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
- * should have received a copy of the GNU General Public License along with this
- * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
+/**
+ * This file is part of the Goobi Application - a Workflow tool for the support of mass digitization.
+ * 
+ * Visit the websites for more information. 
+ *     		- http://www.goobi.org
+ *     		- http://launchpad.net/goobi-production
+ * 		    - http://gdz.sub.uni-goettingen.de
+ * 			- http://www.intranda.com
+ * 			- http://digiverso.com 
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Linking this library statically or dynamically with other modules is making a combined work based on this library. Thus, the terms and conditions
+ * of the GNU General Public License cover the whole combination. As a special exception, the copyright holders of this library give you permission to
+ * link this library with independent modules to produce an executable, regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of your choice, provided that you also meet, for each linked independent module, the terms and
+ * conditions of the license of that module. An independent module is a module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but you are not obliged to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
-
 package de.sub.goobi.forms;
 
 import java.util.ArrayList;
@@ -29,6 +34,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
 import de.sub.goobi.config.ConfigMain;
@@ -38,6 +44,8 @@ import de.sub.goobi.helper.Helper;
  * The SpracheForm class serves to switch the displayed language for the current user in the running application
  */
 public class SpracheForm {
+
+	public static final String SESSION_LOCALE_FIELD_ID = "lang";
 
 	/**
 	 * The constructor of this class loads the required MessageBundle
@@ -99,6 +107,7 @@ public class SpracheForm {
 	 * @param langCodeCombined
 	 *            This parameter can be either of form “‹language›” or of form “‹language›_‹country›”, e.g. “en” or “en_GB” are valid values.
 	 */
+	@SuppressWarnings("unchecked")
 	public void switchLanguage(String langCodeCombined) {
 		String[] languageCode = langCodeCombined.split("_");
 		Locale locale = null;
@@ -107,7 +116,9 @@ public class SpracheForm {
 		} else {
 			locale = new Locale(languageCode[0]);
 		}
-		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getViewRoot().setLocale(locale);
+		context.getExternalContext().getSessionMap().put(SESSION_LOCALE_FIELD_ID, locale);
 	}
 
 	/**
@@ -122,6 +133,18 @@ public class SpracheForm {
 	}
 
 	public Locale getLocale() {
-		return FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		FacesContext fac = FacesContext.getCurrentInstance();
+		@SuppressWarnings("rawtypes")
+		Map session = fac.getExternalContext().getSessionMap();
+		UIViewRoot frame = fac.getViewRoot();
+		if (session.containsKey(SESSION_LOCALE_FIELD_ID)) {
+			Locale locale = (Locale) session.get(SESSION_LOCALE_FIELD_ID);
+			if (frame.getLocale() != locale) {
+				frame.setLocale(locale);
+			}
+			return locale;
+		} else {
+			return frame.getLocale();
+		}
 	}
 }
