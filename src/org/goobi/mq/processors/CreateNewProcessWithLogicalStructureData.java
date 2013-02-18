bc5db31e2a53042f5b0be6042d2a0a30a88f3690
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2013-02-18 14:10:49 +0100

    Interim commit: create and fill letter elements.

--- src/org/goobi/mq/processors/CreateNewProcessWithLogicalStructureData.java
+++ src/org/goobi/mq/processors/CreateNewProcessWithLogicalStructureData.java
@@ -29,14 +29,27 @@ import de.sub.goobi.config.ConfigOpacDoctype;
 import de.sub.goobi.forms.AdditionalField;
 import de.sub.goobi.forms.ProzesskopieForm;
 import de.sub.goobi.helper.Helper;
+import de.sub.goobi.importer.ImportOpac;
 import org.apache.log4j.Logger;
 import org.goobi.mq.ActiveMQProcessor;
 import org.goobi.mq.MapMessageObjectReader;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import ugh.dl.*;
 
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.*;
 
 /**
@@ -45,11 +58,16 @@ import java.util.*;
 public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor {
 	private static final Logger logger = Logger.getLogger(CreateNewProcessWithLogicalStructureData.class);
 
+	XPath xpath = null;
+
 	/**
 	 *
 	 */
 	public CreateNewProcessWithLogicalStructureData() {
 		super(ConfigMain.getParameter("activeMQ.createNewProcessWithLogicalStructureData.queue", null));
+
+		XPathFactory xFactory = XPathFactory.newInstance();
+		xpath = xFactory.newXPath();
 	}
 
 	/**
@@ -64,10 +82,10 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 		String processIdentifier = args.getMandatoryString("id");
 		String templateName = args.getMandatoryString("template");
 		String docType = args.getMandatoryString("docType");
+		String xmlData = args.getMandatoryString("xml");
 		Set<String> collections = args.getMandatorySetOfString("collections");
-		Map<String, String> userFields = args.getMapOfStringToString("userFields");
 
-		createNewProcess(processIdentifier, templateName, docType, collections, userFields);
+		createNewProcess(processIdentifier, templateName, docType, collections, xmlData);
 	}
 
 	/**
@@ -76,10 +94,10 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 	 * @param templateName
 	 * @param docType
 	 * @param collections
-	 * @param userFields
+	 * @param xmlData
 	 * @throws Exception
 	 */
-	protected void createNewProcess(String processIdentifier, String templateName, String docType, Set<String> collections, Map<String, String> userFields) throws Exception {
+	protected void createNewProcess(String processIdentifier, String templateName, String docType, Set<String> collections, String xmlData) throws Exception {
 		try {
 			ProzesskopieForm newPFK = createNewProcessFromTemplate(templateName);
 
@@ -96,7 +114,11 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 				throw new IllegalArgumentException("Needed document type missed or invalid!");
 			}
 
-			if (userFields != null) {
+			Document xmlDocument = createXmlDocument(xmlData);
+
+			Map<String, String> userFields = getGlobalMetadata(xmlDocument);
+
+			if ((userFields != null) && (!userFields.isEmpty())) {
 				setUserFields(newPFK, userFields);
 			} else {
 				throw new IllegalArgumentException("Userfields should not be null!");
@@ -104,7 +126,7 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 
 			newPFK.CalcProzesstitel(); // use given meta data from userFields
 
-			addAdditionalLogicalStructureData(newPFK); // TODO: structure data
+			addAdditionalLogicalStructureData(newPFK, xmlDocument);
 
 			String state = newPFK.NeuenProzessAnlegen(); // create new process and store process metadata to file space storage
 			if (!state.equals("ProzessverwaltungKopie3")) {
@@ -247,30 +269,33 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 	/**
 	 *
 	 * @param pKF
+	 * @param xmlDocument
 	 * @throws Exception
 	 */
-	protected void addAdditionalLogicalStructureData(ProzesskopieForm pKF) throws Exception {
+	protected void addAdditionalLogicalStructureData(ProzesskopieForm pKF, Document xmlDocument) throws Exception {
 
 		DigitalDocument digitalDocument;
 		DocStruct insertPoint;
-		DocStruct docStructElement;
-		Metadata metadataElement;
 		Prefs prefs;
 
 		digitalDocument = getDigitalDocument(pKF);
 
-		insertPoint = getInsertPointOfLogicalDocument(digitalDocument, true);
+		insertPoint = getInsertPointOfLogicalDocument(digitalDocument);
 
 		prefs = getProjectPreferences(pKF);
 
-		docStructElement = createDocStructElement(digitalDocument, prefs, "Letter");
-
-		metadataElement = createMetadataElement(prefs, "TitleDocMain", "Hello World of TitleDocMain");
-		metadataElement.setDocStruct(docStructElement); // adding reference to document structure
-
-		// putting data togehter
-		docStructElement.addMetadata(metadataElement); // adding metadata object to document structure
-		insertPoint.addChild(docStructElement); // adding document structure to logical document structure
+		NodeList elements = extractListInformation(xmlDocument, "/convolute/folders/folder/elements/*");
+		for (int i = 0; i < elements.getLength(); i++){
+			Node currentNode = elements.item(i);
+			String nodeName = currentNode.getNodeName();
+			if (nodeName.equals("letter")) {
+				logger.debug("Found letter - continue processing...");
+				DocStruct letterElement = processLetterElement(prefs, digitalDocument, currentNode);
+				insertPoint.addChild(letterElement);
+			}   else {
+				logger.error("Unknown element named: '" + nodeName + "' Skipping...");
+			}
+		}
 	}
 
 	/**
@@ -300,11 +325,10 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 	/**
 	 *
 	 * @param digitalDocument
-	 * @param multiVolume
 	 * @return
 	 * @throws Exception
 	 */
-	private DocStruct getInsertPointOfLogicalDocument(DigitalDocument digitalDocument, boolean multiVolume) throws Exception {
+	private DocStruct getInsertPointOfLogicalDocument(DigitalDocument digitalDocument) throws Exception {
 
 		DocStruct logicalDocStruct;
 		DocStruct insertPoint;
@@ -314,23 +338,22 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 			throw new Exception("Could not retrieve logical document structure.");
 		}
 
-		if (multiVolume) {
-
-			List<DocStruct> children;
-			DocStruct firstChild;
-			children = logicalDocStruct.getAllChildren();
-			if (children == null || children.isEmpty()) {
-				throw new Exception("Expecting children, but none found.");
-			}
-			firstChild = children.get(0);
-			if (firstChild == null) {
-				throw new Exception("Expecting at leat one children of logical document structure.");
-			}
-
-			insertPoint = firstChild; // used for multi volume
-		} else {
-			insertPoint = logicalDocStruct; // used for monograph
+		List<DocStruct> children;
+		DocStruct firstChild;
+		children = logicalDocStruct.getAllChildren();
+		if (children == null || children.isEmpty()) {
+			throw new Exception("Expecting children, but none found.");
 		}
+		if (children.size() > 1) {
+			logger.warn("More than one child element found. Using first child!");
+		}
+
+		firstChild = children.get(0);
+		if (firstChild == null) {
+			throw new Exception("Expecting at leat one children of logical document structure.");
+		}
+
+		insertPoint = firstChild;
 
 		return insertPoint;
 	}
@@ -398,6 +421,227 @@ public class CreateNewProcessWithLogicalStructureData extends ActiveMQProcessor 
 		md.setValue(metadataValue);
 
 		return md;
+	}
+
+	/**
+	 *
+	 * @param xmlData
+	 * @return
+	 */
+	private Document createXmlDocument(String xmlData) {
+
+		DocumentBuilderFactory factory;
+		DocumentBuilder builder;
+		Document doc = null;
+		InputSource source;
+
+		factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		source = new InputSource(new StringReader(xmlData));
+
+		try {
+			builder = factory.newDocumentBuilder();
+			doc = builder.parse(source);
+		} catch (ParserConfigurationException e) {
+			logger.error(e.getMessage());
+		} catch (SAXException e) {
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+
+		return doc;
+	}
+
+	/**
+	 *
+	 * @param prefs
+	 * @param digitalDocument
+	 * @param currentNode
+	 * @return
+	 * @throws Exception
+	 */
+	private DocStruct processLetterElement(Prefs prefs, DigitalDocument digitalDocument, Node currentNode) throws Exception {
+		DocStruct letterElement = createDocStructElement(digitalDocument, prefs, "Letter");
+		addMetadataToStructure(prefs, letterElement, "CatalogIDDigital", extractTextInformation(currentNode, "./id"));
+		addMetadataToStructure(prefs, letterElement, "slub_comment", "Blatt: " + extractTextInformation(currentNode, "./folio"));
+		addMetadataToStructure(prefs, letterElement, "shelfmarksource", extractTextInformation(currentNode, "./signature"));
+		addMetadataToStructure(prefs, letterElement, "shelfmarksource", extractTextInformation(currentNode, "./further-signature"));
+
+		addPersonDataToStructure(prefs, letterElement, "Author", extractListInformation(currentNode, "./creator"));
+		addPersonDataToStructure(prefs, letterElement, "slub_Recipient", extractListInformation(currentNode, "./addressee"));
+
+		String place = extractTextInformation(currentNode, "./origin");
+		String date = extractTextInformation(currentNode, "./date");
+		String generatedTitle;
+		String title = extractTextInformation(currentNode, "./title");
+		String firstAuthor = extractTextInformation(currentNode, "./creator[1]/name");
+		String firstRecipient = extractTextInformation(currentNode, "./addressee[1]/name");
+
+		generatedTitle = title + " von " + firstAuthor + " an " + firstRecipient + ", " + place + ", " + date;
+
+		addMetadataToStructure(prefs, letterElement, "TitleDocMain", generatedTitle);
+		addMetadataToStructure(prefs, letterElement, "slub_comment", "Umfang: " + extractTextInformation(currentNode, "./extent"));
+		addMetadataToStructure(prefs, letterElement, "slub_comment", "Entstehungszeit: " + date);
+		addMetadataToStructure(prefs, letterElement, "slub_Place", "Entstehungsort: " + place);
+		addMetadataToStructure(prefs, letterElement, "DocLanguage", extractTextInformation(currentNode, "./language"));
+		addMetadataToStructure(prefs, letterElement, "slub_comment", "Formatangabe: " + extractTextInformation(currentNode, "./dimensions"));
+		addMetadataToStructure(prefs, letterElement, "slub_ownerOrig", extractTextInformation(currentNode, "/convolute/owner/name"));
+		addMetadataToStructure(prefs, letterElement, "slub_ownerDigi", extractTextInformation(currentNode, "/convolute/owner/name"));
+
+		return letterElement;
+	}
+
+	/**
+	 *
+	 * @param doc
+	 * @return
+	 */
+	protected Map<String, String> getGlobalMetadata(Document doc) {
+		Map<String, String> result = new HashMap<String, String>();
+
+		try {
+			// required fields
+			result.put("Schrifttyp", "keine OCR");
+			result.put("Titel Konvolut", extractTextInformation(doc, "/convolute/title"));
+			result.put("Identifier Konvolut", extractTextInformation(doc, "/convolute/id"));
+			result.put("Identifier Mappe", extractTextInformation(doc, "/convolute/folders/folder/id"));
+			result.put("Mappennummer", extractTextInformation(doc, "/convolute/folders/folder/folder"));
+
+			String portfolioTitle = extractTextInformation(doc, "/convolute/folders/folder/title");
+
+			// optional fields
+			result.put("Artist", "SLUB");
+			result.put("ATS", createAtsTls(portfolioTitle, ""));
+			result.put("Titel Konvolut (Sortierung)", "");
+			result.put("Autoren", "");
+			result.put("Titel (Mappe)", portfolioTitle);
+			result.put("Titel (Mappe) (Sortierung)", "");
+			result.put("Autoren (Mappe)", "");
+			result.put("Signatur", extractTextInformation(doc, "/convolute/folders/folder/signature"));
+		} catch (XPathExpressionException e) {
+			logger.error(e.getMessage());
+		}
+
+		return result;
+	}
+
+	/**
+	 *
+	 * @param item
+	 * @param xpathExpression
+	 * @return
+	 * @throws XPathExpressionException
+	 */
+	private String extractTextInformation(Object item, String xpathExpression) throws XPathExpressionException {
+		String result;
+
+		result = (String) extractInformation(item, xpathExpression, XPathConstants.STRING);
+
+		return result;
+	}
+
+	/**
+	 *
+	 * @param item
+	 * @param xpathExpression
+	 * @return
+	 * @throws XPathExpressionException
+	 */
+	private NodeList extractListInformation(Object item, String xpathExpression) throws XPathExpressionException {
+		NodeList nodes;
+
+		nodes = (NodeList) extractInformation(item, xpathExpression, XPathConstants.NODESET);
+
+		return nodes;
+	}
+
+	/**
+	 *
+	 * @param item
+	 * @param xpathExpression
+	 * @param returnValue
+	 * @return
+	 * @throws XPathExpressionException
+	 */
+	private Object extractInformation(Object item, String xpathExpression, QName returnValue) throws XPathExpressionException {
+		XPathExpression expr;
+		Object result;
+
+		expr = xpath.compile(xpathExpression);
+		result = expr.evaluate(item, returnValue);
+
+		return result;
+	}
+
+	/**
+	 *
+	 * @param title
+	 * @param authors
+	 * @return
+	 */
+	private String createAtsTls(String title, String authors) {
+		ImportOpac importOpac = new ImportOpac();
+		String result;
+		result = importOpac.createAtstsl(title, authors);
+		logger.debug("generated ats tls: '" + result + "'");
+		return result;
+	}
+
+	/**
+	 *
+	 * @param prefs
+	 * @param docStructElement
+	 * @param metadataName
+	 * @param metadataValue
+	 * @throws Exception
+	 */
+	private void addMetadataToStructure(Prefs prefs, DocStruct docStructElement, String metadataName, String metadataValue) throws Exception {
+		Metadata metadataElement;
+
+		metadataElement = createMetadataElement(prefs, metadataName, metadataValue);
+		metadataElement.setDocStruct(docStructElement); // adding reference to document structure
+
+		// putting data together
+		docStructElement.addMetadata(metadataElement); // adding metadata object to document structure
+	}
+
+	private void addPersonDataToStructure(Prefs prefs, DocStruct docStructElement, String personRole, NodeList personList) throws Exception {
+		Person person;
+		MetadataType mdt;
+
+		mdt = prefs.getMetadataTypeByName(personRole);
+		if (mdt == null) {
+			throw new Exception("Could not retrieve metadata type for '" + personRole + "'.");
+		}
+		logger.debug("Create " + personList.getLength() + " new persons for role " + personRole + ".");
+		for (int i = 0; i < personList.getLength(); i++) {
+			Node currentNode = personList.item(i);
+			person = new Person(mdt);
+	        String firstName = "";
+			String lastName = "";
+	        String personName = extractTextInformation(currentNode, "./name");
+
+			if (personName.contains("<") || personName.contains(">")) {
+				lastName = personName;
+			} else {
+				String nameParts[] = personName.split(",", 2);
+				if (nameParts.length == 2) {
+					firstName = nameParts[1];
+					lastName = nameParts[0];
+				} else {
+					lastName = personName;
+				}
+			}
+			logger.debug("first name: " + firstName + " lastname: " + lastName + " gnd: " + extractTextInformation(currentNode, "./gnd-id"));
+			person.setFirstname(firstName.trim());
+			person.setLastname(lastName.trim());
+			person.setIdentifier(extractTextInformation(currentNode, "./gnd-id"));
+			person.setIdentifierType("GND");
+			person.setRole(personRole);
+
+			docStructElement.addPerson(person);
+		}
 	}
 
 }
