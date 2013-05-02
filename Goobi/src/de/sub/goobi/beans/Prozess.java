13b87d622976f2d1c09ad81567d824215935b9c6
Robert Sehr <Robert.Sehr@intranda.com>
2013-05-02 16:26:33 +0200

    fixed problems with empty master folder

--- Goobi/src/de/sub/goobi/beans/Prozess.java
+++ Goobi/src/de/sub/goobi/beans/Prozess.java
@@ -394,7 +394,7 @@ public class Prozess implements Serializable {
 				origOrdner = DIRECTORY_PREFIX + "_" + this.titel + "_" + DIRECTORY_SUFFIX;
 			}
 			String rueckgabe = getImagesDirectory() + origOrdner + File.separator;
-			if (ConfigMain.getBooleanParameter("createOrigFolderIfNotExists", false)) {
+			if (ConfigMain.getBooleanParameter("createOrigFolderIfNotExists", false) && this.getSortHelperStatus() != "100000000") {
 				FilesystemHelper.createDirectory(rueckgabe);
 			}
 			return rueckgabe;
