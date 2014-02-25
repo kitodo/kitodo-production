526f5c509ae821c36369a36c0e33804e66148e87
Matthias Ronge <matthias.ronge@zeutschel.de>
2014-02-25 18:13:35 +0100

    First part of upper half of calendar editor (not yet working)

--- Goobi/src/de/sub/goobi/forms/CalendarForm.java
+++ Goobi/src/de/sub/goobi/forms/CalendarForm.java
@@ -38,6 +38,17 @@
  */
 package de.sub.goobi.forms;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.goobi.production.model.bibliography.course.Course;
+import org.goobi.production.model.bibliography.course.Title;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 /**
  * The class CalendarForm provides for a calendar editor to enter the course of
  * apparance of a newspaper.
@@ -47,9 +58,220 @@ package de.sub.goobi.forms;
 public class CalendarForm {
 
 	/**
-	 * Empty constructor.
+	 * The dateConverter is used to convert between LocalDate objects and String
+	 */
+	protected static final DateTimeFormatter dateConverter = DateTimeFormat.forPattern("dd.MM.yyyy");
+
+	/**
+	 * The field course holds the course of appearance currently under edit by
+	 * this calendar form instance.
+	 */
+	protected Course course;
+
+	/**
+	 * The field titleShowing holds the Title block currently showing in this
+	 * calendar instance.
+	 */
+	protected Title titleShowing;
+
+	/**
+	 * The Map titlePickerResolver is populated with the IDs used in the title
+	 * picker list box and the references to the Title objects referenced by the
+	 * IDs for easily looking them up on change.
+	 */
+	protected Map<String, Title> titlePickerResolver;
+
+	/**
+	 * Empty constructor. Creates a new form without yet any data.
 	 */
 	public CalendarForm() {
+		course = new Course();
+		titleShowing = null;
+		titlePickerResolver = new HashMap<String, Title>();
+	}
+
+	/**
+	 * The function getTitlePickerOptions() returns the elements for the title
+	 * picker list box as read only property "titlePickerOptions". It returns a
+	 * List of Map with each two entries: "value" and "label". "value" is the
+	 * hashCode() in hex of the Title which will later be used if the field
+	 * "titlePickerSelected" is altered to choose the currently selected title
+	 * block, "label" is the readable description to be shown to the user.
+	 * 
+	 * @return the elements for the title picker list box
+	 */
+	public List<Map<String, String>> getTitlePickerOptions() {
+		List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+		for (Title title : course) {
+			String value = Integer.toHexString(title.hashCode());
+			titlePickerResolver.put(value, title);
+			Map<String, String> item = new HashMap<String, String>();
+			item.put("value", value);
+			item.put("label", title.toString());
+			result.add(item);
+		}
+		return result;
+	}
+
+	/**
+	 * The function getTitlePickerSelected() returns the hashCode() value of the
+	 * title block currently selected as read-write property
+	 * "titlePickerSelected". If a new block is under edit, returns the empty
+	 * String.
+	 * 
+	 * @return identifier of the selected title
+	 */
+	public String getTitlePickerSelected() {
+		return titleShowing == null ? "" : Integer.toHexString(titleShowing.hashCode());
+	}
+
+	/**
+	 * The method setTitlePickerSelected() will be called by Faces to store a
+	 * new value of the read-write property "titlePickerSelected", which means
+	 * that the user selected a different Title block in the title picker list
+	 * box. The event will be used to alter the “titleShowing” field which keeps
+	 * the Title block currently showing.
+	 * 
+	 * @param value
+	 *            hashCode() in hex of the Title to be selected
+	 */
+	public void setTitlePickerSelected(String value) {
+		if ("".equals(value)) {
+			titleShowing = null;
+			return;
+		}
+		titleShowing = titlePickerResolver.get(value);
+	}
+
+	/**
+	 * The method addTitleClick() resets the field titleShowing which will
+	 * result in any modifications to the title data coming in later will be
+	 * stored in a new title block.
+	 */
+	public void addTitleClick() {
+		titleShowing = null;
+	}
+
+	/**
+	 * The method removeTitleClick() deletes the currently selected Title block
+	 * from the course of appearance.The method is not intended to be used if
+	 * there is only one block left.
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 *             if the title referenced by “titleShowing” isn’t contained in
+	 *             the course of appearance
+	 */
+	public void removeTitleClick() {
+		assert course.size() > 1;
+		int index = course.indexOf(titleShowing);
+		course.remove(index);
+		if (index > 0)
+			index--;
+		titleShowing = course.get(index);
+	}
+
+	/**
+	 * The function getTitleHeading() returns the heading of the Title block
+	 * currently showing as read-write property "titleHeading".
+	 * 
+	 * @return heading of currently showing title
+	 */
+	public String getTitleHeading() {
+		if (titleShowing == null)
+			return "";
+		return titleShowing.getHeading();
+	}
+
+	/**
+	 * The method setTitleHeading() will be called by Faces to store a new value
+	 * of the read-write property "titleHeading", which represents the heading
+	 * of the Title block currently showing. The event will be used to either
+	 * alter the heading of the Title block defined by the “titleShowing” field
+	 * or, in case that a new title block is under edit, to initially set its
+	 * title.
+	 * 
+	 * @param heading
+	 *            new heading for the title block
+	 */
+	public void setTitleHeading(String heading) {
+		if (titleShowing != null)
+			titleShowing.setHeading(heading);
+		else {
+			titleShowing = new Title(heading);
+			course.add(titleShowing);
+		}
+	}
+
+	/**
+	 * The function getFirstAppearance() returns the date of first appearance of
+	 * the Title block currently showing as read-write property
+	 * "firstAppearance".
+	 * 
+	 * @return date of first appearance of currently showing title
+	 */
+	public String getFirstAppearance() {
+		if (titleShowing != null)
+			return dateConverter.print(titleShowing.getFirstAppearance());
+		else
+			return "";
+	}
+
+	/**
+	 * The method setTitleHeading() will be called by Faces to store a new value
+	 * of the read-write property "firstAppearance", which represents the the
+	 * date of first appearance of the Title block currently showing. The event
+	 * will be used to either alter the date of first appearance of the Title
+	 * block defined by the “titleShowing” field or, in case that a new title
+	 * block is under edit, to initially set its the date of first appearance.
+	 * 
+	 * @param firstAppearance
+	 *            new date of first appearance
+	 */
+	public void setFirstAppearance(String firstAppearance) {
+		LocalDate newFirstAppearance = dateConverter.parseLocalDate(firstAppearance);
+		if (titleShowing != null)
+			titleShowing.setFirstAppearance(newFirstAppearance);
+		else {
+			titleShowing = new Title();
+			titleShowing.setFirstAppearance(newFirstAppearance);
+			course.add(titleShowing);
+		}
+	}
+
+	/**
+	 * The function getLastAppearance() returns the date of last appearance of
+	 * the Title block currently showing as read-write property
+	 * "lastAppearance".
+	 * 
+	 * @return date of last appearance of currently showing title
+	 */
+	public String getLastAppearance() {
+		if (titleShowing != null)
+			return dateConverter.print(titleShowing.getLastAppearance());
+		else
+			return "";
+	}
+
+	/**
+	 * The method setTitleHeading() will be called by Faces to store a new value
+	 * of the read-write property "lastAppearance", which represents the the
+	 * date of last appearance of the Title block currently showing. The event
+	 * will be used to either alter the date of last appearance of the Title
+	 * block defined by the “titleShowing” field or, in case that a new title
+	 * block is under edit, to initially set its the date of last appearance.
+	 * 
+	 * @param lastAppearance
+	 *            new date of last appearance
+	 */
+	public void setLastAppearance(String lastAppearance) {
+		LocalDate newLastAppearance = dateConverter.parseLocalDate(lastAppearance);
+		if (titleShowing != null)
+			titleShowing.setLastAppearance(newLastAppearance);
+		else {
+			titleShowing = new Title();
+			titleShowing.setLastAppearance(newLastAppearance);
+			course.add(titleShowing);
+		}
 	}
 
 }
