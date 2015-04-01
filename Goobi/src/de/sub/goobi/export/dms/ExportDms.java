79cfcc9e190f1f5713e6dbfba8390eb39f38304d
Frank Ulrich Weber <fuw@zeutschel.de>
2015-04-01 15:06:09 +0200

    Issue #331: Export fulltext directories only if they contain an underscore.

--- Goobi/src/de/sub/goobi/export/dms/ExportDms.java
+++ Goobi/src/de/sub/goobi/export/dms/ExportDms.java
@@ -506,7 +506,7 @@ public class ExportDms extends ExportMets {
 		if (ocr.exists()) {
 			File[] folder = ocr.listFiles();
 			for (File dir : folder) {
-				if (dir.isDirectory() && dir.list().length > 0) {
+				if (dir.isDirectory() && dir.list().length > 0 && dir.getName().contains("_")) {
 					String suffix = dir.getName().substring(dir.getName().lastIndexOf("_"));
 					File destination = new File(benutzerHome + File.separator + atsPpnBand + suffix);
 					if (!destination.exists()) {
