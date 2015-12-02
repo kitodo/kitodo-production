a36b6b5d6545e4506c3e0e65a460e78a0c54d819
Matthias Ronge <matthias.ronge@zeutschel.de>
2015-12-02 08:54:46 +0100

    Fix PaginatorTest

--- Goobi/test/src/de/sub/goobi/metadaten/PaginatorTest.java
+++ Goobi/test/src/de/sub/goobi/metadaten/PaginatorTest.java
@@ -188,7 +188,8 @@ public class PaginatorTest {
 				.setPaginationMode(Paginator.Mode.RECTOVERSO_FOLIATION)
 				.setPagesToPaginate(new Metadatum[]{
 						new MockMetadatum()
-				});
+				})
+				.setPaginationSeparator(" ");
 		paginator.run();
 		assertPagenumberSequence(paginator, new String[]{"1v 2r"});
 	}
@@ -314,7 +315,8 @@ public class PaginatorTest {
 						new MockMetadatum(),
 						new MockMetadatum(),
 						new MockMetadatum()
-				});
+				})
+				.setPaginationSeparator(" ");
 		paginator.run();
 		assertPagenumberSequence(paginator, new String[]{"1v 2r", "2v 3r", "3v 4r"});
 	}
@@ -332,7 +334,8 @@ public class PaginatorTest {
 						new MockMetadatum(),
 						new MockMetadatum(),
 						new MockMetadatum()
-				});
+				})
+				.setPaginationSeparator(" ");
 		paginator.run();
 		assertPagenumberSequence(paginator, new String[]{"[1]v [2]r", "[2]v [3]r", "[3]v [4]r"});
 	}
@@ -350,7 +353,8 @@ public class PaginatorTest {
 						new MockMetadatum(),
 						new MockMetadatum(),
 						new MockMetadatum()
-				});
+				})
+				.setPaginationSeparator(" ");
 		paginator.run();
 		assertPagenumberSequence(paginator, new String[]{"[XX]v [XXI]r", "[XXI]v [XXII]r", "[XXII]v [XXIII]r"});
 	}
