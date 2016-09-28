8c028d5aff9aa50d8dad349b460d3f7df50f494e
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2016-09-28 15:14:17 +0200

    Update license header of Kitodo java files

--- Goobi/test/src/org/goobi/pagination/IntegerSequenceTest.java
+++ Goobi/test/src/org/goobi/pagination/IntegerSequenceTest.java
@@ -1,28 +1,14 @@
-///*
-// * This file is part of the Goobi Application - a Workflow tool for the support of
-// * mass digitization.
-// *
-// * Visit the websites for more information.
-// *    - http://gdz.sub.uni-goettingen.de
-// *    - http://www.kitodo.org
-// *    - https://github.com/goobi/goobi-production
-// *
-// * Copyright 2011, Center for Retrospective Digitization, Göttingen (GDZ),
-// *
-// * This program is free software; you can redistribute it and/or modify it under
-// * the terms of the GNU General Public License as published by the Free Software
-// * Foundation; either version 2 of the License, or (at your option) any later
-// * version.
-// *
-// * This program is distributed in the hope that it will be useful, but WITHOUT ANY
-// * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// * PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// *
-// * You should have received a copy of the GNU General Public License
-// * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// */
-//
-//// 
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
 //package org.goobi.pagination;
 //
 //
