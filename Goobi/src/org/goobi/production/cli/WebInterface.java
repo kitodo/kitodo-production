aaba76862af92fbf52e21e7597e60a5df779cc4c
Stefan Weil <sw@weilnetz.de>
2016-10-14 07:13:02 +0200

    WebInterface: Escape HTML code (XSS)
    Coverity report:
    CID 44688 (#1 of 1): Cross-site scripting (XSS)
    CID 135806 (#1 of 1): Cross-site scripting (XSS)
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- Goobi/src/org/goobi/production/cli/WebInterface.java
+++ Goobi/src/org/goobi/production/cli/WebInterface.java
@@ -40,6 +40,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.log4j.Logger;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.PluginLoader;
@@ -103,7 +104,9 @@ public class WebInterface extends HttpServlet {
 			List<String> allowedCommands = WebInterfaceConfig.getCredentials(ip, password);
 			if (!allowedCommands.contains(command)) {
 				// error, no command found
-				generateAnswer(resp, 401, "command not allowed", "command " + command + " not allowed for your IP (" + ip + ")");
+				generateAnswer(resp, 401, "command not allowed",
+					"command " + StringEscapeUtils.escapeHtml(command) +
+					" not allowed for your IP (" + StringEscapeUtils.escapeHtml(ip) + ")");
 				return;
 			}
 
