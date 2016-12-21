72447c9f7f62cf0877bd0cae0ae5033c4fa2031e
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2016-12-21 17:00:25 +0100

    Clean obsolete data - migration script

--- Kitodo/src/main/resources/db/migration/V2_2__Remove_dangling_data.sql
+++ Kitodo/src/main/resources/db/migration/V2_2__Remove_dangling_data.sql
@@ -1,5 +1,15 @@
 --
 -- Migration: Remove dangling data
 
--- 1. Check if data in many to many tables is missing in referenced tables
+-- 1. Check if process exists, if no, remove references to it in other tables
 --
+DELETE bp FROM batch_x_process AS bp LEFT JOIN process AS p ON p.id = bp.process_id WHERE p.id IS NULL;
+DELETE h FROM history AS h LEFT JOIN process AS p ON p.id = h.process_id WHERE p.id IS NULL;
+DELETE t FROM task AS t LEFT JOIN process AS p ON p.id = t.process_id WHERE p.id IS NULL;
+DELETE t FROM template AS t LEFT JOIN process AS p ON p.id = t.process_id WHERE p.id IS NULL;
+DELETE w FROM workpiece AS w LEFT JOIN process AS p ON p.id = w.process_id WHERE p.id IS NULL;
+
+-- 2. Check if user exists, if no, change user_id in task table to NULL
+--
+UPDATE task SET user_id = NULL
+WHERE (user_id) NOT IN (SELECT id FROM user);
