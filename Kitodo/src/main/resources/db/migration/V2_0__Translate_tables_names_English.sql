845bd41d1e7348bc18cd3c9322e7e42ea2536c67
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-01-06 10:46:48 +0100

    Add license headers

--- Kitodo/src/main/resources/db/migration/V2_0__Translate_tables_names_English.sql
+++ Kitodo/src/main/resources/db/migration/V2_0__Translate_tables_names_English.sql
@@ -1,4 +1,15 @@
 --
+-- (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+--
+-- This file is part of the Kitodo project.
+--
+-- It is licensed under GNU General Public License version 3 or later.
+--
+-- For the full copyright and license information, please read the
+-- GPL3-License.txt file that was distributed with this source code.
+--
+
+--
 -- Migration: Renaming tables to English
 --
 -- 1. Rename tables
@@ -30,4 +41,4 @@ RENAME TABLE
 --
 -- 2. Check if table exists, if yes, remove it
 --
-DROP TABLE IF EXISTS schritteeigenschaften
+DROP TABLE IF EXISTS schritteeigenschaften;
--- Kitodo/src/main/resources/db/migration/V2_1__Translate_columns_names_to_English.sql
+++ Kitodo/src/main/resources/db/migration/V2_1__Translate_columns_names_to_English.sql
@@ -1,4 +1,15 @@
 --
+-- (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+--
+-- This file is part of the Kitodo project.
+--
+-- It is licensed under GNU General Public License version 3 or later.
+--
+-- For the full copyright and license information, please read the
+-- GPL3-License.txt file that was distributed with this source code.
+--
+
+--
 -- Migration: Renaming columns to English
 --
 -- 1. Rename columns in tables
