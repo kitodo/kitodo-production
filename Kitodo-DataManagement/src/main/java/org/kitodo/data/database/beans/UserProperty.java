4d203b8bb20c65d41e310bcf6afd553a7a149cd5
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-01-20 12:52:19 +0100

    UserProperty refactored, inserted UserPropertyDAO and UserPropertyService

---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/beans/UserProperty.java
@@ -0,0 +1,215 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.database.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.kitodo.data.database.beans.property.GoobiPropertyInterface;
+import org.kitodo.data.database.helper.enums.PropertyType;
+
+@Entity
+@Table(name = "userProperty")
+public class UserProperty implements Serializable, GoobiPropertyInterface {
+	private static final long serialVersionUID = -2356566712752716107L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue
+	private Integer id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "value")
+	private String value;
+
+	@Column(name = "isObligatory")
+	private Boolean isObligatory;
+
+	@Column(name = "dataType")
+	private Integer dataType;
+
+	@Column(name = "choice")
+	private String choice;
+
+	@Column(name = "creationDate")
+	private Date creationDate;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Transient
+	private List<String> valueList;
+
+	/**
+	 * Constructor.
+	 */
+	public UserProperty() {
+		this.isObligatory = false;
+		this.dataType = PropertyType.String.getId();
+		this.creationDate = new Date();
+	}
+
+	@Override
+	public Integer getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getTitle() {
+		return this.title;
+	}
+
+	@Override
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String getChoice() {
+		return this.choice;
+	}
+
+	@Override
+	public void setChoice(String choice) {
+		this.choice = choice;
+	}
+
+	@Override
+	public Boolean isObligatory() {
+		if (this.isObligatory == null) {
+			this.isObligatory = false;
+		}
+		return this.isObligatory;
+	}
+
+	@Override
+	public void setIsObligatory(Boolean isObligatory) {
+		this.isObligatory = isObligatory;
+	}
+
+	@Override
+	public Date getCreationDate() {
+		return this.creationDate;
+	}
+
+	@Override
+	public void setCreationDate(Date creation) {
+		this.creationDate = creation;
+	}
+
+	/**
+	 * Getter for dataType set to private for hibernate, for use in program use getType instead - why?!.
+	 * 
+	 * @return dataType as integer
+	 */
+	@SuppressWarnings("unused")
+	private Integer getDataType() {
+		return this.dataType;
+	}
+
+	/**
+	 * Set dataType to defined integer only for internal use through hibernate, for changing dataType
+	 * use setType instead.
+	 * 
+	 * @param dataType as Integer
+	 */
+	@SuppressWarnings("unused")
+	private void setDataType(Integer dataType) {
+		this.dataType = dataType;
+	}
+
+	/**
+	 * Get dataType as {@link PropertyType}.
+	 *
+	 * @return current dataType
+	 */
+	@Override
+	public PropertyType getType() {
+		if (this.dataType == null) {
+			this.dataType = PropertyType.String.getId();
+		}
+		return PropertyType.getById(this.dataType);
+	}
+
+	/**
+	 * Set dataType to specific value from {@link PropertyType}.
+	 *
+	 * @param inputType as {@link PropertyType}
+	 */
+	@Override
+	public void setType(PropertyType inputType) {
+		this.dataType = inputType.getId();
+	}
+
+	@Override
+	public Integer getContainer() {
+		return 0;
+	}
+
+	@Override
+	public void setContainer(Integer container) {
+
+	}
+
+	/**
+	 * How it is possible that here appears list if object can have only one value?!.
+	 *
+	 * @return list of values
+	 */
+	public List<String> getValueList() {
+		if (this.valueList == null) {
+			this.valueList = new ArrayList<>();
+		}
+		return this.valueList;
+	}
+
+	public void setValueList(List<String> valueList) {
+		this.valueList = valueList;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/persistence/UserPropertyDAO.java
@@ -0,0 +1,67 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.database.persistence;
+
+import java.util.List;
+
+import org.kitodo.data.database.beans.UserProperty;
+import org.kitodo.data.database.exceptions.DAOException;
+
+public class UserPropertyDAO extends BaseDAO {
+
+	private static final long serialVersionUID = 834210840673022351L;
+
+	/**
+	 * Find user property object by id.
+	 *
+	 * @param id of searched object
+	 * @return result
+	 * @throws DAOException an exception that can be thrown from the underlying find() procedure failure.
+	 */
+	public UserProperty find(Integer id) throws DAOException {
+		UserProperty result = (UserProperty) retrieveObject(UserProperty.class, id);
+		if (result == null) {
+			throw new DAOException("Object can not be found in database");
+		}
+		return result;
+	}
+
+	/**
+	 * The function findAll() retrieves all users from the database.
+	 *
+	 * @return all persisted users' properties
+	 */
+	@SuppressWarnings("unchecked")
+	public List<UserProperty> findAll() {
+		return retrieveAllObjects(UserProperty.class);
+	}
+
+	public UserProperty save(UserProperty userProperty) throws DAOException {
+		storeObject(userProperty);
+		return (UserProperty) retrieveObject(UserProperty.class, userProperty.getId());
+	}
+
+	/**
+	 * The function remove() removes a user property
+	 * 
+	 * @param userProperty to be removed
+	 * @throws DAOException an exception that can be thrown from the underlying save() procedure upon database
+	 * 				failure.
+	 */
+	public void remove(UserProperty userProperty) throws DAOException {
+		removeObject(userProperty);
+	}
+
+	public Long count(String query) throws DAOException {
+		return retrieveAmount(query);
+	}
+}
