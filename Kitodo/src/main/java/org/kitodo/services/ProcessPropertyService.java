ccc0ca91c91657f5b72d189940f54843951d9754
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-14 10:00:05 +0100

    Fix ProcessPropertyService

--- Kitodo/src/main/java/org/kitodo/services/ProcessPropertyService.java
+++ Kitodo/src/main/java/org/kitodo/services/ProcessPropertyService.java
@@ -12,10 +12,33 @@
 package org.kitodo.services;
 
 import org.kitodo.data.database.beans.ProcessProperty;
+import org.kitodo.data.database.exceptions.DAOException;
+import org.kitodo.data.database.persistence.ProcessPropertyDAO;
+
+import java.util.List;
 
 public class ProcessPropertyService {
 
+    private ProcessPropertyDAO processPropertyDao = new ProcessPropertyDAO();
+
+    public void save(ProcessProperty processProperty) throws DAOException {
+        processPropertyDao.save(processProperty);
+    }
+
+    public ProcessProperty find(Integer id) throws DAOException {
+        return processPropertyDao.find(id);
+    }
+
+    public List<ProcessProperty> findAll() throws DAOException {
+        return processPropertyDao.findAll();
+    }
+
+    public void remove(ProcessProperty processProperty) throws DAOException {
+        processPropertyDao.remove(processProperty);
+    }
+
     public String getNormalizedTitle(ProcessProperty processProperty) {
+        //why trim after replace spaces to _, it should have reversed order
         return processProperty.getTitle().replace(" ", "_").trim();
     }
 
