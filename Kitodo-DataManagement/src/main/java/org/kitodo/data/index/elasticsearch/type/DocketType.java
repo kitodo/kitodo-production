58cc3bcb2305aeb02882a04ca8cee835e2c5d5e7
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-15 16:26:50 +0100

    Add DocketType class

---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/index/elasticsearch/type/DocketType.java
@@ -0,0 +1,48 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.index.elasticsearch.type;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+
+import org.json.simple.JSONObject;
+
+import org.kitodo.data.database.beans.Docket;
+import org.kitodo.data.database.exceptions.DAOException;
+
+/**
+ * Implementation of Docket Type.
+ */
+public class DocketType /*extends BaseType*/ {
+
+    @SuppressWarnings("unchecked")
+    public HttpEntity createDocument(Docket docket) {
+
+        JSONObject docketObject = new JSONObject();
+        docketObject.put("name", docket.getName());
+        docketObject.put("file", docket.getFile());
+
+        return new NStringEntity(docketObject.toJSONString(), ContentType.APPLICATION_JSON);
+    }
+
+    public HashMap<Integer, HttpEntity> createDocuments(List<Docket> dockets) {
+        HashMap<Integer, HttpEntity> documents = new HashMap<>();
+        for (Docket docket : dockets) {
+            documents.put(docket.getId(), createDocument(docket));
+        }
+        return documents;
+    }
+}
