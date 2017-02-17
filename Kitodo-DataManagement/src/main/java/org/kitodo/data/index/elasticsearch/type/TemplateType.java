5b15ab572ae531eb8d66fadd807a5d92fe44725b
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-17 15:41:48 +0100

    Add TemplateType class

---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/index/elasticsearch/type/TemplateType.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.index.elasticsearch.type;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.kitodo.data.database.beans.Template;
+import org.kitodo.data.database.beans.TemplateProperty;
+
+/**
+ * Implementation of Template Type.
+ */
+public class TemplateType /*extends BaseType*/ {
+
+    @SuppressWarnings("unchecked")
+    public HttpEntity createDocument(Template template) {
+
+        LinkedHashMap<String, String> orderedTemplateMap = new LinkedHashMap<>();
+        String process = template.getProcess() != null ? template.getProcess().getId().toString() : "null";
+        orderedTemplateMap.put("process", process);
+
+        JSONObject processObject = new JSONObject(orderedTemplateMap);
+
+        JSONArray properties = new JSONArray();
+        List<TemplateProperty> templateProperties = template.getProperties();
+        for (TemplateProperty property : templateProperties) {
+            JSONObject propertyObject = new JSONObject();
+            propertyObject.put("title", property.getTitle());
+            propertyObject.put("value", property.getValue());
+            properties.add(propertyObject);
+        }
+        processObject.put("properties", properties);
+
+        return new NStringEntity(processObject.toJSONString(), ContentType.APPLICATION_JSON);
+    }
+
+    public HashMap<Integer, HttpEntity> createDocuments(List<Template> templates) {
+        HashMap<Integer, HttpEntity> documents = new HashMap<>();
+        for (Template template : templates) {
+            documents.put(template.getId(), createDocument(template));
+        }
+        return documents;
+    }
+}
