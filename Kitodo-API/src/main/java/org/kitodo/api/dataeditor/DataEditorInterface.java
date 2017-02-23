62390a1bc40efc2ef5ff053d412d7b4f83fc9f64
Huber <kathrin.huber@slub-dresden.de>
2017-02-23 13:02:30 +0100

    Add DataEditorInterface

--- Kitodo-API/src/main/java/org/kitodo/api/dataeditor/DataEditorInterface.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataeditor/DataEditorInterface.java
@@ -11,5 +11,22 @@
 
 package org.kitodo.api.dataeditor;
 
+import java.nio.file.Path;
+
 public interface DataEditorInterface {
+
+    /**
+     *
+     * @param xmlFilePath The Path to the xml file to read.
+     */
+    public void readData(Path xmlFilePath);
+
+    /**
+     *
+     * @param xmlFilePath The Path to the xml file to edit.
+     * @param rulesetFilePath The Path to the rulesetFilePath.
+     * @return true, if editing was succesfull, false otherwise.
+     */
+    public boolean editData(Path xmlFilePath, Path rulesetFilePath);
+
 }
