9df5d8d6118591eb8b1653da5a8587d365d8c572
Huber <kathrin.huber@slub-dresden.de>
2017-02-23 12:34:33 +0100

    Add SchemaConverterInterface

--- Kitodo-API/src/main/java/org/kitodo/api/schemaconverter/SchemaConverterInterface.java
+++ Kitodo-API/src/main/java/org/kitodo/api/schemaconverter/SchemaConverterInterface.java
@@ -11,6 +11,21 @@
 
 package org.kitodo.api.schemaconverter;
 
+import java.io.File;
+
 public interface SchemaConverterInterface {
+
+	/**
+	 * Converts a given file with given Format to a result format.
+	 * 
+	 * @param inputFile
+	 *            The file to convert.
+	 * @param baseFormat
+	 *            The Format of the given File.
+	 * @param resultFormat
+	 *            The Format of the resultFile.
+	 * @return The converted File.
+	 */
+	public File convert(File inputFile, SchemaFormat baseFormat, SchemaFormat resultFormat);
 
 }
