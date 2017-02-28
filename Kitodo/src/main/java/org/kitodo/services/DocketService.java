d54f759094355e8344ba7d3a0dc54785aac3f97d
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-28 13:56:43 +0100

    Integrate Indexer with DocketService

--- Kitodo/src/main/java/org/kitodo/services/DocketService.java
+++ Kitodo/src/main/java/org/kitodo/services/DocketService.java
@@ -11,14 +11,21 @@
 
 package org.kitodo.services;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+
+import java.io.IOException;
 import java.util.List;
 
 import org.kitodo.data.database.beans.Docket;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.DocketDAO;
+import org.kitodo.data.index.Indexer;
+import org.kitodo.data.index.elasticsearch.type.DocketType;
 
 public class DocketService {
     private DocketDAO docketDao = new DocketDAO();
+    private DocketType docketType = new DocketType();
+    private Indexer<Docket, DocketType> indexer = new Indexer<>("kitodo", Docket.class);
 
     public Docket find(Integer id) throws DAOException {
         return docketDao.find(id);
@@ -28,12 +35,26 @@ public class DocketService {
         return docketDao.findAll();
     }
 
-    public Docket save(Docket userGroup) throws DAOException {
-        return docketDao.save(userGroup);
+    /**
+     * Method saves object to database and insert document to the index of Elastic Search.
+     *
+     * @param docket object
+     */
+    public void save(Docket docket) throws DAOException, IOException {
+        docketDao.save(docket);
+        indexer.setMethod(HTTPMethods.PUT);
+        indexer.performSingleRequest(docket, docketType);
     }
 
-    public void remove(Docket userGroup) throws DAOException {
-        docketDao.remove(userGroup);
+    /**
+     * Method removes object from database and document from the index of Elastic Search.
+     *
+     * @param docket object
+     */
+    public void remove(Docket docket) throws DAOException, IOException {
+        docketDao.remove(docket);
+        indexer.setMethod(HTTPMethods.DELETE);
+        indexer.performSingleRequest(docket, docketType);
     }
 
     public List<Docket> search(String query) throws DAOException {
