686004c78de8aec4b7fd757b44f82081cf1a8fd5
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-28 16:09:32 +0100

    Add throws/catch for IOException for integration ProjectService

--- Kitodo/src/main/java/org/goobi/production/importer/ProductionDataImport.java
+++ Kitodo/src/main/java/org/goobi/production/importer/ProductionDataImport.java
@@ -15,13 +15,7 @@ import com.thoughtworks.xstream.XStream;
 
 import de.sub.goobi.config.ConfigMain;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -86,8 +80,8 @@ public class ProductionDataImport {
 		altdaten = generateProject();
 		try {
 			projectService.save(altdaten);
-			projectList = projectService.search("from Projekt");
-		} catch (DAOException e) {
+			projectList = projectService.search("from Project");
+		} catch (DAOException | IOException e) {
 			System.exit(1);
 		}
 	}
