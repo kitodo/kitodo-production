b5ae3aeb04fb57fa85ba408117da7e8297331ef5
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-28 17:39:07 +0100

    Integrate Indexer with TemplateService

--- Kitodo/src/main/java/org/kitodo/services/TemplateService.java
+++ Kitodo/src/main/java/org/kitodo/services/TemplateService.java
@@ -11,24 +11,46 @@
 
 package org.kitodo.services;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+
 import org.kitodo.data.database.beans.Template;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.TemplateDAO;
+import org.kitodo.data.index.Indexer;
+import org.kitodo.data.index.elasticsearch.type.TemplateType;
+
+import java.io.IOException;
 
 public class TemplateService {
 
     private TemplateDAO templateDao = new TemplateDAO();
+    private TemplateType templateType = new TemplateType();
+    private Indexer<Template, TemplateType> indexer = new Indexer<>("kitodo", Template.class);
 
-    public void save(Template template) throws DAOException {
+    /**
+     * Method saves object to database and insert document to the index of Elastic Search.
+     *
+     * @param template object
+     */
+    public void save(Template template) throws DAOException, IOException {
         templateDao.save(template);
+        indexer.setMethod(HTTPMethods.PUT);
+        indexer.performSingleRequest(template, templateType);
     }
 
     public Template find(Integer id) throws DAOException {
         return templateDao.find(id);
     }
 
-    public void remove(Template template) throws DAOException {
+    /**
+     * Method removes object from database and document from the index of Elastic Search.
+     *
+     * @param template object
+     */
+    public void remove(Template template) throws DAOException, IOException {
         templateDao.remove(template);
+        indexer.setMethod(HTTPMethods.PUT);
+        indexer.performSingleRequest(template, templateType);
     }
 
     public void remove(Integer id) throws DAOException {
