9ab505164e9b196396ea1515749066799c22cdc2
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-02-28 14:43:54 +0100

    Integrate Indexer with HistoryService

--- Kitodo/src/main/java/org/kitodo/services/HistoryService.java
+++ Kitodo/src/main/java/org/kitodo/services/HistoryService.java
@@ -11,9 +11,15 @@
 
 package org.kitodo.services;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+
+import java.io.IOException;
+
 import org.kitodo.data.database.beans.History;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.HistoryDAO;
+import org.kitodo.data.index.Indexer;
+import org.kitodo.data.index.elasticsearch.type.HistoryType;
 
 /**
  * HistoryService.
@@ -21,17 +27,33 @@ import org.kitodo.data.database.persistence.HistoryDAO;
 public class HistoryService {
 
     private HistoryDAO historyDao = new HistoryDAO();
+    private HistoryType historyType = new HistoryType();
+    private Indexer<History, HistoryType> indexer = new Indexer<>("kitodo", History.class);
 
-    public void save(History task) throws DAOException {
-        historyDao.save(task);
+    /**
+     * Method saves object to database and insert document to the index of Elastic Search.
+     *
+     * @param history object
+     */
+    public void save(History history) throws DAOException, IOException {
+        historyDao.save(history);
+        indexer.setMethod(HTTPMethods.PUT);
+        indexer.performSingleRequest(history, historyType);
     }
 
     public History find(Integer id) throws DAOException {
         return historyDao.find(id);
     }
 
-    public void remove(History history) throws DAOException {
+    /**
+     * Method removes object from database and document from the index of Elastic Search.
+     *
+     * @param history object
+     */
+    public void remove(History history) throws DAOException, IOException {
         historyDao.remove(history);
+        indexer.setMethod(HTTPMethods.DELETE);
+        indexer.performSingleRequest(history, historyType);
     }
 
     public void remove(Integer id) throws DAOException {
