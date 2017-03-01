cb2c890823adc113b4d5541824ea49963fd5b166
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-03-01 09:26:34 +0100

    Integrate Indexer with WorkpieceService

--- Kitodo/src/main/java/org/kitodo/services/WorkpieceService.java
+++ Kitodo/src/main/java/org/kitodo/services/WorkpieceService.java
@@ -11,24 +11,46 @@
 
 package org.kitodo.services;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+
+import java.io.IOException;
+
 import org.kitodo.data.database.beans.Workpiece;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.WorkpieceDAO;
+import org.kitodo.data.index.Indexer;
+import org.kitodo.data.index.elasticsearch.type.WorkpieceType;
 
 public class WorkpieceService {
 
     private WorkpieceDAO workpieceDao = new WorkpieceDAO();
+    private WorkpieceType workpieceType = new WorkpieceType();
+    private Indexer<Workpiece, WorkpieceType> indexer = new Indexer<>("kitodo", Workpiece.class);
 
-    public void save(Workpiece workpiece) throws DAOException {
+    /**
+     * Method saves object to database and insert document to the index of Elastic Search.
+     *
+     * @param workpiece object
+     */
+    public void save(Workpiece workpiece) throws DAOException, IOException {
         workpieceDao.save(workpiece);
+        indexer.setMethod(HTTPMethods.PUT);
+        indexer.performSingleRequest(workpiece, workpieceType);
     }
 
     public Workpiece find(Integer id) throws DAOException {
         return workpieceDao.find(id);
     }
 
-    public void remove(Workpiece workpiece) throws DAOException {
+    /**
+     * Method removes object from database and document from the index of Elastic Search.
+     *
+     * @param workpiece object
+     */
+    public void remove(Workpiece workpiece) throws DAOException, IOException {
         workpieceDao.remove(workpiece);
+        indexer.setMethod(HTTPMethods.DELETE);
+        indexer.performSingleRequest(workpiece, workpieceType);
     }
 
     public void remove(Integer id) throws DAOException {
