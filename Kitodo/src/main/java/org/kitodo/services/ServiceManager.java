4557f1eb4a30e755a04a1f4e8b3500dff41517a5
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-03-17 10:32:20 +0100

    Add private attributes for ServiceManager class

--- Kitodo/src/main/java/org/kitodo/services/ServiceManager.java
+++ Kitodo/src/main/java/org/kitodo/services/ServiceManager.java
@@ -11,5 +11,41 @@
 
 package org.kitodo.services;
 
+import org.kitodo.services.data.BatchService;
+import org.kitodo.services.data.DocketService;
+import org.kitodo.services.data.HistoryService;
+import org.kitodo.services.data.LdapGroupService;
+import org.kitodo.services.data.ProcessPropertyService;
+import org.kitodo.services.data.ProcessService;
+import org.kitodo.services.data.ProjectFileGroupService;
+import org.kitodo.services.data.ProjectService;
+import org.kitodo.services.data.RulesetService;
+import org.kitodo.services.data.TaskService;
+import org.kitodo.services.data.TemplatePropertyService;
+import org.kitodo.services.data.TemplateService;
+import org.kitodo.services.data.UserGroupService;
+import org.kitodo.services.data.UserPropertyService;
+import org.kitodo.services.data.UserService;
+import org.kitodo.services.data.WorkpiecePropertyService;
+import org.kitodo.services.data.WorkpieceService;
+
 public class ServiceManager {
+
+    private BatchService batchService = new BatchService();
+    private DocketService docketService = new DocketService();
+    private HistoryService historyService = new HistoryService();
+    private LdapGroupService ldapGroupService = new LdapGroupService();
+    private ProcessPropertyService processPropertyService = new ProcessPropertyService();
+    private ProcessService processService = new ProcessService();
+    private ProjectFileGroupService projectFileGroupService = new ProjectFileGroupService();
+    private ProjectService projectService = new ProjectService();
+    private RulesetService rulesetService = new RulesetService();
+    private TaskService taskService = new TaskService();
+    private TemplatePropertyService templatePropertyService = new TemplatePropertyService();
+    private TemplateService templateService = new TemplateService();
+    private UserGroupService userGroupService = new UserGroupService();
+    private UserPropertyService userPropertyService = new UserPropertyService();
+    private UserService userService = new UserService();
+    private WorkpiecePropertyService workpiecePropertyService = new WorkpiecePropertyService();
+    private WorkpieceService workpieceService = new WorkpieceService();
 }
