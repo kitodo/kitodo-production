b0824916cfc17f0963cc3ef5ca863790953c95f2
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-03-20 09:34:27 +0100

    Add initalization of attributes in ServiceManager class

--- Kitodo/src/main/java/org/kitodo/services/ServiceManager.java
+++ Kitodo/src/main/java/org/kitodo/services/ServiceManager.java
@@ -31,89 +31,208 @@ import org.kitodo.services.data.WorkpieceService;
 
 public class ServiceManager {
 
-    private BatchService batchService = new BatchService();
-    private DocketService docketService = new DocketService();
-    private HistoryService historyService = new HistoryService();
-    private LdapGroupService ldapGroupService = new LdapGroupService();
-    private ProcessPropertyService processPropertyService = new ProcessPropertyService();
-    private ProcessService processService = new ProcessService();
-    private ProjectFileGroupService projectFileGroupService = new ProjectFileGroupService();
-    private ProjectService projectService = new ProjectService();
-    private RulesetService rulesetService = new RulesetService();
-    private TaskService taskService = new TaskService();
-    private TemplatePropertyService templatePropertyService = new TemplatePropertyService();
-    private TemplateService templateService = new TemplateService();
-    private UserGroupService userGroupService = new UserGroupService();
-    private UserPropertyService userPropertyService = new UserPropertyService();
-    private UserService userService = new UserService();
-    private WorkpiecePropertyService workpiecePropertyService = new WorkpiecePropertyService();
-    private WorkpieceService workpieceService = new WorkpieceService();
+    private BatchService batchService;
+    private DocketService docketService;
+    private HistoryService historyService;
+    private LdapGroupService ldapGroupService;
+    private ProcessPropertyService processPropertyService;
+    private ProcessService processService;
+    private ProjectFileGroupService projectFileGroupService;
+    private ProjectService projectService;
+    private RulesetService rulesetService;
+    private TaskService taskService;
+    private TemplatePropertyService templatePropertyService;
+    private TemplateService templateService;
+    private UserGroupService userGroupService;
+    private UserPropertyService userPropertyService;
+    private UserService userService;
+    private WorkpiecePropertyService workpiecePropertyService;
+    private WorkpieceService workpieceService;
+
+    private void initializeBatchService() {
+        if (batchService == null) {
+            batchService = new BatchService();
+        }
+    }
+
+    private void initializeDocketService() {
+        if (docketService == null) {
+            docketService = new DocketService();
+        }
+    }
+
+    private void initializeHistoryService() {
+        if (historyService == null) {
+            historyService = new HistoryService();
+        }
+    }
+
+    private void initializeLdapGroupService() {
+        if (ldapGroupService == null) {
+            ldapGroupService = new LdapGroupService();
+        }
+    }
+
+    private void initializeProcessPropertyService() {
+        if (processPropertyService == null) {
+            processPropertyService = new ProcessPropertyService();
+        }
+    }
+
+    private void initializeProcessService() {
+        if (processService == null) {
+            processService = new ProcessService();
+        }
+    }
+
+    private void initializeProjectFileGroupService() {
+        if (projectFileGroupService == null) {
+            projectFileGroupService = new ProjectFileGroupService();
+        }
+    }
+
+    private void initializeProjectService() {
+        if (projectService == null) {
+            projectService = new ProjectService();
+        }
+    }
+
+    private void initializeRulesetService() {
+        if (rulesetService == null) {
+            rulesetService = new RulesetService();
+        }
+    }
+
+    private void initializeTaskService() {
+        if (taskService == null) {
+            taskService = new TaskService();
+        }
+    }
+
+    private void initializeTemplatePropertyService() {
+        if (templatePropertyService == null) {
+            templatePropertyService = new TemplatePropertyService();
+        }
+    }
+
+    private void initializeTemplateService() {
+        if (templateService == null) {
+            templateService = new TemplateService();
+        }
+    }
+
+    private void initializeUserGroupService() {
+        if (userGroupService == null) {
+            userGroupService = new UserGroupService();
+        }
+    }
+
+    private void initializeUserPropertyService() {
+        if (userPropertyService == null) {
+            userPropertyService = new UserPropertyService();
+        }
+    }
+
+    private void initializeUserService() {
+        if (userService == null) {
+            userService = new UserService();
+        }
+    }
+
+    private void initializeWorkpiecePropertyService() {
+        if (workpiecePropertyService == null) {
+            workpiecePropertyService = new WorkpiecePropertyService();
+        }
+    }
+
+    private void initializeWorkpieceService() {
+        if (workpieceService == null) {
+            workpieceService = new WorkpieceService();
+        }
+    }
 
     public BatchService getBatchService() {
+        initializeBatchService();
         return batchService;
     }
 
     public DocketService getDocketService() {
+        initializeDocketService();
         return docketService;
     }
 
     public HistoryService getHistoryService() {
+        initializeHistoryService();
         return historyService;
     }
 
     public LdapGroupService getLdapGroupService() {
+        initializeLdapGroupService();
         return ldapGroupService;
     }
 
     public ProcessPropertyService getProcessPropertyService() {
+        initializeProcessPropertyService();
         return processPropertyService;
     }
 
     public ProcessService getProcessService() {
+        initializeProcessService();
         return processService;
     }
 
     public ProjectFileGroupService getProjectFileGroupService() {
+        initializeProjectFileGroupService();
         return projectFileGroupService;
     }
 
     public ProjectService getProjectService() {
+        initializeProjectService();
         return projectService;
     }
 
     public RulesetService getRulesetService() {
+        initializeRulesetService();
         return rulesetService;
     }
 
     public TaskService getTaskService() {
+        initializeTaskService();
         return taskService;
     }
 
     public TemplatePropertyService getTemplatePropertyService() {
+        initializeTemplatePropertyService();
         return templatePropertyService;
     }
 
     public TemplateService getTemplateService() {
+        initializeTemplateService();
         return templateService;
     }
 
     public UserGroupService getUserGroupService() {
+        initializeUserGroupService();
         return userGroupService;
     }
 
     public UserPropertyService getUserPropertyService() {
+        initializeUserPropertyService();
         return userPropertyService;
     }
 
     public UserService getUserService() {
+        initializeUserService();
         return userService;
     }
 
     public WorkpiecePropertyService getWorkpiecePropertyService() {
+        initializeWorkpiecePropertyService();
         return workpiecePropertyService;
     }
 
     public WorkpieceService getWorkpieceService() {
+        initializeWorkpieceService();
         return workpieceService;
     }
 }
