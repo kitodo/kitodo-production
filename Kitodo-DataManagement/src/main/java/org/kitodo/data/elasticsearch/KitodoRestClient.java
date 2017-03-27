24982eeab8e0e1eade9b1bba457ebe772ec3030c
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-03-27 13:39:21 +0200

    Add KitodoRestClient class which implements RestClientInterface

---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/elasticsearch/KitodoRestClient.java
@@ -0,0 +1,92 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.elasticsearch;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.kitodo.data.elasticsearch.api.RestClientInterface;
+
+/**
+ * Implementation of Elastic Search REST Client for Index Module.
+ */
+public class KitodoRestClient implements RestClientInterface {
+    protected String index;
+    protected String type;
+    protected RestClient restClient;
+
+    /**
+     * Create REST client.
+     *
+     * @param host
+     *            default host is localhost
+     * @param port
+     *            default port ist 9200
+     * @param protocol
+     *            default protocol is http
+     */
+    public void initiateClient(String host, Integer port, String protocol) {
+        restClient = RestClient.builder(new HttpHost(host, port, protocol)).build();
+    }
+
+    /**
+     * Get information about client server.
+     *
+     * @return information about the server
+     */
+    public String getServerInformation() throws IOException {
+        Response response = restClient.performRequest("GET", "/", Collections.singletonMap("pretty", "true"));
+        return EntityUtils.toString(response.getEntity());
+    }
+
+    /**
+     * Close REST Client.
+     *
+     * @throws IOException
+     *             add description
+     */
+    public void closeClient() throws IOException {
+        restClient.close();
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    /**
+     * Setter for index.
+     *
+     * @param index
+     *            - equal to the name of database, default kitodo
+     */
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Setter for type.
+     *
+     * @param type
+     *            - equal to the name of table in database
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}
