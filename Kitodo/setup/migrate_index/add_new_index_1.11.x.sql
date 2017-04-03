5873f0be974fe6f093b8b697bfea56e75089d196
Kathrin Huber <kathrin.huber@slub-dresden.de>
2017-04-03 10:49:48 +0200

    fix capital letters

--- Kitodo/setup/migrate_index/add_new_index_1.11.x.sql
+++ Kitodo/setup/migrate_index/add_new_index_1.11.x.sql
@@ -1,5 +1,5 @@
 --
--- Add new indexes for kitodo.Production CE newer than 1.11.2
+-- Add new indexes for Kitodo.Production CE newer than 1.11.2
 --
 ALTER TABLE `batchesprozesse`
   ADD KEY `FK_ProzesseID` (`ProzesseID`),
