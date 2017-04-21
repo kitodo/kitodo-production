234bca5b5bffe46d86b0bbdedffbbf1ee85b3a6a
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-04-21 13:33:45 +0200

    Add find methods for DocketService

--- Kitodo/src/main/java/org/kitodo/services/data/DocketService.java
+++ Kitodo/src/main/java/org/kitodo/services/data/DocketService.java
@@ -16,16 +16,21 @@ import com.sun.research.ws.wadl.HTTPMethods;
 import java.io.IOException;
 import java.util.List;
 
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.json.simple.parser.ParseException;
 import org.kitodo.data.database.beans.Docket;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.DocketDAO;
 import org.kitodo.data.elasticsearch.exceptions.ResponseException;
 import org.kitodo.data.elasticsearch.index.Indexer;
 import org.kitodo.data.elasticsearch.index.type.DocketType;
+import org.kitodo.data.elasticsearch.search.SearchResult;
 import org.kitodo.data.elasticsearch.search.Searcher;
-import org.kitodo.services.data.base.SearchService;
+import org.kitodo.services.data.base.TitleSearchService;
 
-public class DocketService extends SearchService {
+public class DocketService extends TitleSearchService {
     private DocketDAO docketDao = new DocketDAO();
     private DocketType docketType = new DocketType();
     private Indexer<Docket, DocketType> indexer = new Indexer<>(Docket.class);
@@ -77,6 +82,51 @@ public class DocketService extends SearchService {
 
     public Long count(String query) throws DAOException {
         return docketDao.count(query);
+    }
+
+    /**
+     * Find docket with exact file name.
+     *
+     * @param file
+     *            of the searched docket
+     * @return search result
+     */
+    public SearchResult findByFile(String file) throws IOException, ParseException {
+        QueryBuilder query = createSimpleQuery("file", file, true);
+        return searcher.findDocument(query.toString());
+    }
+
+    /**
+     * Find docket with exact title and file name.
+     * 
+     * @param title
+     *            of the searched docket
+     * @param file
+     *            of the searched docket
+     * @return search result
+     */
+    public SearchResult findByTitleAndFile(String title, String file) throws IOException, ParseException {
+        BoolQueryBuilder query = new BoolQueryBuilder();
+        query.must(createSimpleQuery("title", title, true));
+        query.must(createSimpleQuery("file", file, true));
+        return searcher.findDocument(query.toString());
+    }
+
+    /**
+     * Find docket with exact title or file name.
+     *
+     * @param title
+     *            of the searched docket
+     * @param file
+     *            of the searched docket
+     * @return search result
+     */
+    public List<SearchResult> findByTitleOrFile(String title, String file) throws IOException, ParseException {
+        BoolQueryBuilder query = new BoolQueryBuilder();
+        query.should(createSimpleQuery("title", title, true));
+        query.should(createSimpleQuery("file", file, true));
+        System.out.println(query.toString());
+        return searcher.findDocuments(query.toString());
     }
 
     /**
