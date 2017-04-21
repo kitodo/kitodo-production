d75cd24d533228bf756e77e9e3e7f947b68ef120
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-04-21 14:27:53 +0200

    Add ElasticSearch's tests for DocketService

--- Kitodo/src/test/java/org/kitodo/services/data/DocketServiceIT.java
+++ Kitodo/src/test/java/org/kitodo/services/data/DocketServiceIT.java
@@ -24,6 +24,7 @@ import org.kitodo.MockDatabase;
 import org.kitodo.data.database.beans.Docket;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.elasticsearch.exceptions.ResponseException;
+import org.kitodo.data.elasticsearch.search.SearchResult;
 
 /**
  * Tests for DocketService class.
@@ -55,5 +56,88 @@ public class DocketServiceIT {
 
         List<Docket> dockets = docketService.findAll();
         assertEquals("Not all dockets were found in database!", 2, dockets.size());
+    }
+
+    @Test
+    public void shouldFindById() throws Exception {
+        DocketService docketService = new DocketService();
+
+        SearchResult docket = docketService.findById(1);
+        String actual = docket.getProperties().get("title");
+        String expected = "default";
+        assertEquals("Docket was not found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByTitle() throws Exception {
+        DocketService docketService = new DocketService();
+
+        SearchResult docket = docketService.findByTitle("default", true);
+        String actual = docket.getProperties().get("title");
+        String expected = "default";
+        assertEquals("Docket was not found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByFile() throws Exception {
+        DocketService docketService = new DocketService();
+
+        SearchResult docket = docketService.findByFile("docket.xsl");
+        String actual = docket.getProperties().get("file");
+        String expected = "docket.xsl";
+        assertEquals("Docket was not found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByTitleAndFile() throws Exception {
+        DocketService docketService = new DocketService();
+
+        SearchResult docket = docketService.findByTitleAndFile("default","docket.xsl");
+        Integer actual = docket.getId();
+        Integer expected = 1;
+        assertEquals("Docket was not found in index!", expected, actual);
+
+        docket = docketService.findByTitleAndFile("default","none");
+        actual = docket.getId();
+        expected = null;
+        assertEquals("Docket was found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByTitleOrFile() throws Exception {
+        DocketService docketService = new DocketService();
+
+        List<SearchResult> docket = docketService.findByTitleOrFile("default","docket.xsl");
+        Integer actual = docket.size();
+        Integer expected = 2;
+        assertEquals("Dockets were not found in index!", expected, actual);
+
+        docket = docketService.findByTitleOrFile("default","none");
+        actual = docket.size();
+        expected = 1;
+        assertEquals("Docket was not found in index!", expected, actual);
+
+        docket = docketService.findByTitleOrFile("none","none");
+        actual = docket.size();
+        expected = 0;
+        assertEquals("Some dockets were found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindAllDocketsDocuments() throws Exception {
+        DocketService docketService = new DocketService();
+
+        List<SearchResult> dockets = docketService.findAllDocuments();
+        assertEquals("Not all dockets were found in index!", 2, dockets.size());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shouldConvertSearchResultsToObjectList() throws Exception {
+        DocketService docketService = new DocketService();
+
+        List<SearchResult> searchResults = docketService.findAllDocuments();
+        List<Docket> dockets = (List<Docket>) docketService.convertSearchResultsToObjectList(searchResults, "Docket");
+        assertEquals("Not all dockets were converted!", 2, dockets.size());
     }
 }
