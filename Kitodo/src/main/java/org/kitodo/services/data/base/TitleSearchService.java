27c7021e7fdb0bcb1e61175dd76944224de99775
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-04-21 13:57:08 +0200

    Introduce TitleSearchService which extends SearchService with method findByTitle

---
+++ Kitodo/src/main/java/org/kitodo/services/data/base/TitleSearchService.java
@@ -0,0 +1,48 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.services.data.base;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.json.simple.parser.ParseException;
+import org.kitodo.data.elasticsearch.search.SearchResult;
+import org.kitodo.data.elasticsearch.search.Searcher;
+
+/**
+ * Class for implementing methods used by service classes which search for title
+ * in ElasticSearch index.
+ */
+public abstract class TitleSearchService extends SearchService {
+
+    public TitleSearchService(Searcher searcher) {
+        super(searcher);
+    }
+
+    /**
+     * Find object matching to given title.
+     *
+     * @param title
+     *            of the searched process
+     * @param contains
+     *            if true result should contain given plain text, if false it
+     *            should not contain
+     * @return list of search result
+     */
+    public List<SearchResult> findByTitle(String title, boolean contains) throws IOException, ParseException {
+        QueryBuilder query = createSimpleQuery("title", title, contains);
+        return searcher.findDocuments(query.toString());
+    }
+}
