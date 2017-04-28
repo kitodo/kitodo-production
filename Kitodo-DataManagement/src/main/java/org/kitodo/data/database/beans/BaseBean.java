a8a6695ef1c518ca4af443e3a792eac0e1eb104d
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-04-28 16:16:05 +0200

    Add attribute for tables which are indexed

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/beans/BaseBean.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/beans/BaseBean.java
@@ -14,9 +14,13 @@ package org.kitodo.data.database.beans;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.kitodo.data.database.helper.enums.IndexAction;
 
 /**
  * Base bean class.
@@ -31,11 +35,35 @@ public abstract class BaseBean implements Serializable {
     @GeneratedValue
     private Integer id;
 
+    @Column(name = "indexAction")
+    @Enumerated(EnumType.STRING)
+    private IndexAction indexAction;
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    /**
+     * Get action which should be performed on ElasticSearch index.
+     *
+     * @return action which needs to be performed on index
+     */
+    public IndexAction getIndexAction() {
+        return indexAction;
+    }
+
+    /**
+     * Set DONE if record is already indexed in ElasticSearch, set INDEX if it
+     * needs to be indexed and DELETE if it needs to be deleted.
+     *
+     * @param indexAction
+     *            index, delete or done
+     */
+    public void setIndexAction(IndexAction indexAction) {
+        this.indexAction = indexAction;
     }
 }
