8bac3c107cb0664a6fcae4bdc934357d1ee70049
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-05-08 14:54:24 +0200

    Add tests for UserGroupService

--- Kitodo/src/test/java/org/kitodo/services/data/UserGroupServiceIT.java
+++ Kitodo/src/test/java/org/kitodo/services/data/UserGroupServiceIT.java
@@ -15,14 +15,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kitodo.MockDatabase;
 import org.kitodo.data.database.beans.UserGroup;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.elasticsearch.exceptions.CustomResponseException;
+import org.kitodo.data.elasticsearch.search.SearchResult;
 
 /**
  * Tests for UserGroupService class.
@@ -48,6 +51,11 @@ public class UserGroupServiceIT {
         assertTrue("User group was not found in database!", condition);
     }
 
+    @Before
+    public void multipleInit() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
     @Test
     public void shouldRemoveUserGroup() throws Exception {
         UserGroupService userGroupService = new UserGroupService();
@@ -71,6 +79,81 @@ public class UserGroupServiceIT {
         userGroupService.remove(5);
         foundUserGroup = userGroupService.convertSearchResultToObject(userGroupService.findById(5));
         assertEquals("Additional user group was not removed from database!", null, foundUserGroup);
+    }
+
+    @Test
+    public void shouldFindById() throws Exception {
+        UserGroupService userGroupService = new UserGroupService();
+
+        SearchResult userGroup = userGroupService.findById(1);
+        String actual = userGroup.getProperties().get("title");
+        String expected = "Admin";
+        assertEquals("User group was not found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByTitle() throws Exception {
+        UserGroupService userGroupService = new UserGroupService();
+
+        List<SearchResult> userGroups = userGroupService.findByTitle("Admin", true);
+        Integer actual = userGroups.size();
+        Integer expected = 1;
+        assertEquals("User group was not found in index!", expected, actual);
+
+        userGroups = userGroupService.findByTitle("none", true);
+        actual = userGroups.size();
+        expected = 0;
+        assertEquals("User group was found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByPermission() throws Exception {
+        UserGroupService userGroupService = new UserGroupService();
+
+        List<SearchResult> userGroups = userGroupService.findByPermission(1);
+        Integer actual = userGroups.size();
+        Integer expected = 1;
+        assertEquals("User group was not found in index!", expected, actual);
+
+        userGroups = userGroupService.findByPermission(4);
+        actual = userGroups.size();
+        expected = 1;
+        assertEquals("User group was not found in index!", expected, actual);
+
+        userGroups = userGroupService.findByPermission(5);
+        actual = userGroups.size();
+        expected = 0;
+        assertEquals("User group was found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByUserId() throws Exception {
+        UserGroupService userGroupService = new UserGroupService();
+
+        List<SearchResult> userGroups = userGroupService.findByUserId(1);
+        Integer actual = userGroups.size();
+        Integer expected = 1;
+        assertEquals("User group was not found in index!", expected, actual);
+
+        userGroups = userGroupService.findByUserId(2);
+        actual = userGroups.size();
+        expected = 0;
+        assertEquals("User groups were found in index!", expected, actual);
+    }
+
+    @Test
+    public void shouldFindByUserLogin() throws Exception {
+        UserGroupService userGroupService = new UserGroupService();
+
+        List<SearchResult> userGroups = userGroupService.findByUserLogin("kowal");
+        Integer actual = userGroups.size();
+        Integer expected = 1;
+        assertEquals("User group was not found in index!", expected, actual);
+
+        userGroups = userGroupService.findByUserLogin("dora");
+        actual = userGroups.size();
+        expected = 0;
+        assertEquals("User groups were found in index!", expected, actual);
     }
 
     @Test
