95566fe937f1e2acf7218dd74f4343206018c500
Julien Ambos <ambos@effective-webwork.de>
2017-05-17 11:48:14 +0200

    Fixed the missing files, added correct relative path to toggle.js and toggle.js.css script in head.xhtml
    and removed TODOs in several files related with the <jd:hideableController> and <jd:hideableArea>

---
+++ Kitodo/src/main/webapp/js/toggle.js
@@ -0,0 +1,43 @@
+/*
+ *  Simple toggle visibility script
+ *
+ *  Every HTML-Element with the class "toggle" has a click event attached to change the visibility of another
+ *  HTML-Element, according to the specified "data-for" attribute.
+ *
+ *  Usage:
+ *  Create an HTML-Element which will function as the trigger for the visibility change of the target HTML-Element.
+ *  Give it the class "toggle" and specify a target element with the "data-for" attribute. Valid inputs are
+ *  classnames.
+ *
+ *  Example:
+ *  <span class="toggle" data-for="toggle-1">Click me</span>
+ *
+ *  Then create the HTML-Elemenent, whose visibilty will be toggled and give it the classname that you specified in
+ *  the "data-for" attribute of your trigger HTML-Element
+ *
+ *  Example:
+ *  <div class="toggle-1">Content</div>
+ *
+ *  Further usage:
+ *  Initially set the visibility status of the target HTML-Element with the css attribute "display"
+ *
+ *  Example:
+ *  <div class="toggle-1" style="display: none;">I'm invisible when loaded</div>
+ *
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call(document.querySelectorAll(".toggle"), function(el) {
+      el.addEventListener("click", function() {
+        try {
+            var toggleEl = document.querySelectorAll("." + el.getAttribute('data-for'))[0];
+            if (toggleEl.style.display == 'none') {
+                toggleEl.style.display = '';
+            } else if (toggleEl.style.display == '' ){
+                toggleEl.style.display = 'none';
+            }
+        }
+        catch (err) {
+        }
+      });
+    });
+});
