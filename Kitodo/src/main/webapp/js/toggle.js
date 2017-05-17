fd2a72e650f6b44f436e5ec9e2c2984050692bc7
Julien Ambos <ambos@effective-webwork.de>
2017-05-17 16:03:08 +0200

    Added line endings to toggle.js and toggle.js.css files and added licensing headers

--- Kitodo/src/main/webapp/js/toggle.js
+++ Kitodo/src/main/webapp/js/toggle.js
@@ -1,4 +1,15 @@
-/*
+/**
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+/**
  *  Simple toggle visibility script
  *
  *  Every HTML-Element with the class "toggle" has a click event attached to change the visibility of another
@@ -41,3 +52,4 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
 });
+
