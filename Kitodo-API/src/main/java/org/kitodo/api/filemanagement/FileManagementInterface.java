85a20616ecfe7adf36326317064a27206add4f2b
Kathrin Huber <kathrin.huber@slub-dresden.de>
2017-06-01 14:55:09 +0200

    Adjust filemanagement

--- Kitodo-API/src/main/java/org/kitodo/api/filemanagement/FileManagementInterface.java
+++ Kitodo-API/src/main/java/org/kitodo/api/filemanagement/FileManagementInterface.java
@@ -16,6 +16,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 
+/**
+ * Manages the handling of files.
+ */
 public interface FileManagementInterface {
 
     /**
@@ -46,13 +49,37 @@ public interface FileManagementInterface {
     boolean delete(URI uri) throws IOException;
 
     /**
+     * Renames a resource at a given URI.
+     * 
+     * @param uri
+     *            The uri to the ressource to rename.
+     * @param newName
+     *            The new name of the resource.
+     * @return true, if successful, false otherwise.
+     */
+    boolean rename(URI uri, String newName);
+
+    /**
+     * Returns the uri to a given subtype of a process.
+     * 
+     * @param processUri
+     *            The base uri of the process.
+     * @param subType
+     *            the subtype.
+     * @param id
+     *            the id of a specific element at the subtype
+     * @return the URI to the requested resource.
+     */
+    URI getProcessSubTypeUri(URI processUri, ProcessSubType subType, int id);
+
+    /**
      * Creates the FolderStructure needed for a process in kitodo.
      * 
      * @param processId
      *            the id of the process
-     * @return a ProcessLocation
+     * @return the URI to the process Location
      */
-    ProcessLocation createProcessLocation(String processId) throws IOException;
+    URI createProcessLocation(String processId) throws IOException;
 
     /**
      * Creates a directory with a given name at a given uri.
@@ -75,5 +102,37 @@ public interface FileManagementInterface {
      * @return the URI of the new resource
      */
     URI createResource(URI parentFolderUri, String fileName);
+
+    /**
+     * Creates a symbolic link.
+     *
+     * @param targetUri
+     *            The target URI for the link.
+     * @param homeUri
+     *            The home URI.
+     * @return true, if link creation was successfull.
+     */
+    boolean createSymLink(URI targetUri, URI homeUri);
+
+    /**
+     * Delets a symbolik link.
+     *
+     * @param homeUri
+     *            The uri of the home folder, where the link should be deleted.
+     * @return true, if deletion was successull.
+     */
+    boolean deleteSymLink(URI homeUri);
+
+    /**
+     * Creates the URI, if a process and it's location already exists, so no
+     * processlocation needs to be created. This method is for migration
+     * purposes, because when this interface is used, there will be already
+     * processes in the filesystem.
+     * 
+     * @param processId
+     *            the id of the process.
+     * @return The uri of the process location.
+     */
+    URI createUriForExistingProcess(String processId);
 
 }
