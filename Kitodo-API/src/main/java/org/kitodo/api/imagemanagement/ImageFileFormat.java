e5a9e2c726b38ab5dba968476335f042172a1435
Kathrin Huber <kathrin.huber@slub-dresden.de>
2017-06-01 14:58:31 +0200

    Adjust imagemanagement

--- Kitodo-API/src/main/java/org/kitodo/api/imagemanagement/ImageFileFormat.java
+++ Kitodo-API/src/main/java/org/kitodo/api/imagemanagement/ImageFileFormat.java
@@ -11,8 +11,11 @@
 
 package org.kitodo.api.imagemanagement;
 
+/**
+ * Possible File formats.
+ */
 public enum ImageFileFormat {
 
-    JPEG, JPEG2000, GIF, PNG, BMP, PDF, TIFF;
+    JPEG, JPEG2000, GIF, PNG, BMP, TIFF;
 
 }
