b8883e52c101ed9bd4f35f4d9d163e16b2b0a8b1
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-06-13 13:09:00 +0200

    Add additional values for tests

--- Kitodo-PersistentIdentifier/src/test/java/org/kitodo/persistentidentifier/URNPersistentIdentifierTest.java
+++ Kitodo-PersistentIdentifier/src/test/java/org/kitodo/persistentidentifier/URNPersistentIdentifierTest.java
@@ -23,7 +23,24 @@ public class URNPersistentIdentifierTest {
     @Test
     public void shouldGenerateUnifiedResourceName() throws Exception {
         URNPersistentIdentifier urnPersistentIdentifier = new URNPersistentIdentifier();
-        String urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "14", "db", "378704842");
+
+        String urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "14", "db", "id378704842");
         assertEquals("Incorrect check digit in URN ", "urn:nbn:de:bsz:14-db-id3787048428", urn);
+
+        urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "14", "db", "id470035625");
+        assertEquals("Incorrect check digit in URN ", "urn:nbn:de:bsz:14-db-id4700356257", urn);
+
+        urn = urnPersistentIdentifier.generateUnifiedResourceName("boc", "12", "db", "478704844");
+        assertEquals("Incorrect check digit in URN ", "urn:nbn:de:boc:12-db-4787048440", urn);
+
+        urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "14", "qucosa", "22579");
+        assertEquals("Incorrect check digit in URN ", "urn:nbn:de:bsz:14-qucosa-225799", urn);
+
+        urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "d120", "qucosa", "22440");
+        assertEquals("Incorrect check digit in URN ", "urn:nbn:de:bsz:d120-qucosa-224404", urn);
+
+        urn = urnPersistentIdentifier.generateUnifiedResourceName("bsz", "ch1", "qucosa", "22583");
+        assertEquals("Incorrect check digit in URN ", "urn:nbn:de:bsz:ch1-qucosa-225834", urn);
+
     }
 }
