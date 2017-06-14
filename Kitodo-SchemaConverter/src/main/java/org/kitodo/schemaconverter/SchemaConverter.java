d1a8043eb980f7c7b3f0288e67e72025c82710a8
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-06-14 11:33:20 +0200

    Add main SchemaConverter class and log4j2.xml files

---
+++ Kitodo-SchemaConverter/src/main/java/org/kitodo/schemaconverter/SchemaConverter.java
@@ -0,0 +1,28 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.schemaconverter;
+
+import java.net.URI;
+
+import org.kitodo.api.schemaconverter.SchemaConverterInterface;
+import org.kitodo.api.schemaconverter.SchemaFormat;
+
+/**
+ * Schema converter.
+ */
+public class SchemaConverter implements SchemaConverterInterface {
+
+    @Override
+    public URI convert(URI inputFileUri, SchemaFormat baseFormat, SchemaFormat resultFormat) {
+        return null;
+    }
+}
