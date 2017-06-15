59cf0180a6e30c502c22e4a9b8458126062cb6ea
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-06-15 17:08:11 +0200

    Add wrapper exception - DataException

---
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/exceptions/DataException.java
@@ -0,0 +1,28 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.data.exceptions;
+
+/**
+ * Exception for wrapping CustomResponseException, DAOException and IOException.
+ */
+public class DataException extends Exception {
+
+    private static final long serialVersionUID = 1987853363232807999L;
+
+    public DataException(Exception e) {
+        super(e);
+    }
+
+    public DataException(String message) {
+        super(message);
+    }
+}
