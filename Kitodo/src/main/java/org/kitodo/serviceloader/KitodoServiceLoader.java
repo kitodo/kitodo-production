d1dc2b5d4f2315a0cb6ad364dcbd391391027275
Kathrin Huber <kathrin.huber@slub-dresden.de>
2017-06-16 11:03:55 +0200

    Refine ServiceLoader

--- Kitodo/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
+++ Kitodo/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
@@ -31,17 +31,19 @@ public class KitodoServiceLoader<T> {
         File loc = new File("plugins");
 
         URLClassLoader sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        URL urls[] = sysLoader.getURLs(), udir = loc.toURI().toURL();
+        URL urls[] = sysLoader.getURLs();
+        URL udir = loc.toURI().toURL();
         String udirs = udir.toString();
-        for (int i = 0; i < urls.length; i++)
-            if (urls[i].toString().equalsIgnoreCase(udirs)) {
+        for (URL url : urls) {
+            if (url.toString().equalsIgnoreCase(udirs)) {
                 return null;
             }
+        }
         Class<URLClassLoader> sysClass = URLClassLoader.class;
         try {
-            Method method = sysClass.getDeclaredMethod("addURL", new Class[] {URL.class });
+            Method method = sysClass.getDeclaredMethod("addURL", URL.class);
             method.setAccessible(true);
-            method.invoke(sysLoader, new Object[] {udir });
+            method.invoke(sysLoader, udir);
         } catch (Throwable t) {
             t.printStackTrace();
         }
