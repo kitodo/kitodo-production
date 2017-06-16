d9b4a2b2cd5d10bbef4c5d5a67c826966d46b08a
Kathrin Huber <kathrin.huber@slub-dresden.de>
2017-06-16 11:19:06 +0200

    Fix ServiceLoader

--- Kitodo/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
+++ Kitodo/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
@@ -12,12 +12,20 @@
 package org.kitodo.serviceloader;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Iterator;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ServiceLoader;
+
+import org.kitodo.config.ConfigMain;
 
 public class KitodoServiceLoader<T> {
     private ServiceLoader<T> loader;
@@ -27,30 +35,38 @@ public class KitodoServiceLoader<T> {
         this.clazz = clazz;
     }
 
-    public T loadModule() throws MalformedURLException {
-        File loc = new File("plugins");
+    public T loadModule() throws IOException {
 
-        URLClassLoader sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        URL urls[] = sysLoader.getURLs();
-        URL udir = loc.toURI().toURL();
-        String udirs = udir.toString();
-        for (URL url : urls) {
-            if (url.toString().equalsIgnoreCase(udirs)) {
-                return null;
-            }
-        }
-        Class<URLClassLoader> sysClass = URLClassLoader.class;
-        try {
-            Method method = sysClass.getDeclaredMethod("addURL", URL.class);
-            method.setAccessible(true);
-            method.invoke(sysLoader, udir);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+        loadModulesIntoClasspath();
 
+        ServiceLoader<T> loader;
         loader = ServiceLoader.load(clazz);
-        Iterator<T> modules = loader.iterator();
-        return modules.next();
+
+        return loader.iterator().next();
+    }
+
+    public void loadModulesIntoClasspath() {
+        Path pluginFolder = FileSystems.getDefault().getPath(ConfigMain.getParameter("pluginFolder"));
+
+        URLClassLoader sysLoader = null;
+        try {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(pluginFolder, "*.jar");
+            for (Path f : stream) {
+                File loc = new File(f.toString());
+                sysLoader = (URLClassLoader) this.getClass().getClassLoader();
+                ArrayList<URL> urls = new ArrayList<>(Arrays.asList(sysLoader.getURLs()));
+                URL udir = loc.toURI().toURL();
+
+                if (!urls.contains(udir)) {
+                    Class<URLClassLoader> sysClass = URLClassLoader.class;
+                    Method method = sysClass.getDeclaredMethod("addURL", URL.class);
+                    method.setAccessible(true);
+                    method.invoke(sysLoader, udir);
+                }
+            }
+        } catch (IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
 }
