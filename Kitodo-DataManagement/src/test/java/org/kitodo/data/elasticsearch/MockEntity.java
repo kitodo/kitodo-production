b7727cb88470e82a85bceef7d7c545aa7ab62397
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-07-12 16:19:45 +0200

    Add settings for node to MockEntitiy class

--- Kitodo-DataManagement/src/test/java/org/kitodo/data/elasticsearch/MockEntity.java
+++ Kitodo-DataManagement/src/test/java/org/kitodo/data/elasticsearch/MockEntity.java
@@ -11,16 +11,36 @@
 
 package org.kitodo.data.elasticsearch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
-
-import java.util.HashMap;
 
 /**
  * Mock entities for ElasticSearch classes.
  */
 public class MockEntity {
+
+    @SuppressWarnings("unchecked")
+    public static Map prepareNodeSettings(String httpPort, String httpTransportPort, String nodeName) {
+        Map settingsMap = new HashMap();
+        settingsMap.put("node.name", nodeName);
+        // create all data directories under Maven build directory
+        settingsMap.put("path.conf", "target");
+        settingsMap.put("path.data", "target");
+        settingsMap.put("path.logs", "target");
+        settingsMap.put("path.home", "target");
+        // set ports used by Elastic Search to something different than default
+        settingsMap.put("http.type", "netty4");
+        settingsMap.put("http.port", httpPort);
+        settingsMap.put("transport.tcp.port", httpTransportPort);
+        settingsMap.put("transport.type", "netty4");
+        // disable automatic index creation
+        settingsMap.put("action.auto_create_index", "false");
+        return settingsMap;
+    }
 
     public static HashMap<Integer, HttpEntity> createEntities() {
         HashMap<Integer, HttpEntity> documents = new HashMap<>();
