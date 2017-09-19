af92b559f2a22644e3458dffdf7e45ec318f0284
Julien Ambos <ambos@effective-webwork.de>
2017-09-19 14:36:37 +0200

    The KitodoServiceLoader now loads frontend files of modules into the core module.

--- Kitodo-API/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
+++ Kitodo-API/src/main/java/org/kitodo/serviceloader/KitodoServiceLoader.java
@@ -12,7 +12,9 @@
 package org.kitodo.serviceloader;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -21,9 +23,12 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ServiceLoader;
+import java.util.*;
+
+import com.sun.org.apache.xpath.internal.operations.Equals;
+import org.apache.commons.io.FileUtils;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,7 +40,7 @@ public class KitodoServiceLoader<T> {
 
     /**
      * Constructor for KitodoServiceLoader.
-     * 
+     *
      * @param clazz
      *            interface class of module to load
      * @param modulePath
@@ -52,17 +57,203 @@ public class KitodoServiceLoader<T> {
 
     /**
      * Loads a module from the classpath which implements the constructed clazz.
-     * 
+     * Frontend files of all modules will be loaded into the core module.
+     *
      * @return A module with type T.
      */
     @SuppressWarnings("unchecked")
-    public T loadModule() {
+    public T loadModule() throws IOException {
 
         loadModulesIntoClasspath();
+        loadFrontendFilesIntoCore();
 
         ServiceLoader<T> loader = ServiceLoader.load(clazz);
 
         return loader.iterator().next();
+    }
+
+    /**
+     * If the found jar files have frontend files, they will be extracted and copied into the frontend folder
+     * of the core module. Before copying, existing frontend files of the same module will be deleted from the
+     * core module. Afterwards the created temporary folder will be deleted as well.
+     */
+    private void loadFrontendFilesIntoCore() throws IOException {
+        Path moduleFolder = FileSystems.getDefault().getPath(modulePath);
+
+        try {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(moduleFolder, "*.jar");
+
+            for (Path f : stream) {
+
+                File loc = new File(f.toString());
+                JarFile jarFile = new JarFile(loc);
+
+                if (hasFrontendFiles(jarFile)) {
+                    String temporaryFolderName = generateRandomString(10);
+                    extractFrontEndFiles(loc.getAbsolutePath(), temporaryFolderName);
+                    String moduleName = extractModuleName(temporaryFolderName);
+                    FileUtils.deleteDirectory(new File("Kitodo/src/main/webapp/pages/" + moduleName));
+                    copyFrontEndFiles(temporaryFolderName + "/META-INF/resources", "Kitodo/src/main/webapp/pages/" + moduleName);
+                    FileUtils.deleteDirectory(new File(temporaryFolderName));
+                }
+            }
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Extracts the module name of the current module by finding the pom.properties
+     * in the folder given by the temporary folder name
+     *
+     * @param temporaryFolderName
+     *            folder name in which the pom.properties file will be searched for
+     *
+     * @return boolean
+     */
+    private String extractModuleName(String temporaryFolderName) throws IOException {
+        Properties prop = new Properties();
+        InputStream input = null;
+        String moduleName = "";
+        File properties = findFile("pom.properties", temporaryFolderName);
+
+        try {
+            input = new FileInputStream(properties);
+            prop.load(input);
+            return moduleName = prop.getProperty("artifactId");
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * This method generates random string
+     *
+     * @return
+     */
+    private String generateRandomString(int randomStringLength){
+        final String charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuffer randomString = new StringBuffer();
+        for(int i=0; i<randomStringLength; i++){
+            int number = getRandomNumber();
+            randomString.append(charList.charAt(number));
+        }
+        return randomString.toString();
+    }
+
+    /**
+     * This method generates random numbers
+     *
+     * @return int
+     */
+    private int getRandomNumber() {
+        final String charList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int randomInt = 0;
+        Random randomGenerator = new Random();
+        randomInt = randomGenerator.nextInt(charList.length());
+        if (randomInt - 1 == -1) {
+            return randomInt;
+        } else {
+            return randomInt - 1;
+        }
+    }
+
+    /**
+     * Copies extracted frontend files by a given source folder name to the destination Folder
+     * given by a destination folder name
+     *
+     * @param sourceFolder
+     *            copies all extracted frontend files
+     * @param destinationFolder
+     *            jarFile that will be checked for frontend files
+     */
+    private void copyFrontEndFiles(String sourceFolder, String destinationFolder) throws IOException {
+        FileUtils.copyDirectory(new File(sourceFolder), new File(destinationFolder));
+    }
+
+    /**
+     * Checks, whether a passed jarFile has frontend files or not. Returns true, when the jar contains
+     * a folder with the name "resources"
+     *
+     * @param jarPath
+     *            jarFile that will be checked for frontend files
+     * @param destinationPath
+     *            jarFile that will be checked for frontend files
+     *
+     */
+    private void extractFrontEndFiles(String jarPath, String destinationPath) throws IOException {
+        File destinationFolder = new File(destinationPath);
+        destinationFolder.mkdir();
+
+        java.util.jar.JarFile jar = new java.util.jar.JarFile(jarPath);
+        java.util.Enumeration enumEntries = jar.entries();
+        while (enumEntries.hasMoreElements()) {
+            java.util.jar.JarEntry file2 = (java.util.jar.JarEntry) enumEntries.nextElement();
+
+            if (file2.getName().contains("resources") || file2.getName().contains("pom.properties")) {
+                java.io.File f = new java.io.File(destinationPath + java.io.File.separator + file2.getName());
+                if (file2.isDirectory()) {
+                    f.mkdirs();
+                    continue;
+                }
+                if (file2.getName().contains("pom.properties")) {
+                    f.getParentFile().mkdirs();
+                }
+
+                java.io.InputStream is = jar.getInputStream(file2);
+                java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+                while (is.available() > 0) {
+                    fos.write(is.read());
+                }
+                fos.close();
+                is.close();
+            }
+        }
+        jar.close();
+    }
+
+    /**
+     * Checks, whether a passed jarFile has frontend files or not. Returns true, when the jar contains
+     * a folder with the name "resources"
+     *
+     * @param jarFile
+     *            jarFile that will be checked for frontend files
+     *
+     * @return boolean
+     */
+    private boolean hasFrontendFiles(JarFile jarFile) {
+        Enumeration enums = jarFile.entries();
+        while (enums.hasMoreElements()) {
+            JarEntry jarEntry = (JarEntry)enums.nextElement();
+            if (jarEntry.getName().contains("resources") && jarEntry.isDirectory()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Tries to find a file by a given file name in a folder by folder name
+     *
+     * @param name
+     *            file name that will be searched for
+     * @param folderName
+     *            folder that will be searched
+     *
+     * @return File, null
+     */
+    public File findFile(String name, String folderName)
+    {
+        Collection<File> s = FileUtils.listFiles(new File(folderName), null, true);
+
+        for (File f:
+                s) {
+            if (f.getName().equals(name))
+                return f;
+        }
+        return null;
     }
 
     /**
