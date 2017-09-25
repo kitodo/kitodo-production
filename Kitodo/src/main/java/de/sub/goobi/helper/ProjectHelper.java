b32b812204093a71836342d137479ecc5e18fc65
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-09-25 13:19:50 +0200

    Replace criteria in ProjectHelper

--- Kitodo/src/main/java/de/sub/goobi/helper/ProjectHelper.java
+++ Kitodo/src/main/java/de/sub/goobi/helper/ProjectHelper.java
@@ -18,16 +18,11 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.goobi.production.flow.statistics.StepInformation;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.ProjectionList;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.kitodo.data.database.beans.Process;
 import org.kitodo.data.database.beans.Project;
 import org.kitodo.data.database.beans.Task;
 import org.kitodo.data.database.helper.enums.TaskStatus;
+import org.kitodo.data.exceptions.DataException;
+import org.kitodo.services.ServiceManager;
 
 public class ProjectHelper {
 
@@ -39,155 +34,87 @@ public class ProjectHelper {
      * @return a GoobiCollection of the following structure: GoobiCollection 1-n
      *         representing the steps each step has the following properties @
      *         stepTitle, stepOrder,
-     *         stepCount,stepImageCount,totalProcessCount,totalImageCount which
-     *         can get extracted by the IGoobiCollection Interface using the
+     *         stepCount,stepImageCount,totalProcessCount,totalImageCount which can
+     *         get extracted by the IGoobiCollection Interface using the
      *         getItem(&lt;name&gt;) method standard workflow of the project
-     *         according to the definition that only steps shared by all
-     *         processes are returned. The workflow order is returned according
-     *         to the average order return by a grouping by step title consider
-     *         workflow structure to be a prototype, it would probably make
-     *         things easier, to either assemble the underlying construction in
-     *         separate classes or to create a new class with these properties
+     *         according to the definition that only steps shared by all processes
+     *         are returned. The workflow order is returned according to the average
+     *         order return by a grouping by step title consider workflow structure
+     *         to be a prototype, it would probably make things easier, to either
+     *         assemble the underlying construction in separate classes or to create
+     *         a new class with these properties
      */
+    public static synchronized List<StepInformation> getProjectWorkFlowOverview(Project project) throws DataException {
+        ServiceManager serviceManager = new ServiceManager();
 
-    @SuppressWarnings("unchecked")
-    public static synchronized List<StepInformation> getProjectWorkFlowOverview(Project project) {
-        Long totalNumberOfProc = 0L;
-        Long totalNumberOfImages = 0L;
+        Long totalNumberOfProcesses = serviceManager.getProcessService()
+                .findNumberOfNotTemplateProcessesForProjectId(project.getId());
+        Double totalNumberOfImages = serviceManager.getProcessService()
+                .findAmountOfImagesForNotTemplatesAndProjectId(project.getId());
 
-        Session session = Helper.getHibernateSession();
-
-        Criteria critTotals = session.createCriteria(Process.class, "proc");
-        critTotals.add(Restrictions.eq("proc.template", Boolean.FALSE));
-        critTotals.add(Restrictions.eq("proc.project", project));
-
-        ProjectionList proList = Projections.projectionList();
-
-        proList.add(Projections.count("proc.id"));
-        proList.add(Projections.sum("proc.sortHelperImages"));
-
-        critTotals.setProjection(proList);
-
-        List<Object> list = critTotals.list();
-
-        for (Object obj : list) {
-            Object[] row = (Object[]) obj;
-
-            totalNumberOfProc = (Long) row[FieldList.totalProcessCount.fieldLocation];
-            totalNumberOfImages = (Long) row[FieldList.totalImageCount.fieldLocation];
-            ;
-        }
-
-        proList = null;
-        list = null;
-
-        Criteria critSteps = session.createCriteria(Task.class);
-
-        critSteps.createCriteria("process", "proc");
-        critSteps.addOrder(Order.asc("ordering"));
-
-        critSteps.add(Restrictions.eq("proc.template", Boolean.FALSE));
-        critSteps.add(Restrictions.eq("proc.project", project));
-
-        proList = Projections.projectionList();
-
-        proList.add(Projections.groupProperty(("title")));
-        proList.add(Projections.count("id"));
-        proList.add(Projections.avg("ordering"));
-
-        critSteps.setProjection(proList);
+        List<Task> tasks = serviceManager.getTaskService()
+                .getTasksForProjectHelper(project.getId());
+        List<Long> tasksSize = serviceManager.getTaskService()
+                .getSizeOfTasksForProjectHelper(project.getId());
+        List<Double> averageTaskOrders = serviceManager.getTaskService()
+                .getAverageOrderingOfTasksForProjectHelper(project.getId());
 
         // now we have to discriminate the hits where the max number of hits
-        // doesn't reach numberOfProcs
-        // and extract a workflow, which is the workflow common for all
-        // processes according to its titel
-        // the position will be calculated by the average of 'reihenfolge' of
-        // steps
-
-        list = critSteps.list();
-
+        // doesn't reach numberOfProcesses and extract a workflow, which is
+        // the workflow common for all processes according to its title
+        // the position will be calculated by the average of 'ordering' of
+        // tasks
         String title;
-        Double averageStepOrder;
-        Long numberOfSteps;
+        Double averageTaskOrder;
+        Long numberOfTasks;
         Long numberOfImages;
 
         List<StepInformation> workFlow = new ArrayList<>();
 
-        for (Object obj : list) {
-            Object[] row = (Object[]) obj;
-
-            title = (String) (row[FieldList.stepName.fieldLocation]);
-            numberOfSteps = (Long) (row[FieldList.stepCount.fieldLocation]);
-            averageStepOrder = (Double) (row[FieldList.stepOrder.fieldLocation]);
+        for (int i = 0; i < tasks.size(); i++) {
+            title = tasks.get(i).getTitle();
+            numberOfTasks = tasksSize.get(i);
+            averageTaskOrder = averageTaskOrders.get(i);
 
             // in this step we only take the steps which are present in each of
             // the workflows
-            if (numberOfSteps.equals(totalNumberOfProc)) {
-                StepInformation newStep = new StepInformation(title, averageStepOrder);
+            if (numberOfTasks.equals(totalNumberOfProcesses)) {
+                StepInformation newStep = new StepInformation(title, averageTaskOrder);
                 newStep.setNumberOfTotalImages(totalNumberOfImages.intValue());
-                newStep.setNumberOfTotalSteps(totalNumberOfProc.intValue());
+                newStep.setNumberOfTotalSteps(totalNumberOfProcesses.intValue());
                 workFlow.add(newStep);
             }
         }
 
-        Criteria critStepDone = session.createCriteria(Task.class, "step");
+        List<Task> doneTasks = serviceManager.getTaskService()
+                .getTasksWithProcessingStatusForProjectHelper(
+                        TaskStatus.DONE.getValue(), project.getId());
+        List<Long> doneTasksSize = serviceManager.getTaskService()
+                .getSizeOfTasksWithProcessingStatusForProjectHelper(
+                        TaskStatus.DONE.getValue(), project.getId());
+        List<Long> amountOfImages = serviceManager.getTaskService()
+                .getAmountOfImagesForTasksWithProcessingStatusForProjectHelper(
+                        TaskStatus.DONE.getValue(), project.getId());
 
-        critStepDone.createCriteria("process", "proc");
-
-        critStepDone.add(Restrictions.eq("step.processingStatus", TaskStatus.DONE.getValue()));
-        critStepDone.add(Restrictions.eq("proc.template", Boolean.FALSE));
-        critStepDone.add(Restrictions.eq("proc.project", project));
-
-        ProjectionList proCount = Projections.projectionList();
-
-        proCount.add(Projections.groupProperty(("step.title")));
-        proCount.add(Projections.count("proc.id"));
-        proCount.add(Projections.sum("proc.sortHelperImages"));
-
-        critStepDone.setProjection(proCount);
-
-        list = critStepDone.list();
-
-        for (Object obj : list) {
-
-            Object[] row = (Object[]) obj;
-
-            title = (String) (row[FieldList.stepName.fieldLocation]);
-            numberOfSteps = (Long) (row[FieldList.stepCount.fieldLocation]);
-            numberOfImages = (Long) (row[FieldList.imageCount.fieldLocation]);
+        for (int i = 0; i < doneTasks.size(); i++) {
+            title = doneTasks.get(i).getTitle();
+            numberOfTasks = doneTasksSize.get(i);
+            numberOfImages = amountOfImages.get(i);
 
             // getting from the workflow collection the collection which
-            // represents step <title>
+            // represents task <title>
             // we only created one for each step holding the counts of processes
             for (StepInformation currentStep : workFlow) {
                 if (currentStep.getTitle().equals(title)) {
-                    currentStep.setNumberOfStepsDone(numberOfSteps.intValue());
+                    currentStep.setNumberOfStepsDone(numberOfTasks.intValue());
                     currentStep.setNumberOfImagesDone(numberOfImages.intValue());
                 }
             }
         }
+
         Comparator<StepInformation> comp = new compareWorkflowSteps();
         Collections.sort(workFlow, comp);
         return workFlow;
-    }
-
-    /*
-     * enum to help addressing the fields of the projections above
-     */
-    private enum FieldList {
-        stepName(0), stepCount(1), stepOrder(2),
-
-        // different projection
-        imageCount(2),
-
-        // different projection
-        totalProcessCount(0), totalImageCount(1);
-
-        Integer fieldLocation;
-
-        FieldList(Integer fieldLocation) {
-            this.fieldLocation = fieldLocation;
-        }
     }
 
     // TODO: move this class to StepInformation
