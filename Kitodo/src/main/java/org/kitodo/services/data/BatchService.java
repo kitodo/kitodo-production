082585a87e4ee5a99285892f559015ae181942b6
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-09-26 15:28:27 +0200

    Update findByProcessTitle in BatchService

--- Kitodo/src/main/java/org/kitodo/services/data/BatchService.java
+++ Kitodo/src/main/java/org/kitodo/services/data/BatchService.java
@@ -15,9 +15,7 @@ import de.sub.goobi.helper.Helper;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -159,13 +157,8 @@ public class BatchService extends TitleSearchService<Batch, BatchDTO, BatchDAO> 
      * @return list of JSON objects with batches for specific process title
      */
     public List<JSONObject> findByProcessTitle(String title) throws DataException {
-        Set<Integer> processIds = new HashSet<>();
-
-        List<JSONObject> processes = serviceManager.getProcessService().findByTitle(title, true);
-        for (JSONObject process : processes) {
-            processIds.add(getIdFromJSONObject(process));
-        }
-        return searcher.findDocuments(createSetQuery("processes.id", processIds, true).toString());
+        QueryBuilder query = createSimpleQuery("processes.title", title, true, Operator.AND);
+        return searcher.findDocuments(query.toString());
     }
 
     @Override
