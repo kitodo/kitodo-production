6924e7f2c6b078a3b272920e1d5bc0d363102234
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2017-10-23 16:03:52 +0200

    Add empty validation classes

---
+++ Kitodo-Validation/src/main/java/org/kitodo/validation/filestructure/FileStructureValidation.java
@@ -0,0 +1,26 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.validation.filestructure;
+
+import java.net.URI;
+
+import org.kitodo.api.validation.ValidationResult;
+import org.kitodo.api.validation.filestructure.FileStructureValidationInterface;
+
+public class FileStructureValidation implements FileStructureValidationInterface {
+
+    @Override
+    public ValidationResult validate(URI xmlFileUri, URI xsdFileUri) {
+        return null;
+    }
+
+}
