d06e3b566dbf9d43a137328775e9884dd43f427c
Arved Solth <arved.solth@uni-hamburg.de>
2017-10-23 17:05:46 +0200

    Add test for LazyDTOModel

---
+++ Kitodo/src/test/java/de/sub/goobi/model/LazyDTOModelTest.java
@@ -0,0 +1,49 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package de.sub.goobi.model;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.kitodo.MockDatabase;
+import org.kitodo.dto.UserDTO;
+import org.kitodo.services.ServiceManager;
+import org.primefaces.model.SortOrder;
+
+public class LazyDTOModelTest {
+
+    private ServiceManager serviceManager = new ServiceManager();
+    private LazyDTOModel lazyDTOModel = new LazyDTOModel(serviceManager.getUserService());
+
+    @Before
+    public void setUp() throws Exception {
+        MockDatabase.startNode();
+        MockDatabase.insertUserGroupsFull();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        MockDatabase.stopNode();
+        MockDatabase.cleanDatabase();
+    }
+
+    @Test
+    public void shouldLoad() throws Exception {
+        List users = lazyDTOModel.load(0, 2, "login", SortOrder.ASCENDING, null);
+        UserDTO user = (UserDTO) users.get(0);
+        Assert.assertEquals(user.getLogin(), "dora");
+    }
+
+}
