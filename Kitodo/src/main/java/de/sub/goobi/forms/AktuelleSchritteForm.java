d6787a3f0ffa434c52e5c7ea1d1b59b5bbe75a4c
Oliver Stöhr <stoehr@effective-webwork.de>
2017-11-17 14:37:14 +0100

    created tasks page

--- Kitodo/src/main/java/de/sub/goobi/forms/AktuelleSchritteForm.java
+++ Kitodo/src/main/java/de/sub/goobi/forms/AktuelleSchritteForm.java
@@ -73,6 +73,7 @@ import org.kitodo.data.database.helper.enums.TaskEditType;
 import org.kitodo.data.database.helper.enums.TaskStatus;
 import org.kitodo.data.exceptions.DataException;
 import org.kitodo.dto.TaskDTO;
+import org.kitodo.model.LazyDTOModel;
 import org.kitodo.services.ServiceManager;
 
 @Named("AktuelleSchritteForm")
@@ -113,6 +114,8 @@ public class AktuelleSchritteForm extends BasisForm {
      * Constructor.
      */
     public AktuelleSchritteForm() {
+        super();
+        super.setLazyDTOModel(new LazyDTOModel(serviceManager.getTaskService()));
         this.anzeigeAnpassen = new HashMap<>();
         this.anzeigeAnpassen.put("lockings", false);
         this.anzeigeAnpassen.put("selectionBoxes", false);
