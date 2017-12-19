f1addf6ebdc2d7f2c59f9816a810269272798f07
Christopher Timm <timm@effective-webwork.de>
2017-12-19 15:58:33 +0100

    remove unused import

--- Kitodo/src/main/java/de/sub/goobi/forms/HelperForm.java
+++ Kitodo/src/main/java/de/sub/goobi/forms/HelperForm.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import javax.inject.Named;
