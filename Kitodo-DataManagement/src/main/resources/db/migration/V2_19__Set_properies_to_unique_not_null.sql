bc4805ef702861b13200fdf4d93402c43b7b0dc0
Tim Boerner <tim.boerner@slub-dresden.de>
2018-01-04 14:23:25 +0100

    added comments to script

--- Kitodo-DataManagement/src/main/resources/db/migration/V2_19__Set_properies_to_unique_not_null.sql
+++ Kitodo-DataManagement/src/main/resources/db/migration/V2_19__Set_properies_to_unique_not_null.sql
@@ -9,7 +9,14 @@
 -- GPL3-License.txt file that was distributed with this source code.
 --
 
+--
+-- Migration: Changing the columns for authorization title, userGroup title and project title to not null.
+--            Changing the columns for user login, authorization title, userGroup title and project title to unique.
+--            The setting unique query will not work on mysql versions below 5.7.7
+--
+
 ALTER TABLE user ADD UNIQUE (login);
+-- user login is not set to NOT NUll because the NULL state is used at self destruct method
 
 ALTER TABLE authorization ADD UNIQUE (title);
 ALTER TABLE authorization MODIFY COLUMN title VARCHAR(255) NOT NULL;
