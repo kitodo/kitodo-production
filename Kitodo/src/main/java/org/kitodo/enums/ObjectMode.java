43764cfa748e54a9da33edcb7059de5c65025c53
Beatrycze Kmiec <beatrycze.kmiec@slub-dresden.de>
2018-01-05 14:58:45 +0100

    Add ObjectMode enum for possible display/edit modes in forms

---
+++ Kitodo/src/main/java/org/kitodo/enums/ObjectMode.java
@@ -0,0 +1,34 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.enums;
+
+public enum ObjectMode {
+    PROCESS("process"),
+    PROCESSPROPERTY("processProperty"),
+    PROJECT("project"),
+    TASK("task"),
+    TEMPLATE("template"),
+    TEMPLATEPROPERTY("templateProperty"),
+    WORKPIECE("workpiece"),
+    WORKPIECEPROPERTY("workpieceProperty"),
+    NONE("");
+
+    private String mode;
+
+    ObjectMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+}
