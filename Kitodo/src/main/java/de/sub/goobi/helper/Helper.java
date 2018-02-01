ba2a60b9083995bdb4ac901ff6243cbc89327073
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-02-01 13:45:10 +0100

    Add method to Helper to access metadata language of current user

--- Kitodo/src/main/java/de/sub/goobi/helper/Helper.java
+++ Kitodo/src/main/java/de/sub/goobi/helper/Helper.java
@@ -411,6 +411,10 @@ public class Helper extends HibernateHelper implements Observer {
         return login != null ? login.getMyBenutzer() : null;
     }
 
+    public static String getMetadataLanguageForCurrentUser() {
+        return (String) Helper.getManagedBeanValue("#{LoginForm.myBenutzer.metadataLanguage}");
+    }
+
     public static final FilenameFilter imageNameFilter = new FilenameFilter() {
         @Override
         public boolean accept(File dir, String name) {
