02f9f56513f48d8363384a7f8db6707a9262130f
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-04-23 14:20:40 +0200

    Add integration test for workflow - template conversion

--- Kitodo/src/test/java/org/kitodo/workflow/ReaderTest.java
+++ Kitodo/src/test/java/org/kitodo/workflow/ReaderTest.java
@@ -2,8 +2,6 @@ package org.kitodo.workflow;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 
 import org.junit.AfterClass;
@@ -15,7 +13,7 @@ import org.kitodo.data.database.beans.Template;
 import org.kitodo.services.ServiceManager;
 import org.kitodo.services.file.FileService;
 import org.kitodo.workflow.model.Reader;
-import org.kitodo.workflow.model.beans.Workflow;
+import org.kitodo.workflow.model.beans.Diagram;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -30,13 +28,11 @@ public class ReaderTest {
         fileService.createDirectory(URI.create(""), "diagrams");
 
         FileLoader.createExtendedDiagramTestFile();
-        FileLoader.createExtendedGatewayDiagramTestFile();
     }
 
     @AfterClass
     public static void tearDown() throws IOException {
         FileLoader.deleteExtendedDiagramTestFile();
-        FileLoader.deleteExtendedGatewayDiagramTestFile();
 
         fileService.delete(URI.create("diagrams"));
     }
@@ -54,7 +50,7 @@ public class ReaderTest {
     public void shouldGetWorkflow() throws Exception {
         Reader reader = new Reader("test");
         reader.loadProcess();
-        Workflow workflow = reader.getWorkflow();
+        Diagram workflow = reader.getWorkflow();
 
         assertEquals("Process definition - workflow was read incorrectly!", workflow.getTitle(), "say-hello");
         assertEquals("Process definition - workflow was read incorrectly!", workflow.getOutputName(), "Say Hello");
@@ -79,33 +75,5 @@ public class ReaderTest {
         assertEquals("Process definition - workflow's task was read incorrectly!", task.getScriptName(), "Test script");
         assertNull("Process definition - workflow's task was read incorrectly!", task.getScriptPath());
         assertEquals("Process definition - workflow's task was read incorrectly!", task.getOrdering().intValue(), 2);
-    }
-
-    @Test
-    public void shouldConvertConditionalWorkflowToTemplate() throws Exception {
-        Reader reader = new Reader("gateway");
-        reader.loadProcess();
-
-        Template template = reader.convertWorkflowToTemplate();
-        assertEquals("Process definition - workflow was read incorrectly!", 5, template.getTasks().size());
-
-        List<Task> tasks = template.getTasks();
-        tasks.sort(Comparator.comparing(Task::getOrdering, Comparator.nullsFirst(Comparator.naturalOrder())));
-
-        for (Task task : tasks) {
-            System.out.println(task.getOrdering() + " " + task.getTitle() + " " + task.getWorkflowCondition());
-        }
-
-        assertEquals("Process definition - workflow's task ordering was determined incorrectly!", 1,
-            tasks.get(0).getOrdering().intValue());
-        assertEquals("Process definition - workflow's task conditions were determined incorrectly!", "default",
-            tasks.get(0).getWorkflowCondition());
-
-        assertEquals("Process definition - workflow's task ordering was determined incorrectly!", 5,
-            tasks.get(4).getOrdering().intValue());
-        assertTrue("Process definition - workflow's task conditions were determined incorrectly!",
-            tasks.get(4).getWorkflowCondition().contains("default")
-                    && tasks.get(4).getWorkflowCondition().contains("${type == 1}")
-                    && tasks.get(4).getWorkflowCondition().contains("${type == 2}"));
     }
 }
