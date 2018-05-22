75d72c1f3cf0d6da593b8cb71a276bfdd498aaee
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-05-22 13:55:34 +0200

    Modfiy workflow table - add flags archived/ready

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/beans/Workflow.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/beans/Workflow.java
@@ -11,6 +11,8 @@
 
 package org.kitodo.data.database.beans;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -25,6 +27,18 @@ public class Workflow extends BaseBean {
 
     @Column(name = "fileName")
     private String fileName;
+
+    @Column(name = "archived")
+    private Boolean archived = false;
+
+    @Column(name = "ready")
+    private Boolean ready = false;
+
+    /**
+     * Empty constructor.
+     */
+    public Workflow() {
+    }
 
     /**
      * Public constructor.
@@ -75,5 +89,47 @@ public class Workflow extends BaseBean {
      */
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    /**
+     * Check if workflow is archived.
+     *
+     * @return true or false
+     */
+    public Boolean isArchived() {
+        if (Objects.isNull(this.archived)) {
+            this.archived = false;
+        }
+        return this.archived;
+    }
+
+    /**
+     * Set workflow as archived.
+     *
+     * @param archived as Boolean
+     */
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    /**
+     * Check if workflow is ready to use.
+     *
+     * @return true or false
+     */
+    public Boolean isReady() {
+        if (Objects.isNull(this.ready)) {
+            this.ready = false;
+        }
+        return this.ready;
+    }
+
+    /**
+     * Set workflow as ready to use.
+     *
+     * @param ready as Boolean
+     */
+    public void setReady(Boolean ready) {
+        this.ready = ready;
     }
 }
