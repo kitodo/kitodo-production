a4d752e9faf4eaf62a829573bf95ea7f96edbf3d
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-05-22 12:45:17 +0200

    Remove unused importas / exception throws

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/persistence/HibernateSessionLong.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/persistence/HibernateSessionLong.java
@@ -18,7 +18,6 @@ import javax.inject.Named;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
