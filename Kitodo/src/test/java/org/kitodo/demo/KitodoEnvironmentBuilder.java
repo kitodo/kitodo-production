a491f034ea75d5d8dae064152729080e86e716e2
Tim Boerner <tim.boerner@slub-dresden.de>
2018-05-28 10:42:21 +0200

    renamed start up class

---
+++ Kitodo/src/test/java/org/kitodo/demo/KitodoEnvironmentBuilder.java
@@ -0,0 +1,27 @@
+package org.kitodo.demo;
+
+import org.kitodo.MockDatabase;
+
+public class KitodoEnvironmentBuilder {
+
+    /**
+     * Sets up in-memory elastic search and database server and inserts some demo data.
+     */
+    public static void setUpEnvironment() {
+        try {
+            System.out.println("Starting ElasticSearch server ...");
+            MockDatabase.startNode();
+            System.out.println("Starting Database server ...");
+            MockDatabase.insertProcessesFull();
+            MockDatabase.startDatabaseServer();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+	public static void main(String[] args) {
+        setUpEnvironment();
+        System.out.println("Kitodo is running now. You can try access the application by the URL: http://localhost:8080/kitodo/pages/login.jsf");
+        System.out.println("You can stop the application by pressing Ctrl + c");
+	}
+}
