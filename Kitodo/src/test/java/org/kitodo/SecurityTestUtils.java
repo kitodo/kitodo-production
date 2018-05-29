6e6692c6ca426422600d0aa082328270a5ad261b
Tim Boerner <tim.boerner@slub-dresden.de>
2018-05-29 16:48:05 +0200

    added SecurityTestUtils and tests for SecurityAccessService

---
+++ Kitodo/src/test/java/org/kitodo/SecurityTestUtils.java
@@ -0,0 +1,34 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo;
+
+import org.kitodo.data.database.beans.User;
+import org.kitodo.security.SecurityUserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityTestUtils {
+
+    /**
+     * Adds data and authorities of a given user to security context. It is not a
+     * real authentication and can not be used for front end testing.
+     * 
+     * @param user
+     *            the user object.
+     */
+    public static void addUserDataToSecurityContext(User user) {
+        SecurityUserDetails securityUserDetails = new SecurityUserDetails(user);
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, securityUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+}
---
+++ Kitodo/src/test/java/org/kitodo/services/security/SecurityAccessServiceIT.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.services.security;
+
+import java.util.Collection;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kitodo.MockDatabase;
+import org.kitodo.SecurityTestUtils;
+import org.kitodo.services.ServiceManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityAccessServiceIT {
+
+    private static ServiceManager serviceManager = new ServiceManager();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        MockDatabase.startNode();
+        MockDatabase.insertUserGroupsFull();
+        SecurityTestUtils.addUserDataToSecurityContext(serviceManager.getUserService().getById(1));
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        MockDatabase.stopNode();
+        MockDatabase.cleanDatabase();
+    }
+
+    @Test
+    public void shouldGetAuthorities() {
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication()
+                .getAuthorities();
+        Assert.assertEquals("Security context holder does not hold the corresponding authorities", 36,
+            authorities.size());
+    }
+
+    @Test
+    public void isAdminTest() {
+        Assert.assertTrue("Authenticated user was not identified as admin",
+            serviceManager.getSecurityAccessService().isAdmin());
+    }
+
+    @Test
+    public void hasAuthorityTest() {
+        Assert.assertTrue("The authority \"editClient\" was not found for authenticated user",
+            serviceManager.getSecurityAccessService().hasAuthorityGlobal("editClient"));
+    }
+}
