bafd10dfeef7ddc4c5093a2c65143ffaa871f900
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-06-01 12:54:29 +0200

    Remove not used methods from Helper and Sprache forms

--- Kitodo/src/main/java/de/sub/goobi/forms/HelperForm.java
+++ Kitodo/src/main/java/de/sub/goobi/forms/HelperForm.java
@@ -12,7 +12,6 @@
 package de.sub.goobi.forms;
 
 import de.sub.goobi.config.ConfigCore;
-import de.sub.goobi.helper.Helper;
 
 import java.io.FilenameFilter;
 import java.io.Serializable;
@@ -21,13 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletRequest;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.goobi.production.GoobiVersion;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.PluginLoader;
@@ -47,81 +42,11 @@ import org.kitodo.services.ServiceManager;
 public class HelperForm implements Serializable {
     private static final long serialVersionUID = -5872893771807845586L;
     private transient ServiceManager serviceManager = new ServiceManager();
-    private static final Logger logger = LogManager.getLogger(HelperForm.class);
-    static final String MAIN_JSF_PATH = "/pages";
-    private static final String IMAGE_PATH = "/pages/images";
     private static final String CSS_BASE_PATH = "/WEB-INF/resources/css";
     private static final String CSS_PATH = "/userStyles";
 
     public String getVersion() {
         return GoobiVersion.getBuildversion();
-    }
-
-    /**
-     * Get application logo.
-     *
-     * @return link to logo
-     */
-    public String getApplicationLogo() {
-        String logo = getServletPathWithHostAsUrl() + IMAGE_PATH + "/template/";
-        logo += ConfigCore.getParameter("ApplicationLogo", "kitodo-header-logo.svg");
-        return logo;
-    }
-
-    /**
-     * Get application header background.
-     *
-     * @return css style for header
-     */
-    public String getApplicationHeaderBackground() {
-        String headerImage = getServletPathWithHostAsUrl() + IMAGE_PATH + "/template/";
-        headerImage += ConfigCore.getParameter("ApplicationHeaderBackground", "goobi_meta_verlauf.jpg");
-        /* wenn ein Background angegeben wurde, dann diesen jetzt strecken */
-        if (headerImage.length() > 0) {
-            headerImage = "background: url(" + headerImage + ") repeat-x;";
-        }
-        return headerImage;
-    }
-
-    // TODO: Change the defaults
-    public String getApplicationHeaderTitle() {
-        return ConfigCore.getParameter("ApplicationHeaderTitle", "Goobi - Universitätsbibliothek Göttingen");
-    }
-
-    public String getApplicationTitle() {
-        return ConfigCore.getParameter("ApplicationTitle", "http://goobi.gdz.uni-goettingen.de");
-    }
-
-    public String getApplicationTitleStyle() {
-        return ConfigCore.getParameter("ApplicationTitleStyle", "font-size:17; font-family:verdana; color: black;");
-    }
-
-    public String getApplicationWebsiteUrl() {
-        return getServletPathAsUrl();
-    }
-
-    public String getApplicationWebsiteMsg() {
-        String result = ConfigCore.getParameter("ApplicationWebsiteMsg", getApplicationWebsiteUrl());
-        return Helper.getTranslation(result);
-    }
-
-    public String getApplicationHomepageMsg() {
-        String result = ConfigCore.getParameter("ApplicationHomepageMsg", getApplicationWebsiteUrl());
-        return Helper.getTranslation(result);
-    }
-
-    public String getApplicationTechnicalBackgroundMsg() {
-        String result = ConfigCore.getParameter("ApplicationTechnicalBackgroundMsg", getApplicationWebsiteUrl());
-        return Helper.getTranslation(result);
-    }
-
-    public String getApplicationImpressumMsg() {
-        String result = ConfigCore.getParameter("ApplicationImpressumMsg", getApplicationWebsiteUrl());
-        return Helper.getTranslation(result);
-    }
-
-    public String getApplicationIndividualHeader() {
-        return ConfigCore.getParameter("ApplicationIndividualHeader", "");
     }
 
     public boolean getAnonymized() {
@@ -187,31 +112,6 @@ public class HelperForm implements Serializable {
     }
 
     /**
-     * Get servlet path.
-     *
-     * @return servlet path as String
-     */
-    public String getServletPathAsUrl() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        return context.getExternalContext().getRequestContextPath() + "/";
-    }
-
-    /**
-     * Get servlet path with host.
-     *
-     * @return servlet path with host as String
-     */
-    public String getServletPathWithHostAsUrl() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        String scheme = request.getScheme(); // http
-        String serverName = request.getServerName(); // hostname.com
-        int serverPort = request.getServerPort(); // 80
-        String contextPath = request.getContextPath(); // /mywebapp
-        return scheme + "://" + serverName + ":" + serverPort + contextPath;
-    }
-
-    /**
      * Get all css files from root folder.
      *
      * @return list of css files
@@ -224,30 +124,6 @@ public class HelperForm implements Serializable {
             list.add(new SelectItem(uri.toString(), uri.toString()));
         }
         return list;
-    }
-
-    /**
-     * Method returns a valid css file, which is the suggestion unless suggestion is
-     * not available if not available default.css is returned.
-     *
-     * @param cssFileName
-     *            suggested css file
-     *
-     * @return valid css file
-     */
-    public String getCssLinkIfExists(String cssFileName) {
-        FilenameFilter filter = new FileNameEndsWithFilter(".css");
-        List<URI> uris = serviceManager.getFileService().getSubUris(filter, URI.create(CSS_BASE_PATH + CSS_PATH));
-        for (URI uri : uris) {
-            if (uri.toString().equals(cssFileName)) {
-                return cssFileName;
-            }
-        }
-        return CSS_PATH + "/default.css";
-    }
-
-    public String getLogoUrl() {
-        return getServletPathWithHostAsUrl() + "/pages/images/template/kitodo-homepage-logo.svg";
     }
 
     /**
