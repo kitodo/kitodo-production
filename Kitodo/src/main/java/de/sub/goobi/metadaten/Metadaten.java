d74a9b71e9dce682be3894277f92d42861c7620c
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-06-19 10:03:02 +0200

    Use SortType enum for HelperComparator

--- Kitodo/src/main/java/de/sub/goobi/metadaten/Metadaten.java
+++ Kitodo/src/main/java/de/sub/goobi/metadaten/Metadaten.java
@@ -83,6 +83,7 @@ import org.kitodo.data.database.beans.Process;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.exceptions.DataException;
 import org.kitodo.enums.PositionOfNewDocStrucElement;
+import org.kitodo.enums.SortType;
 import org.kitodo.legacy.UghImplementation;
 import org.kitodo.services.ServiceManager;
 import org.kitodo.services.file.FileService;
@@ -518,7 +519,7 @@ public class Metadaten {
 
         // sort the metadata types
         HelperComparator c = new HelperComparator();
-        c.setSortType("MetadatenTypen");
+        c.setSortType(SortType.METADATA_TYPE);
         Collections.sort(types, c);
 
         int counter = types.size();
@@ -555,7 +556,7 @@ public class Metadaten {
 
         // sort the metadata types
         HelperComparator c = new HelperComparator();
-        c.setSortType("MetadatenTypen");
+        c.setSortType(SortType.METADATA_TYPE);
         Collections.sort(types, c);
 
         /*
