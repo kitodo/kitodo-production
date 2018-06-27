560451ec3b3f00872acd40f0a3c6cadd87e8fbfb
Oliver Stöhr <stoehr@effective-webwork.de>
2018-06-27 11:15:00 +0200

    Fix save button not activating when input changes

--- Kitodo/src/main/webapp/WEB-INF/resources/js/save.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/save.js
@@ -10,18 +10,21 @@
  */
 
 function checkForm(form) {
-    window.inputs;
+    var inputs;
     window.values = [];
 
     inputs = document.querySelectorAll('#' + form + ' .ui-inputtext');
+    $('#' + form).on('input', '.ui-inputtext', function () {compareForm(form)});
     inputs.forEach(function (element) {
         values.push(element.value);
-        element.addEventListener('input', function () {compareForm(form)});
     });
 }
 
 function compareForm(form) {
+    var inputs;
     var currentValues = [];
+
+    inputs = document.querySelectorAll('#' + form + ' .ui-inputtext');
 
     inputs.forEach(function (element) {
         currentValues.push(element.value);
