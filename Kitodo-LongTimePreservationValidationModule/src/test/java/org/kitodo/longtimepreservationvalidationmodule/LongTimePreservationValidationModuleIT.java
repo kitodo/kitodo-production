437faee08303de3b7606d7831b35586e2c92af25
Matthias Ronge <matthias.ronge@zeutschel.de>
2018-06-28 09:03:32 +0200

    Improve the tests, add test on corrupted TIFF file

--- Kitodo-LongTimePreservationValidationModule/src/test/java/org/kitodo/longtimepreservationvalidationmodule/LongTimePreservationValidationModuleIT.java
+++ Kitodo-LongTimePreservationValidationModule/src/test/java/org/kitodo/longtimepreservationvalidationmodule/LongTimePreservationValidationModuleIT.java
@@ -25,74 +25,53 @@ import org.kitodo.api.validation.longtimepreservation.FileType;
 import org.kitodo.api.validation.longtimepreservation.LongTimePreservationValidationInterface;
 
 public class LongTimePreservationValidationModuleIT {
-    private static final URI GIF_URI = uriOf("src/test/resources/rose.gif");
-    private static final URI JAVA_URI = uriOf(
-        "src/test/java/org/kitodo/longtimepreservationvalidationmodule/LongTimePreservationValidationModuleIT.java");
-    private static final URI JP2_URI = uriOf("src/test/resources/rose.jp2");
-    private static final URI JPG_URI = uriOf("src/test/resources/rose.jpg");
-    private static final URI PDF_URI = uriOf("src/test/resources/rose.pdf");
-    private static final URI PNG_URI = uriOf("src/test/resources/rose.png");
-    private static final URI TIF_URI = uriOf("src/test/resources/rose.tif");
-
-    @Test
-    public void testValidate() {
-        LongTimePreservationValidationInterface validation = new LongTimePreservationValidationModule();
-
-        assertThat(validation.validate(GIF_URI, FileType.GIF).getState(), is(equalTo(State.SUCCESS)));
-        assertThat(validation.validate(JAVA_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JPG_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PDF_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PNG_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(TIF_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
-
-        assertThat(validation.validate(GIF_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JAVA_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JPG_URI, FileType.JPEG).getState(), is(equalTo(State.SUCCESS)));
-        assertThat(validation.validate(PDF_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PNG_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(TIF_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
-
-        assertThat(validation.validate(GIF_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JAVA_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.JPEG_2000).getState(), is(equalTo(State.SUCCESS)));
-        assertThat(validation.validate(JPG_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PDF_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PNG_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(TIF_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
-
-        assertThat(validation.validate(GIF_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JAVA_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JPG_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PDF_URI, FileType.PDF).getState(), is(equalTo(State.SUCCESS)));
-        assertThat(validation.validate(PNG_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(TIF_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
-
-        assertThat(validation.validate(GIF_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JAVA_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JPG_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PDF_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PNG_URI, FileType.PNG).getState(), is(equalTo(State.SUCCESS)));
-        assertThat(validation.validate(TIF_URI, FileType.PNG).getState(), is(equalTo(State.ERROR)));
-
-        assertThat(validation.validate(GIF_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JAVA_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JP2_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(JPG_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PDF_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(PNG_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
-        assertThat(validation.validate(TIF_URI, FileType.TIFF).getState(), is(equalTo(State.SUCCESS)));
-    }
-
-    private static URI uriOf(String uri) {
+    private static URI CORRUPTED_TIF_URI, GIF_URI, JAVA_URI, JP2_URI, JPG_URI, PDF_URI, PNG_URI, TIF_URI;
+    {
         try {
-            return new URI(uri);
+            CORRUPTED_TIF_URI = new URI("src/test/resources/corrupted.tif");
+            GIF_URI = new URI("src/test/resources/rose.gif");
+            JAVA_URI = new URI(
+                    "src/test/java/org/kitodo/longtimepreservationvalidationmodule/LongTimePreservationValidationModuleIT.java");
+            JP2_URI = new URI("src/test/resources/rose.jp2");
+            JPG_URI = new URI("src/test/resources/rose.jpg");
+            PDF_URI = new URI("src/test/resources/rose.pdf");
+            PNG_URI = new URI("src/test/resources/rose.png");
+            TIF_URI = new URI("src/test/resources/rose.tif");
         } catch (URISyntaxException e) {
             throw new UndeclaredThrowableException(e);
         }
+    }
+
+    @Test
+    public void testThatValidFilesValidateWithDefaultModules() {
+        LongTimePreservationValidationInterface validation = new LongTimePreservationValidationModule();
+        assertThat(validation.validate(GIF_URI, FileType.GIF).getState(), is(equalTo(State.SUCCESS)));
+        assertThat(validation.validate(JP2_URI, FileType.JPEG_2000).getState(), is(equalTo(State.SUCCESS)));
+        assertThat(validation.validate(JPG_URI, FileType.JPEG).getState(), is(equalTo(State.SUCCESS)));
+        assertThat(validation.validate(PDF_URI, FileType.PDF).getState(), is(equalTo(State.SUCCESS)));
+        assertThat(validation.validate(TIF_URI, FileType.TIFF).getState(), is(equalTo(State.SUCCESS)));
+    }
+
+    @Test
+    public void testThatValidFilesValidateWithExtendedModules() {
+        LongTimePreservationValidationInterface validation = new LongTimePreservationValidationModule();
+        assertThat(validation.validate(PNG_URI, FileType.PNG).getState(), is(equalTo(State.SUCCESS)));
+    }
+
+    @Test
+    public void testThatACorruptedFileDoesNotValidate() {
+        LongTimePreservationValidationInterface validation = new LongTimePreservationValidationModule();
+        assertThat(validation.validate(CORRUPTED_TIF_URI, FileType.TIFF).getState(), is(equalTo(State.ERROR)));
+    }
+
+    @Test
+    public void testThatFilesOfTheWrongTypeDoNotValidate() {
+        LongTimePreservationValidationInterface validation = new LongTimePreservationValidationModule();
+        assertThat(validation.validate(JAVA_URI, FileType.PDF).getState(), is(equalTo(State.ERROR)));
+        assertThat(validation.validate(PDF_URI, FileType.GIF).getState(), is(equalTo(State.ERROR)));
+        assertThat(validation.validate(JP2_URI, FileType.JPEG).getState(), is(equalTo(State.ERROR)));
+        assertThat(validation.validate(PNG_URI, FileType.JPEG_2000).getState(), is(equalTo(State.ERROR)));
+
     }
 
 }
