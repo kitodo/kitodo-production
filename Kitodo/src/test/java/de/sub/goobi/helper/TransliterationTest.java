f34f99db7d6e03b4897ecb50b2233d4125fc980b
Beatrycze Kmiec <beatrycze.volk@slub-dresden.de>
2018-06-28 10:17:54 +0200

    Adjustments to test after library introduction

--- Kitodo/src/test/java/de/sub/goobi/helper/TransliterationTest.java
+++ Kitodo/src/test/java/de/sub/goobi/helper/TransliterationTest.java
@@ -23,7 +23,7 @@ public class TransliterationTest {
     public void shouldTransliterateISO() {
         String input = "Лорем ипсум долор сит амет, путент персиус фацилиси меи те, еам лорем нихил цоммуне ат, дицо аццусамус витуператорибус ет нец. Но пер виде магна омнес, дуо ат аперири витуперата. Дуо путент малорум яуаеяуе ет.";
         String transliterated = transliteration.transliterateISO(input);
-        String expected = "Lorem ipsum dolor sit amet, putent persius facilisi mei te, eam lorem nihil commune at, dico accusamus vituperatoribus et nec. No per vide magna omnes, duo at aperiri vituperata. Duo putent malorum ǎuaeǎue et.";
+        String expected = "Lorem ipsum dolor sit amet, putent persius facilisi mei te, eam lorem nihil commune at, dico accusamus vituperatoribus et nec. No per vide magna omnes, duo at aperiri vituperata. Duo putent malorum âuaeâue et.";
 
         assertEquals("String was transliterated incorrectly!", expected, transliterated);
     }
@@ -32,7 +32,7 @@ public class TransliterationTest {
     public void shouldTransliterateISOAlphabet() {
         String input = "А а Б б В в Г г Д д Е е Ж ж З з И и Й й К к Л л М м Н н О о П п Р р С с Т т У у Ф ф Х х Ц ц Ч ч Ш ш Щ щ Ь ь Ю ю Я я";
         String transliterated = transliteration.transliterateISO(input);
-        String  expected = "A a B b V v G g D d E e Ž ž Z z I i J j K k L l M m N n O o P p R r S s T t U u F f H h C c Č č Š š Ŝ ŝ Ь ' Ǔ ǔ Ǎ ǎ";
+        String  expected = "A a B b V v G g D d E e Ž ž Z z I i J j K k L l M m N n O o P p R r S s T t U u F f H h C c Č č Š š Ŝ ŝ ʹ̱ ʹ Û û Â â";
 
         assertEquals("String was transliterated incorrectly!", expected, transliterated);
     }
@@ -50,7 +50,7 @@ public class TransliterationTest {
     public void shouldTransliterateDINAlphabet() {
         String input = "А а Б б В в Г г Д д Е е Ж ж З з И и Й й К к Л л М м Н н О о П п Р р С с Т т У у Ф ф Х х Ц ц Ч ч Ш ш Щ щ Ь ь Ю ю Я я";
         String transliterated = transliteration.transliterateDIN(input);
-        String expected = "A a B b V v G g D d E e Ž ž Z z I i J j K k L l M m N n O o P p R r S s T t U u F f Ch ch C c Č č Š š Šč šč Ь ' Ju ju Ja ja";
+        String expected = "A a B b V v G g D d E e Ž ž Z z I i J j K k L l M m N n O o P p R r S s T t U u F f Ch ch C c Č č Š š Šč šč ʹ̱ ʹ Ju ju Ja ja";
 
         assertEquals("String was transliterated incorrectly!", expected, transliterated);
     }
