86c31fd9684373677fd7cacab4924a2717e45191
Arved Solth <solth@effective-webwork.de>
2018-07-17 14:10:17 +0200

    Make desktop widget tables scrollable

--- Kitodo/src/main/webapp/WEB-INF/resources/js/desktop.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/desktop.js
@@ -9,6 +9,25 @@
  * GPL3-License.txt file that was distributed with this source code.
  */
 
-$("#desktopGrid").ready(function() {
+$(window).on("load resize", function() {
     $("main").height(window.innerHeight - ($("footer").outerHeight(true) + $("#breadcrumb").outerHeight(true) + $("header").outerHeight(true)));
+
+    // update widget heights
+    const widgetTables = $("#taskTable, #processTable, #projectTable, #statisticsTable");
+    const widgetHeight = widgetTables.parent().outerHeight();
+    const headerHeight = widgetTables.prev(".content-header").outerHeight();
+    widgetTables.height(widgetHeight - headerHeight);
+
+    const tableHeaders = $("#desktopGrid .ui-datatable-scrollable-header");
+    const tableBodies = $("#desktopGrid .ui-datatable-scrollable-body");
+    const actionCells = tableHeaders.find("th:last");
+
+    // add right padding to header for scrollbars
+    const scrollBarWidth = tableBodies.width() - tableBodies.find("table[role='grid']").width();
+    actionCells.css("padding-right", parseInt(actionCells.css("padding-right")) + scrollBarWidth + "px");
+
+    // update table heights
+    const tableHeight = tableBodies.parent().outerHeight();
+    const tableHeaderHeight = tableBodies.prev(".ui-widget-header").outerHeight();
+    tableBodies.height(tableHeight - tableHeaderHeight);
 });
