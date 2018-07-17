5e5233a2786922acbefc956c82f1c7aa58ff3afb
Matthias Ronge <matthias.ronge@zeutschel.de>
2018-07-17 17:49:43 +0200

    Add reason messages to FileFormatsConfigIT

--- Kitodo/src/test/java/org/kitodo/config/xml/fileformats/FileFormatsConfigIT.java
+++ Kitodo/src/test/java/org/kitodo/config/xml/fileformats/FileFormatsConfigIT.java
@@ -15,12 +15,17 @@ import org.kitodo.api.imagemanagement.ImageFileFormat;
 public class FileFormatsConfigIT {
     @Test
     public void testFileFormatsConfig() throws JAXBException, IOException {
-        assertThat(FileFormatsConfig.getFileFormats().size(), is(equalTo(8)));
+        assertThat("kitodo_fileFormats.xml does not contain exactly 8 entries",
+            FileFormatsConfig.getFileFormats().size(), is(equalTo(8)));
         FileFormat tiff = FileFormatsConfig.getFileFormat("image/tiff").get();
-        assertThat(tiff.getLabel(), is(equalTo("Tagged Image File Format (image/tiff, *.tif)")));
-        assertThat(tiff.getLabel(Locale.LanguageRange.parse("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")),
+        assertThat("Wrong label without declared language of TIFF file format", tiff.getLabel(),
             is(equalTo("Tagged Image File Format (image/tiff, *.tif)")));
-        assertThat(tiff.getFileType().isPresent(), is(true));
-        assertThat(tiff.getImageFileFormat().get(), is(equalTo(ImageFileFormat.TIFF)));
+        assertThat("Wrong label with declared language range of TIFF file format",
+            tiff.getLabel(Locale.LanguageRange.parse("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")),
+            is(equalTo("Tagged Image File Format (image/tiff, *.tif)")));
+        assertThat("Long time preservation validation file type is missing for TIFF file format",
+            tiff.getFileType().isPresent(), is(true));
+        assertThat("Image management file format of TIFF file format is not TIFF", tiff.getImageFileFormat().get(),
+            is(equalTo(ImageFileFormat.TIFF)));
     }
 }
