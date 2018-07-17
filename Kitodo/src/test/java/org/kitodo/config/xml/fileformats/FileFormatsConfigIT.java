a37ecb6131ae9adaaf40186789850fc71904da0c
Matthias Ronge <matthias.ronge@zeutschel.de>
2018-07-17 18:01:19 +0200

    Add test for transliteration feature

--- Kitodo/src/test/java/org/kitodo/config/xml/fileformats/FileFormatsConfigIT.java
+++ Kitodo/src/test/java/org/kitodo/config/xml/fileformats/FileFormatsConfigIT.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Locale.LanguageRange;
 
 import javax.xml.bind.JAXBException;
 
@@ -17,8 +18,9 @@ public class FileFormatsConfigIT {
     public void testFileFormatsConfig() throws JAXBException, IOException {
         assertThat("kitodo_fileFormats.xml does not contain exactly 8 entries",
             FileFormatsConfig.getFileFormats().size(), is(equalTo(8)));
+
         FileFormat tiff = FileFormatsConfig.getFileFormat("image/tiff").get();
-        assertThat("Wrong label without declared language of TIFF file format", tiff.getLabel(),
+        assertThat("Wrong label of TIFF file format", tiff.getLabel(),
             is(equalTo("Tagged Image File Format (image/tiff, *.tif)")));
         assertThat("Wrong label with declared language range of TIFF file format",
             tiff.getLabel(Locale.LanguageRange.parse("fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5")),
@@ -27,5 +29,18 @@ public class FileFormatsConfigIT {
             tiff.getFileType().isPresent(), is(true));
         assertThat("Image management file format of TIFF file format is not TIFF", tiff.getImageFileFormat().get(),
             is(equalTo(ImageFileFormat.TIFF)));
+    }
+
+    @Test
+    public void testFileFormatsConfigTransliteration() throws JAXBException, IOException {
+        FileFormat gif = FileFormatsConfig.getFileFormat("image/gif").get();
+        assertThat("Wrong label without declared language of GIF file format", gif.getLabel(),
+            is(equalTo("Graphics Interchange Format (image/gif, *.gif)")));
+        assertThat("Wrong label for language requesting arab of GIF file format",
+            gif.getLabel(LanguageRange.parse("fr;q=0.9,ar;q=0.4,*;q=0.2")),
+            is(equalTo("تنسيق تبادل الرسومات (image/gif, *.gif)")));
+        assertThat("Wrong label for language requesting no specialized label of GIF file format",
+            gif.getLabel(LanguageRange.parse("en;q=0.9,fr;q=0.4,*;q=0.2")),
+            is(equalTo("Graphics Interchange Format (image/gif, *.gif)")));
     }
 }
