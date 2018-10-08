f6a45ac418e2bac06dfc3589135db6f675ad0545
Erik Sommer <Erik.Sommer@slub-dresden.de>
2018-10-08 18:03:02 +0200

    Fix formatting issue with single spaces

--- Kitodo/src/main/webapp/WEB-INF/resources/js/modeler.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/modeler.js
@@ -29,8 +29,8 @@ var saveDiagramFunctionCall;
                 isAttr: !0,
                 type: 'Integer'
             }, {name: 'permittedUserRole',
-                 isAttr: !0,
-                 type: 'Integer'
+                isAttr: !0,
+                type: 'Integer'
             }, {name: 'typeMetadata',
                 isAttr: !0,
                 type: 'Boolean'
@@ -77,12 +77,12 @@ var saveDiagramFunctionCall;
                 {name: 'In processing', value: 2},
                 {name: 'Closed', value: 3}],
             modelProperty: 'processingStatus'
-        })),group.entries.push(_EntryFactory2.default.selectBox({
-             id: 'permittedUserRole',
-             description: '',
-             label: 'Permitted User Role',
-             selectOptions : availableUserRoles,
-             modelProperty: 'permittedUserRole'
+        })), group.entries.push(_EntryFactory2.default.selectBox({
+            id: 'permittedUserRole',
+            description: '',
+            label: 'Permitted User Role',
+            selectOptions : availableUserRoles,
+            modelProperty: 'permittedUserRole'
         })), group.entries.push(_EntryFactory2.default.checkbox({
             id: 'typeMetadata',
             description: '',
