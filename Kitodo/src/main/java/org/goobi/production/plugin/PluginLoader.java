97425ee56e007c384c98147048f197c36af7eb24
Beacze <beatrycze.kmiec@slub-dresden.de>
2018-12-11 12:16:32 +0100

    Rename üackage name for catalouge plugin

--- Kitodo/src/main/java/org/goobi/production/plugin/PluginLoader.java
+++ Kitodo/src/main/java/org/goobi/production/plugin/PluginLoader.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.goobi.production.enums.ImportType;
 import org.goobi.production.enums.PluginType;
-import org.goobi.production.plugin.CataloguePlugin.CataloguePlugin;
+import org.goobi.production.plugin.catalogue.CataloguePlugin;
 import org.goobi.production.plugin.interfaces.IImportPlugin;
 import org.goobi.production.plugin.interfaces.IPlugin;
 import org.kitodo.config.ConfigCore;
