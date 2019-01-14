952f0d154fc8b0cbaee649d4b363bf3da93b3bc8
Matthias Ronge <matthias.ronge@zeutschel.de>
2019-01-14 09:38:37 +0100

    Add the domain to the meta-data class
    As part of the refactoring process to extract the incorrectly
    implemented service loaders for the METS module into data classes for
    the API, the domain is taken from the
    org.kitodo.dataformat.access.Metadata class to the
    org.kitodo.api.Metadata class to use this as a data class for meta-data.
    
    In Production, meta-data can be stored in five different areas that
    describe different concept spaces. The different areas are represented
    in the METS file as different XML elements.

--- Kitodo-API/src/main/java/org/kitodo/api/Metadata.java
+++ Kitodo-API/src/main/java/org/kitodo/api/Metadata.java
@@ -11,10 +11,27 @@
 
 package org.kitodo.api;
 
+import org.kitodo.api.dataformat.mets.MdSec;
+
 public class Metadata {
+    /**
+     * In which conceptual area in the METS file this meta-data entry is stored.
+     */
+    private MdSec domain;
 
     // The key of the metadata.
     private String key;
+
+    /**
+     * Specifies the location of the meta-data entry in the METS file. METS
+     * allows the storage of meta-data in five different areas with different
+     * semantics. See {@link MdSec} for details.
+     *
+     * @return the location of the meta-data entry
+     */
+    public MdSec getDomain() {
+        return domain;
+    }
 
     /**
      * Get the key o the metadata.
@@ -26,9 +43,24 @@ public class Metadata {
     }
 
     /**
-     *Set the key of the metadata.
+     * Specifies the location of the meta-data entry in the METS file. METS
+     * allows the storage of meta-data in five different areas with different
+     * semantics. See {@link MdSec} for details. Note that you cannot set the
+     * location for a meta-data entry that is a member of a meta-data group
+     * differently from the location of the meta-data group.
      *
-     * @param key The key value of the metadata.
+     * @param domain
+     *            location to set for the meta-data entry
+     */
+    public void setDomain(MdSec domain) {
+        this.domain = domain;
+    }
+
+    /**
+     * Set the key of the metadata.
+     *
+     * @param key
+     *            The key value of the metadata.
      */
     public void setKey(String key) {
         this.key = key;
