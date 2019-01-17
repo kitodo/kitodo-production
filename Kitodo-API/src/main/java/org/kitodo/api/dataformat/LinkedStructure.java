e1913914b5bc2b690e6767bb8c68aa10aa878cc3
Matthias Ronge <matthias.ronge@zeutschel.de>
2019-01-17 08:13:45 +0100

    Make the interface a sub-interface of DivXmlElementAccessInterface
    We make the newly defined interface a sub-interface of
    DivXmlElementAccessInterface, so that it is automatically used in the
    structures, so that the children can either be structures or linked
    structures.

--- Kitodo-API/src/main/java/org/kitodo/api/dataformat/LinkedStructure.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataformat/LinkedStructure.java
@@ -13,25 +13,44 @@ package org.kitodo.api.dataformat;
 
 import java.math.BigInteger;
 import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+
+import org.kitodo.api.dataformat.mets.AreaXmlElementAccessInterface;
+import org.kitodo.api.dataformat.mets.DivXmlElementAccessInterface;
+import org.kitodo.api.dataformat.mets.MetadataAccessInterface;
 
 /**
  * A link to a structure in a different {@code Workpiece}.
  */
 class LinkedStructure implements ExistingOrLinkedStructure {
     /**
-     * The label of the linked structure.
+     * The label of the linked structure. The label is saved in the linked file.
      */
     private String label;
 
     /**
      * The order is a number that serves to place other structures together with
      * them in the correct order in the same parental structure. After a link
-     * has been made, the order value no longer affects the order.
+     * has been made, the order value no longer affects the order. The order is
+     * saved in the file that contains the link.
      */
     private BigInteger order;
 
     /**
-     * The URI of the linked process.
+     * The type of structure, for example, book, chapter, page. Although the
+     * data type of this variable is a string, it is recommended to use a
+     * controlled vocabulary. If the generated METS files are to be used with
+     * the DFG Viewer, the list of possible structure types is defined. The type
+     * is saved in the linked file.
+     * 
+     * @see "https://dfg-viewer.de/en/structural-data-set/"
+     */
+    private String type;
+
+    /**
+     * The URI of the linked process. The URI is saved in the file that contains
+     * the link.
      */
     private URI uri;
 
@@ -88,5 +107,40 @@ class LinkedStructure implements ExistingOrLinkedStructure {
      */
     public void setUri(URI uri) {
         this.uri = uri;
+    }
+
+    @Override
+    public List<AreaXmlElementAccessInterface> getAreas() {
+        throw new UnsupportedOperationException("areas is not supported by linked structure");
+    }
+
+    @Override
+    public List<DivXmlElementAccessInterface> getChildren() {
+        throw new UnsupportedOperationException("children is not supported by linked structure");
+    }
+
+    @Override
+    public Collection<MetadataAccessInterface> getMetadata() {
+        throw new UnsupportedOperationException("metadata is not supported by linked structure");
+    }
+
+    @Override
+    public String getOrderlabel() {
+        throw new UnsupportedOperationException("orderlabel is not supported by linked structure");
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void setOrderlabel(String orderlabel) {
+        throw new UnsupportedOperationException("orderlabel is not supported by linked structure");
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
     }
 }
