552c2c303f93fa8807b02e1e64ecf62ffe619eea
Matthias Ronge <matthias.ronge@zeutschel.de>
2019-02-08 12:26:04 +0100

    Link folders to project in tests

--- Kitodo/src/test/java/org/kitodo/production/services/data/FolderServiceIT.java
+++ Kitodo/src/test/java/org/kitodo/production/services/data/FolderServiceIT.java
@@ -12,6 +12,7 @@
 package org.kitodo.production.services.data;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -21,6 +22,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kitodo.MockDatabase;
 import org.kitodo.data.database.beans.Folder;
+import org.kitodo.data.database.beans.Project;
+import org.kitodo.production.services.ServiceManager;
 
 /**
  * Tests for TaskService class.
@@ -54,5 +57,15 @@ public class FolderServiceIT {
 
         List<Folder> folders = folderService.getAll();
         assertEquals("Folder was not found in database!", 5, folders.size());
+        for (Folder folder : folders) {
+            assertNotEquals("No project assigned", null, folder.getProject());
+        }
+
+        Project project = ServiceManager.getProjectService().getById(1);
+        assertEquals("No project assigned", 5, project.getFolders().size());
+        for (Folder folder : project.getFolders()) {
+            assertNotEquals("No project assigned", null, folder.getProject());
+            assertEquals("No project assigned", project.getTitle(), folder.getProject().getTitle());
+        }
     }
 }
