69baa4bf558a40dca3f1aa133a7851e687abf819
Beacze <beatrycze.kmiec@slub-dresden.de>
2019-03-13 14:15:54 +0100

    Add database migration for repeatOnCorection column in task table

---
+++ Kitodo-DataManagement/src/main/resources/db/migration/V2_74__Add_repeatOnCorrection_to_task_table.sql
@@ -0,0 +1,14 @@
+--
+-- (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+--
+-- This file is part of the Kitodo project.
+--
+-- It is licensed under GNU General Public License version 3 or later.
+--
+-- For the full copyright and license information, please read the
+-- GPL3-License.txt file that was distributed with this source code.
+--
+
+-- Add column repeatOnCorrection to task table.
+
+ALTER TABLE task ADD repeatOnCorrection TINYINT(1) DEFAULT 0;
