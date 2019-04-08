46ada0a67ea51605bcc7d275fe4f256514044af4
Matthias Ronge <matthias.ronge@zeutschel.de>
2019-04-08 13:08:44 +0200

    Add tests for FileService.createDirectories(URI)

--- Kitodo/src/test/java/org/kitodo/production/services/file/FileServiceIT.java
+++ Kitodo/src/test/java/org/kitodo/production/services/file/FileServiceIT.java
@@ -11,9 +11,18 @@
 
 package org.kitodo.production.services.file;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -21,6 +30,20 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FileServiceIT {
+
+    private static final FileVisitor<Path> DELETE_TREE = new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(Path child, BasicFileAttributes unused) throws IOException {
+            Files.delete(child);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path theDirectory, IOException e) throws IOException {
+            Files.delete(theDirectory);
+            return FileVisitResult.CONTINUE;
+        }
+    };
 
     @BeforeClass
     public static void setUp() throws IOException {
@@ -55,5 +78,43 @@ public class FileServiceIT {
             outputStream.close();
         }
 
+    }
+
+    @Test
+    public void testCreateDirectories() throws IOException {
+        Path testBaseDirectoryPath = Paths.get("src/test/resources");
+
+        // delete existing directories so that directories are actually created
+        Path firstDirectoryOfTestPath = testBaseDirectoryPath.resolve("several");
+        if (Files.isDirectory(firstDirectoryOfTestPath)) {
+            Files.walkFileTree(firstDirectoryOfTestPath, DELETE_TREE);
+        }
+
+        String testBaseDirectoryURI = testBaseDirectoryPath.toUri().toString();
+        String createDirectories = "several/directories/can/be/created/at/once";
+        new FileService().createDirectories(URI.create(testBaseDirectoryURI + createDirectories));
+        assertTrue(Files.isDirectory(firstDirectoryOfTestPath.resolve("directories/can/be/created/at/once")));
+
+        // clean up
+        Files.walkFileTree(firstDirectoryOfTestPath, DELETE_TREE);
+    }
+
+    @Test
+    public void testCreateDirectoriesWithTrailingSlash() throws IOException {
+        Path testBaseDirectoryPath = Paths.get("src/test/resources");
+
+        // delete existing directories so that directories are actually created
+        Path firstDirectoryOfTestPath = testBaseDirectoryPath.resolve("several");
+        if (Files.isDirectory(firstDirectoryOfTestPath)) {
+            Files.walkFileTree(firstDirectoryOfTestPath, DELETE_TREE);
+        }
+
+        String testBaseDirectoryURI = testBaseDirectoryPath.toUri().toString();
+        String createDirectories = "several/directories/can/be/created/with/trailing/";
+        new FileService().createDirectories(URI.create(testBaseDirectoryURI + createDirectories));
+        assertTrue(Files.isDirectory(firstDirectoryOfTestPath.resolve("directories/can/be/created/with/trailing")));
+
+        // clean up
+        Files.walkFileTree(firstDirectoryOfTestPath, DELETE_TREE);
     }
 }
