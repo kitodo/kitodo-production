a16c7e99c072e5bfabeb672878e151eda42aefbd
Arved Solth <solth@effective-webwork.de>
2019-04-15 15:12:46 +0200

    Refactor CustomColumnInitializer code

--- Kitodo/src/main/java/org/kitodo/production/helper/CustomListColumnInitializer.java
+++ Kitodo/src/main/java/org/kitodo/production/helper/CustomListColumnInitializer.java
@@ -11,7 +11,6 @@
 
 package org.kitodo.production.helper;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -47,32 +46,20 @@ public class CustomListColumnInitializer {
      * @param context ServletContext
      */
     public void init(@Observes @Initialized(ApplicationScoped.class) ServletContext context) {
-        processProperties = loadCustomColumnsFromConfigurationFile(ParameterCore.PROCESS_PROPERTIES, PROCESS_PREFIX);
-        taskProcessProperties = loadCustomColumnsFromConfigurationFile(ParameterCore.TASK_CUSTOM_COLUMNS, TASK_PREFIX);
         try {
-            ArrayList<String> customColumnList = new ArrayList<>();
-            customColumnList.addAll(Arrays.asList(processProperties));
-            customColumnList.addAll(Arrays.asList(taskProcessProperties));
-            ServiceManager.getListColumnService().removeCustomListColumns(customColumnList);
+            processProperties = loadCustomColumnsFromConfigurationFile(ParameterCore.PROCESS_PROPERTIES, PROCESS_PREFIX);
+            taskProcessProperties = loadCustomColumnsFromConfigurationFile(ParameterCore.TASK_CUSTOM_COLUMNS, TASK_PREFIX);
 
-            List<String> availableColumnNames = ServiceManager.getListColumnService().getAllCustomListColumns().stream()
-                    .map(ListColumn::getTitle)
-                    .collect(Collectors.toList());
-            logger.info("Loading custom column names from configuration file...");
-            for (String ccn : customColumnList) {
-                if (!ccn.trim().isEmpty()) {
-                    if (availableColumnNames.contains(ccn)) {
-                        logger.info("Custom column '" + ccn + "' already saved to database as custom list column!");
-                    } else {
-                        ListColumn customColumn = new ListColumn(ccn, true);
-                        ServiceManager.getListColumnService().saveToDatabase(customColumn);
-                        logger.info("Custom column '" + ccn + "' successfully saved to database as custom list column!");
-                    }
-                }
-            }
+            updateCustomColumnsInDatabase(Arrays.asList(processProperties));
+            updateCustomColumnsInDatabase(Arrays.asList(taskProcessProperties));
+
         } catch (DAOException e) {
             logger.error("Unable to update custom list columns in database!");
-
+        } catch (NoSuchElementException e) {
+            logger.info("Configuration key '"
+                    + ParameterCore.PROCESS_PROPERTIES.toString()
+                    + "' or '" + ParameterCore.TASK_CUSTOM_COLUMNS.toString()
+                    + "' not found in configuration => unable to load corresponding custom columns!");
         }
     }
 
@@ -94,19 +81,52 @@ public class CustomListColumnInitializer {
         return taskProcessProperties;
     }
 
-    private String[] loadCustomColumnsFromConfigurationFile(ParameterCore configurationKey, String prefix) {
-        String[] customColumns;
-        try {
-            customColumns = Arrays.stream(ConfigCore.getParameter(configurationKey).split(","))
-                    .filter(name -> !name.trim().isEmpty())
-                    .map(name -> prefix + name.trim())
-                    .toArray(String[]::new);
-        } catch (NoSuchElementException e) {
-            customColumns = new String[0];
-            logger.info("Configuration key '"
-                    + ParameterCore.PROCESS_PROPERTIES.toString()
-                    + "' not found in configuration => unable to load process list custom columns!");
+    /**
+     * Load custom column names for configuration key 'configurationKey' from Kitodo configuration file, prefix them
+     * with given String 'prefix' and return names as list.
+     *
+     * @param configurationKey
+     *              Configuration key for custom column names in Kitodo configuration file
+     * @param prefix
+     *              String that is prepend to extracted custom column names before returning them as a list
+     * @return List of custom column names prepended with given String 'prefix'.
+     * @throws NoSuchElementException
+     *              thrown when 'configurationKey' could not be found in Kitodo configuration file
+     */
+    private String[] loadCustomColumnsFromConfigurationFile(ParameterCore configurationKey, String prefix)
+            throws NoSuchElementException {
+        return Arrays.stream(ConfigCore.getParameter(configurationKey).split(","))
+                .filter(name -> !name.trim().isEmpty())
+                .map(name -> prefix + name.trim())
+                .toArray(String[]::new);
+    }
+
+    /**
+     * Update list of custom columns in database. First, delete all custom column in database except for those in given
+     * list 'customColumnList'. Then, add all elements from given list to database that are not already in database.
+     *
+     * @param customColumnList
+     *              list of custom column names that should be in the database
+     * @throws DAOException
+     *              thrown when custom colum table or entries cannot be found in database
+     */
+    private void updateCustomColumnsInDatabase(List<String> customColumnList) throws DAOException {
+        ServiceManager.getListColumnService().removeCustomListColumns(customColumnList);
+
+        List<String> availableColumnNames = ServiceManager.getListColumnService().getAllCustomListColumns().stream()
+                .map(ListColumn::getTitle)
+                .collect(Collectors.toList());
+        logger.info("Loading custom column names from configuration file...");
+        for (String ccn : customColumnList) {
+            if (!ccn.trim().isEmpty()) {
+                if (availableColumnNames.contains(ccn)) {
+                    logger.info("Custom column '" + ccn + "' already saved to database as custom list column!");
+                } else {
+                    ListColumn customColumn = new ListColumn(ccn, true);
+                    ServiceManager.getListColumnService().saveToDatabase(customColumn);
+                    logger.info("Custom column '" + ccn + "' successfully saved to database as custom list column!");
+                }
+            }
         }
-        return customColumns;
     }
 }
