b81a6bbf7955f62f30e42bfa0f889aa8b299ae29
Arved Solth <solth@effective-webwork.de>
2019-05-14 16:26:20 +0200

    Remove unused import

--- Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/MetadataPanel.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/MetadataPanel.java
@@ -17,8 +17,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-import javax.faces.model.SelectItem;
-
 import org.kitodo.api.Metadata;
 import org.kitodo.api.dataeditor.rulesetmanagement.StructuralElementViewInterface;
 import org.kitodo.api.dataformat.IncludedStructuralElement;
