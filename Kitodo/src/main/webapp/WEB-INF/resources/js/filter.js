5c3956dbbebcef42387f9eb5776636fe86bfb73c
Oliver Stöhr <stoehr@effective-webwork.de>
2019-06-28 14:23:10 +0200

    Rewrite filter.js

--- Kitodo/src/main/webapp/WEB-INF/resources/js/filter.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/filter.js
@@ -11,41 +11,48 @@
  *
  */
 
-var PROCESS = "process";
-var TASK = "task";
-var invisibleProcessFilterID = "processesTabView:processesForm:processesTable:titleColumn:filter";
-var invisibleTaskFilterID = "tasksTabView:tasksForm:taskTable:titleColumn:filter";
-var filterInputId = "filterMenu:filterfield";
+var filter = {
+    PROCESS: "process",
+    TASK: "task",
+    invisibleProcessFilterID: "processesTabView:processesForm:processesTable:titleColumn:filter",
+    invisibleTaskFilterID: "tasksTabView:tasksForm:taskTable:titleColumn:filter",
+    filterInputId: "filterMenu:filterfield",
 
-function filterProcesses() {
-    applyFilter(PROCESS, document.getElementById(filterInputId).value);
-}
-
-function filterTasks() {
-    applyFilter(TASK, document.getElementById(filterInputId).value);
-}
-
-function applyFilter(type, filterString) {
-    var invisibleFilterInput;
-    if (type === PROCESS) {
-        invisibleFilterInput = document.getElementById(invisibleProcessFilterID);
-    } else if (type === TASK) {
-        invisibleFilterInput = document.getElementById(invisibleTaskFilterID);
+    applyFilter(type, filterString) {
+        let invisibleFilterInput;
+        if (type === this.PROCESS) {
+            invisibleFilterInput = document.getElementById(this.invisibleProcessFilterID);
+        } else if (type === this.TASK) {
+            invisibleFilterInput = document.getElementById(this.invisibleTaskFilterID);
+        }
+        if (invisibleFilterInput != null) {
+            invisibleFilterInput.value = filterString;
+            invisibleFilterInput.dispatchEvent(new Event("keyup"));
+        } else {
+            let summary = "Error initializing filters: ";
+            let detail = "Could not find invisible filter input.";
+            let wrapperStructure = "<div class=\"ui-messages-error ui-corner-all\"><span class=\"ui-messages-error-icon\"></span><ul></ul></div>";
+            let messageStructure = "<li><span class=\"ui-messages-error-summary\">" + summary + "</span><span class=\"ui-messages-error-detail\">" + detail + "</span></li>";
+            $("#error-messages").append(wrapperStructure).find("ul").append(messageStructure);
+        }
+    },
+    filterProcesses() {
+        this.applyFilter(this.PROCESS, document.getElementById(this.filterInputId).value);
+    },
+    filterTasks() {
+        this.applyFilter(this.TASK, document.getElementById(this.filterInputId).value);
+    },
+    init() {
+        let filterInput = document.getElementById(this.filterInputId);
+        if (filterInput.value.length > 0) {
+            filterInput.dispatchEvent(new Event("change"));
+        }
     }
-    if (invisibleFilterInput != null) {
-        invisibleFilterInput.value = filterString;
-        invisibleFilterInput.dispatchEvent(new Event("keyup"));
-    } else {
-        console.error("Could not find invisible filter input");
-    }
-}
+};
 
 $(document).ready(function () {
     $(document).ready(function () {
         // twice in document.ready to execute after PrimeFaces callbacks
-        var filterInput = document.getElementById(filterInputId);
-        if (filterInput.value.length > 0) {
-            filterInput.dispatchEvent(new Event("change"));
-        }
+        filter.init();
     });
 });
