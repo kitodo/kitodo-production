b8760d77a3bc23a497b79860d276846bd35e6653
Kathrin Huber <kathrin.huber@slub-dresden.de>
2019-10-11 11:41:56 +0200

    respect task ordering for key creation

--- Kitodo/src/main/java/org/kitodo/production/services/migration/MigrationService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/migration/MigrationService.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -106,6 +108,7 @@ public class MigrationService {
      */
     public String createTaskString(List<Task> processTasks) {
         String taskString = "";
+        processTasks.sort(Comparator.comparingInt(Task::getOrdering));
         for (Task processTask : processTasks) {
             taskString = taskString.concat(processTask.getTitle());
         }
