bb78bb46cd760d0430c0cbb28ee0df54ab881b9b
Arved Solth <solth@effective-webwork.de>
2020-02-19 21:55:35 +0100

    Add option to import children of specific data record from OPAC

--- Kitodo/src/main/java/org/kitodo/production/forms/createprocess/ImportTab.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/createprocess/ImportTab.java
@@ -27,6 +27,7 @@ import javax.faces.model.SelectItem;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kitodo.api.MdSec;
@@ -36,6 +37,7 @@ import org.kitodo.api.externaldatamanagement.SingleHit;
 import org.kitodo.api.schemaconverter.ExemplarRecord;
 import org.kitodo.data.database.beans.Process;
 import org.kitodo.data.database.exceptions.DAOException;
+import org.kitodo.exceptions.CatalogException;
 import org.kitodo.exceptions.ConfigException;
 import org.kitodo.exceptions.NoRecordFoundException;
 import org.kitodo.exceptions.ParameterNotFoundException;
@@ -65,8 +67,13 @@ public class ImportTab implements Serializable {
     private static final String INSERTION_TREE = "editForm:processFromTemplateTabView:insertionTree";
     private static final String GROWL_MESSAGE =
             "PF('notifications').renderMessage({'summary':'SUMMARY','detail':'DETAIL','severity':'SEVERITY'});";
+    private static final int NUMBER_OF_CHILDREN_WARNING_THRESHOLD = 10;
 
+    private String currentRecordId = "";
     private int importDepth = 1;
+    private boolean importChildren = false;
+    private int numberOfChildren = 0;
+    private String opacErrorMessage = "";
 
     /**
      * Standard constructor.
@@ -113,10 +120,17 @@ public class ImportTab implements Serializable {
      * Call search method of ImportService.
      */
     public void search() {
-        List<?> hits = hitModel.load(0, 10, null, SortOrder.ASCENDING, Collections.EMPTY_MAP);
+        List<?> hits;
+        try {
+            hits = hitModel.load(0, 10, null, SortOrder.ASCENDING, Collections.EMPTY_MAP);
+        } catch (CatalogException e) {
+            this.opacErrorMessage = e.getMessage();
+            PrimeFaces.current().ajax().update("opacErrorDialog");
+            PrimeFaces.current().executeScript("PF('opacErrorDialog').show();");
+            return;
+        }
         if (hits.size() == 1) {
             getRecordById(((SingleHit) hits.get(0)).getIdentifier());
-            showRecord();
         } else {
             try {
                 ((DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent(HITSTABLE_NAME)).reset();
@@ -144,8 +158,9 @@ public class ImportTab implements Serializable {
      * Get the full record with the given ID from the catalog.
      */
     public void getSelectedRecord() {
+        this.createProcessForm.setChildProcesses(new LinkedList<>());
+        this.createProcessForm.setProcesses(new LinkedList<>());
         getRecordById(Helper.getRequestParameter(ID_PARAMETER_NAME));
-        showRecord();
     }
 
     private void showRecord() {
@@ -194,29 +209,75 @@ public class ImportTab implements Serializable {
         }
     }
 
-    private void getRecordById(String recordId) {
-        try {
-            LinkedList<TempProcess> processes = ServiceManager.getImportService().importProcessHierarchy(recordId,
-                    this.hitModel.getSelectedCatalog(), this.createProcessForm.getProject().getId(),
-                    this.createProcessForm.getTemplate().getId(), this.importDepth,
-                    this.createProcessForm.getRuleset().getFunctionalKeys(FunctionalMetadata.HIGHERLEVEL_IDENTIFIER));
-            this.createProcessForm.setProcesses(processes);
+    /**
+     * Retrieve complete record hierarchy of record from currently selected catalog, including
+     * potential child records and ancestor records.
+     */
+    public void getRecordHierarchy() {
+        if (StringUtils.isBlank(this.currentRecordId)) {
+            Helper.setErrorMessage("No record selected!");
+        } else {
+            try {
+                int projectId = this.createProcessForm.getProject().getId();
+                int templateId = this.createProcessForm.getTemplate().getId();
+                String opac = this.hitModel.getSelectedCatalog();
+                // import children
+                if (this.importChildren) {
+                    this.createProcessForm.setChildProcesses(ServiceManager.getImportService().getChildProcesses(
+                            opac, this.currentRecordId, projectId, templateId, numberOfChildren));
+                }
+                // import ancestors
+                LinkedList<TempProcess> processes = ServiceManager.getImportService().importProcessHierarchy(
+                        this.currentRecordId, opac, projectId, templateId, this.importDepth,
+                        this.createProcessForm.getRuleset().getFunctionalKeys(
+                                FunctionalMetadata.HIGHERLEVEL_IDENTIFIER));
+                this.createProcessForm.setProcesses(processes);
 
-            // Fill metadata fields in metadata tab on successful import
-            if (!processes.isEmpty() && processes.getFirst().getMetadataNodes().getLength() > 0) {
-                TempProcess firstProcess = processes.getFirst();
-                this.createProcessForm.getProcessDataTab().setDocType(firstProcess.getWorkpiece().getRootElement().getType());
-                Collection<Metadata> metadata = ImportService.importMetadata(firstProcess.getMetadataNodes(),
-                    MdSec.DMD_SEC);
-                createProcessForm.getProcessMetadataTab().getProcessDetails().setMetadata(metadata);
+                // Fill metadata fields in metadata tab with metadata values of first process on successful import
+                if (!processes.isEmpty() && processes.getFirst().getMetadataNodes().getLength() > 0) {
+                    TempProcess firstProcess = processes.getFirst();
+                    this.createProcessForm.getProcessDataTab().setDocType(
+                            firstProcess.getWorkpiece().getRootElement().getType());
+                    Collection<Metadata> metadata = ImportService.importMetadata(firstProcess.getMetadataNodes(),
+                            MdSec.DMD_SEC);
+                    createProcessForm.getProcessMetadataTab().getProcessDetails().setMetadata(metadata);
+                }
+
+                String summary = Helper.getTranslation("newProcess.catalogueSearch.importSuccessfulSummary");
+                String detail = Helper.getTranslation("newProcess.catalogueSearch.importSuccessfulDetail",
+                        Arrays.asList(String.valueOf(processes.size()), opac));
+                showGrowlMessage(summary, detail);
+
+                if (this.importChildren) {
+                    summary = Helper.getTranslation("newProcess.catalogueSearch.loadingChilrenSuccessfulSummary");
+                    detail = Helper.getTranslation("newProcess.catalogueSearch.loadingChilrenSuccessfulDetail",
+                            Collections.singletonList(String.valueOf(this.createProcessForm.getChildProcesses().size())));
+                    showGrowlMessage(summary, detail);
+                }
+
+                showRecord();
+            } catch (IOException | ProcessGenerationException | XPathExpressionException | URISyntaxException
+                    | ParserConfigurationException | UnsupportedFormatException | SAXException | NoRecordFoundException
+                    | DAOException | ConfigException e) {
+                Helper.setErrorMessage(e);
             }
-            String summary = Helper.getTranslation("newProcess.catalogueSearch.importSuccessfulSummary");
-            String detail = Helper.getTranslation("newProcess.catalogueSearch.importSuccessfulDetail",
-                    Arrays.asList(String.valueOf(processes.size()), this.hitModel.getSelectedCatalog()));
-            showGrowlMessage(summary, detail);
-        } catch (IOException | ProcessGenerationException | XPathExpressionException | URISyntaxException
-                | ParserConfigurationException | UnsupportedFormatException | SAXException | NoRecordFoundException
-                | DAOException | ConfigException e) {
+        }
+    }
+
+    private void getRecordById(String recordId) {
+        this.currentRecordId = recordId;
+        try {
+            if (this.importChildren) {
+                this.numberOfChildren = ServiceManager.getImportService().getNumberOfChildren(
+                        this.hitModel.getSelectedCatalog(), this.currentRecordId);
+            }
+            if (this.importChildren && this.numberOfChildren > NUMBER_OF_CHILDREN_WARNING_THRESHOLD) {
+                Ajax.update("manyChildrenWarningDialog");
+                PrimeFaces.current().executeScript("PF('manyChildrenWarningDialog').show();");
+            } else {
+                getRecordHierarchy();
+            }
+        } catch (ConfigException e) {
             Helper.setErrorMessage(e);
         }
     }
@@ -261,6 +322,24 @@ public class ImportTab implements Serializable {
     }
 
     /**
+     * Get import children.
+     *
+     * @return import children
+     */
+    public boolean isImportChildren() {
+        return importChildren;
+    }
+
+    /**
+     * Set import children.
+     *
+     * @param childImport import children
+     */
+    public void setImportChildren(boolean childImport) {
+        this.importChildren = childImport;
+    }
+
+    /**
      * Get exemplarRecords.
      *
      * @return value of exemplarRecords
@@ -287,5 +366,37 @@ public class ImportTab implements Serializable {
             Helper.setErrorMessage("newProcess.catalogueSearch.exemplarRecordParameterNotFoundError",
                     new Object[] {e.getMessage(), this.hitModel.getSelectedCatalog() });
         }
+    }
+
+    /**
+     * Check and return whether the "parentElement" is configured in kitodo_opac.xml for the currently selected OPAC.
+     *
+     * @return whether "parentElement" is configured for current OPAC
+     */
+    public boolean isParentElementConfigured() {
+        try {
+            return ServiceManager.getImportService().isParentElementConfigured(this.hitModel.getSelectedCatalog());
+        } catch (ConfigException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Get OPAC error message.
+     *
+     * @return OPAC error message
+     */
+    public String getOpacErrorMessage() {
+        return this.opacErrorMessage;
+    }
+
+    /**
+     * Return String containing warning about high number of child processes that are going to be imported.
+     *
+     * @return warning about high number of child processes to be imported
+     */
+    public String getNumberOfChildProcessesWarning() {
+        return Helper.getTranslation("newProcess.catalogueSearch.manyChildrenWarning",
+                Collections.singletonList(String.valueOf(this.numberOfChildren)));
     }
 }
