e98b9fbe2fcabd9606586bddff883f77470943a8
Matthias Ronge <matthias.ronge@zeutschel.de>
2020-06-04 18:14:12 +0200

    Add test for generation of seasons

--- Kitodo/src/test/java/org/kitodo/NewspaperCourse.java
+++ Kitodo/src/test/java/org/kitodo/NewspaperCourse.java
@@ -36,12 +36,7 @@ public class NewspaperCourse {
     private static Course createCourse() {
         Course course = new Course();
         addFirstBlock(course);
-        // addSecondBlock(course); // currently locked to speed up the test
-        // addThirdBlock(course);
-        // addFourthBlock(course);
-        // addFifthBlock(course);
-        // addSixthBlock(course);
-        // addSeventhBlock(course);
+        addSecondBlock(course);
         return course;
     }
 
@@ -64,7 +59,7 @@ public class NewspaperCourse {
 
     private static void addSecondBlock(Course course) {
         Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1704, 12, 31), LocalDate.of(1713, 7, 22));
+        block.setPublicationPeriod(LocalDate.of(1704, 12, 31), LocalDate.of(1707, 6, 30));
         Issue issue = new Issue(course);
         issue.setWednesday(true);
         issue.setSaturday(true);
@@ -78,104 +73,8 @@ public class NewspaperCourse {
         issue.addExclusion(LocalDate.of(1706, 3, 17));
         issue.addExclusion(LocalDate.of(1706, 5, 1));
         issue.addAddition(LocalDate.of(1706, 5, 2));
-        issue.addExclusion(LocalDate.of(1707, 7, 27));
-        issue.addAddition(LocalDate.of(1707, 7, 29));
-        issue.addExclusion(LocalDate.of(1708, 9, 19));
-        issue.addAddition(LocalDate.of(1708, 9, 21));
-        issue.addExclusion(LocalDate.of(1709, 5, 25));
-        issue.addExclusion(LocalDate.of(1711, 5, 23));
-        issue.addExclusion(LocalDate.of(1711, 5, 27));
-        issue.addExclusion(LocalDate.of(1711, 5, 30));
-        issue.addExclusion(LocalDate.of(1711, 6, 24));
-        issue.addExclusion(LocalDate.of(1711, 6, 27));
-        issue.addExclusion(LocalDate.of(1712, 6, 11));
-        issue.addAddition(LocalDate.of(1712, 6, 14));
-        issue.addExclusion(LocalDate.of(1712, 9, 10));
-        issue.addExclusion(LocalDate.of(1712, 9, 14));
-        issue.addExclusion(LocalDate.of(1712, 12, 31));
-        issue.addExclusion(LocalDate.of(1713, 1, 21));
-        issue.addAddition(LocalDate.of(1713, 1, 22));
-        issue.addExclusion(LocalDate.of(1713, 2, 22));
-        issue.addExclusion(LocalDate.of(1713, 2, 25));
-        issue.addExclusion(LocalDate.of(1713, 4, 12));
-        issue.addAddition(LocalDate.of(1713, 4, 13));
         block.addIssue(issue);
         course.add(block);
     }
 
-    private static void addThirdBlock(Course course) {
-        Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1713, 9, 13), LocalDate.of(1714, 2, 17));
-        Issue issue = new Issue(course);
-        issue.setWednesday(true);
-        issue.setSaturday(true);
-        issue.addAddition(LocalDate.of(1713, 10, 17));
-        issue.addExclusion(LocalDate.of(1713, 10, 18));
-        block.addIssue(issue);
-        course.add(block);
-    }
-
-    private static void addFourthBlock(Course course) {
-        Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1714, 4, 28), LocalDate.of(1714, 10, 19));
-        Issue issue = new Issue(course);
-        issue.addAddition(LocalDate.of(1714, 4, 28));
-        issue.addAddition(LocalDate.of(1714, 5, 2));
-        issue.addAddition(LocalDate.of(1714, 5, 19));
-        issue.addAddition(LocalDate.of(1714, 5, 23));
-        issue.addAddition(LocalDate.of(1714, 6, 2));
-        issue.addAddition(LocalDate.of(1714, 6, 6));
-        issue.addAddition(LocalDate.of(1714, 6, 9));
-        issue.addAddition(LocalDate.of(1714, 6, 23));
-        issue.addAddition(LocalDate.of(1714, 6, 27));
-        issue.addAddition(LocalDate.of(1714, 6, 30));
-        issue.addAddition(LocalDate.of(1714, 7, 4));
-        issue.addAddition(LocalDate.of(1714, 7, 11));
-        issue.addAddition(LocalDate.of(1714, 7, 14));
-        issue.addAddition(LocalDate.of(1714, 7, 21));
-        issue.addAddition(LocalDate.of(1714, 8, 22));
-        issue.addAddition(LocalDate.of(1714, 8, 25));
-        issue.addAddition(LocalDate.of(1714, 9, 1));
-        issue.addAddition(LocalDate.of(1714, 9, 4));
-        issue.addAddition(LocalDate.of(1714, 9, 8));
-        issue.addAddition(LocalDate.of(1714, 10, 19));
-        block.addIssue(issue);
-        course.add(block);
-    }
-
-    private static void addFifthBlock(Course course) {
-        Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1715, 6, 1), LocalDate.of(1715, 7, 13));
-        Issue issue = new Issue(course);
-        issue.setWednesday(true);
-        issue.setSaturday(true);
-        issue.addExclusion(LocalDate.of(1715, 6, 8));
-        issue.addExclusion(LocalDate.of(1715, 6, 15));
-        issue.addExclusion(LocalDate.of(1715, 6, 26));
-        issue.addExclusion(LocalDate.of(1715, 6, 29));
-        block.addIssue(issue);
-        course.add(block);
-    }
-
-    private static void addSixthBlock(Course course) {
-        Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1716, 8, 8), LocalDate.of(1716, 12, 26));
-        Issue issue = new Issue(course);
-        issue.setWednesday(true);
-        issue.setSaturday(true);
-        issue.addExclusion(LocalDate.of(1715, 8, 22));
-        block.addIssue(issue);
-        course.add(block);
-    }
-
-    private static void addSeventhBlock(Course course) {
-        Block block = new Block(course);
-        block.setPublicationPeriod(LocalDate.of(1719, 1, 1), LocalDate.of(1720, 12, 31));
-        Issue issue = new Issue(course);
-        issue.setWednesday(true);
-        issue.setSaturday(true);
-        issue.addExclusion(LocalDate.of(1719, 9, 9));
-        block.addIssue(issue);
-        course.add(block);
-    }
 }
--- Kitodo/src/test/java/org/kitodo/production/process/NewspaperProcessesGeneratorIT.java
+++ Kitodo/src/test/java/org/kitodo/production/process/NewspaperProcessesGeneratorIT.java
@@ -13,19 +13,24 @@ package org.kitodo.production.process;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.MonthDay;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale.LanguageRange;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.SystemUtils;
 import org.awaitility.Awaitility;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -35,6 +40,8 @@ import org.kitodo.MockDatabase;
 import org.kitodo.NewspaperCourse;
 import org.kitodo.SecurityTestUtils;
 import org.kitodo.TreeDeleter;
+import org.kitodo.api.dataformat.IncludedStructuralElement;
+import org.kitodo.api.dataformat.Workpiece;
 import org.kitodo.config.ConfigCore;
 import org.kitodo.config.enums.KitodoConfigFile;
 import org.kitodo.config.enums.ParameterCore;
@@ -43,9 +50,13 @@ import org.kitodo.production.model.bibliography.course.Course;
 import org.kitodo.production.model.bibliography.course.Granularity;
 import org.kitodo.production.services.ServiceManager;
 import org.kitodo.production.services.data.ProcessService;
+import org.kitodo.production.services.dataformat.MetsService;
 
 public class NewspaperProcessesGeneratorIT {
+    private static final String EMPTY_ACQUISITION_STAGE = "";
+    private static final List<LanguageRange> ENGLISH = LanguageRange.parse("en");
     private static final ProcessService processService = ServiceManager.getProcessService();
+    private static final MetsService metsService = ServiceManager.getMetsService();
 
     private static final String firstProcess = "First process";
     private static final File script = new File(ConfigCore.getParameter(ParameterCore.SCRIPT_CREATE_DIR_META));
@@ -59,8 +70,8 @@ public class NewspaperProcessesGeneratorIT {
      * @throws Exception
      *             if that does not work
      */
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         if (!SystemUtils.IS_OS_WINDOWS) {
             ExecutionPermission.setExecutePermission(script);
         }
@@ -76,8 +87,8 @@ public class NewspaperProcessesGeneratorIT {
     /**
      * The test environment is cleaned up and the database is closed.
      */
-    @AfterClass
-    public static void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         MockDatabase.stopNode();
         MockDatabase.cleanDatabase();
         KitodoConfigFile.PROJECT_CONFIGURATION.getFile().delete();
@@ -109,6 +120,119 @@ public class NewspaperProcessesGeneratorIT {
             underTest.getProgress());
 
         // restore backuped meta data file
+        FileUtils.deleteQuietly(metaFile);
+        FileUtils.moveFile(backupFile, metaFile);
+        cleanUp();
+    }
+
+    @Test
+    public void shouldGenerateSeasonProcesses() throws Exception {
+        // create backup of meta data file as this file is modified inside test
+        File metaFile = new File("src/test/resources/metadata/10/meta.xml");
+        File backupFile = new File("src/test/resources/metadata/10/meta.xml.1");
+        FileUtils.copyFile(metaFile, backupFile);
+
+        // Set base type in metadata/10/meta.xml to "Season"
+        Process seasonProcess = ServiceManager.getProcessService().getById(10);
+        URI seasonUri = processService.getMetadataFileUri(seasonProcess);
+        Workpiece seasonMets = metsService.loadWorkpiece(seasonUri);
+        seasonMets.getRootElement().setType("Season");
+        metsService.saveWorkpiece(seasonMets, seasonUri);
+
+        Course course = NewspaperCourse.getCourse();
+        course.setYearStart(MonthDay.of(7, 1));
+        course.splitInto(Granularity.DAYS);
+
+        NewspaperProcessesGenerator underTest = new NewspaperProcessesGenerator(seasonProcess, course);
+        while (underTest.getProgress() < underTest.getNumberOfSteps()) {
+            underTest.nextStep();
+        }
+        Assert.assertEquals("The newspaper processes generator has not been completed!", underTest.getNumberOfSteps(),
+            underTest.getProgress());
+
+        // check season-year processes
+        for (Process saisonProcess : processService.getAll()) {
+            if (Objects.nonNull(saisonProcess.getParent()) && !saisonProcess.getChildren().isEmpty()) {
+                URI saisonYearProcessMetadataUri = processService.getMetadataFileUri(saisonProcess);
+                Workpiece seasonYearWorkpiece = metsService.loadWorkpiece(saisonYearProcessMetadataUri);
+
+                /*
+                 * Year identifier must be two consecutive integer years
+                 * separated by '/'.
+                 */
+                String twoYears = seasonYearWorkpiece.getRootElement().getOrderlabel();
+                List<String> years = Arrays.asList(twoYears.split("/", 2));
+                Assert.assertTrue("Bad season-year in " + saisonProcess + ": " + twoYears,
+                    Integer.parseInt(years.get(0)) + 1 == Integer.parseInt(years.get(1)));
+
+                // all months must be in the timespan
+                for (IncludedStructuralElement monthIncludedStructuralElement : seasonYearWorkpiece.getRootElement()
+                        .getChildren()) {
+                    String monthValue = monthIncludedStructuralElement.getOrderlabel();
+                    List<String> monthValueFields = Arrays.asList(monthValue.split("-", 2));
+                    int monthNumberOfMonth = Integer.parseInt(monthValueFields.get(1));
+                    if (monthValueFields.get(0).equals(years.get(0))) {
+                        Assert.assertTrue(
+                            "Error in " + saisonProcess + ": Found misplaced month " + monthValue
+                                    + ", should not be in year " + twoYears + ", starting by the 1st of July!",
+                            monthNumberOfMonth >= 7);
+                    } else if (monthValueFields.get(0).equals(years.get(1))) {
+                        Assert.assertTrue(
+                            "Error in " + saisonProcess + ": Found misplaced month " + monthValue
+                                    + ", should not be in year " + twoYears + ", starting by the 1st of July!",
+                            monthNumberOfMonth < 7);
+                    } else {
+                        Assert.fail("Error in " + saisonProcess + ": Month " + monthValue + " is not in years "
+                                + twoYears + '!');
+                    }
+                }
+
+                // months must be ordered ascending
+                String previousMonthValue = null;
+                for (IncludedStructuralElement monthIncludedStructuralElement : seasonYearWorkpiece.getRootElement()
+                        .getChildren()) {
+                    String monthValue = monthIncludedStructuralElement.getOrderlabel();
+                    if (Objects.nonNull(previousMonthValue)) {
+                        Assert.assertTrue(
+                            "Bad order of months in " + saisonProcess + ": " + monthValue + " should be before "
+                                    + previousMonthValue + ", but isn’t!",
+                            monthValue.compareTo(previousMonthValue) > 0);
+                    }
+                    previousMonthValue = monthValue;
+                }
+
+                // all days must be inside their month
+                for (IncludedStructuralElement monthIncludedStructuralElement : seasonYearWorkpiece.getRootElement()
+                        .getChildren()) {
+                    String monthValue = monthIncludedStructuralElement.getOrderlabel();
+                    for (IncludedStructuralElement dayIncludedStructuralElement : monthIncludedStructuralElement
+                            .getChildren()) {
+                        String dayValue = dayIncludedStructuralElement.getOrderlabel();
+                        Assert.assertTrue(
+                            "Error in " + saisonProcess + ": " + dayValue + " misplaced in month " + monthValue + '!',
+                            dayValue.startsWith(monthValue));
+                    }
+                }
+
+                // days must be ordered ascending
+                for (IncludedStructuralElement monthIncludedStructuralElement : seasonYearWorkpiece.getRootElement()
+                        .getChildren()) {
+                    String previousDayValue = null;
+                    for (IncludedStructuralElement dayIncludedStructuralElement : monthIncludedStructuralElement
+                            .getChildren()) {
+                        String dayValue = dayIncludedStructuralElement.getOrderlabel();
+                        if (Objects.nonNull(previousDayValue)) {
+                            Assert.assertTrue("Bad order of days in " + saisonProcess + ": " + dayValue
+                                    + " should be before " + previousDayValue + ", but isn’t!",
+                                dayValue.compareTo(previousDayValue) > 0);
+                        }
+                        previousDayValue = dayValue;
+                    }
+                }
+            }
+        }
+
+        // restore backed-up meta data file
         FileUtils.deleteQuietly(metaFile);
         FileUtils.moveFile(backupFile, metaFile);
         cleanUp();
