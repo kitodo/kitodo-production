35f055f8edb2a2146b9a57f70e6afe387d6f8195
Matthias Ronge <matthias.ronge@zeutschel.de>
2020-08-04 11:01:00 +0200

    Summarize common factors from IncludedStructuralElement and MediaUnit

--- Kitodo-API/src/main/java/org/kitodo/api/dataformat/IncludedStructuralElement.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataformat/IncludedStructuralElement.java
@@ -11,20 +11,18 @@
 
 package org.kitodo.api.dataformat;
 
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
 
-import org.kitodo.api.Metadata;
 import org.kitodo.api.dataformat.mets.LinkedMetsResource;
 
 /**
- * The tree-like outline included structural element for digital representation.
- * This structuring included structural element can be subdivided into arbitrary
- * finely granular {@link #subincludedStructuralElements}. It can be described
- * by {@link #metadata}.
+ * A tree-shaped description of the included structural element of the digital
+ * representation of a digital medium. The included structural element can be
+ * imagined as a table of contents and is used to display the table of contents
+ * in the viewer. It uses {@link View}s to refer to elements of the
+ * {@link MediaUnit} of the digital medium, or can {@link #link} to other
+ * processes.
  */
 public class IncludedStructuralElement extends Division<IncludedStructuralElement> {
     /**
@@ -38,44 +36,6 @@ public class IncludedStructuralElement extends Division<IncludedStructuralElemen
      * Specifies the link if there is one.
      */
     private LinkedMetsResource link;
-
-    /**
-     * The metadata for this included structural element. This included
-     * structural element level can be described with any metadata.
-     */
-    private Collection<Metadata> metadata = new HashSet<>();
-
-    /**
-     * Order of IncludedStructuralElements in relation to Views assigned to the parent.
-     */
-    private int order;
-
-    /**
-     * The order label of this included structural element, used to store the
-     * machine-readable value if the label contains a human-readable value that
-     * can be mapped to a machine-readable value.
-     */
-    private String orderlabel;
-
-    /**
-     * The sub-included structural elements of this included structural element,
-     * which form the included structural element tree. The order of the
-     * sub-included structural elements described by the order of the
-     * {@code <div>} elements in the {@code <structMap TYPE="LOGICAL">} in the
-     * METS file.
-     */
-    private List<IncludedStructuralElement> subincludedStructuralElements = new LinkedList<>();
-
-    /**
-     * The type of included structural element, for example, book, chapter,
-     * page. Although the data type of this variable is a string, it is
-     * recommended to use a controlled vocabulary. If the generated METS files
-     * are to be used with the DFG Viewer, the list of possible included
-     * structural element types is defined.
-     *
-     * @see "https://dfg-viewer.de/en/structural-data-set/"
-     */
-    private String type;
 
     /**
      * The views on {@link MediaUnit}s that this included structural element
@@ -98,25 +58,10 @@ public class IncludedStructuralElement extends Division<IncludedStructuralElemen
      *            included structural element that serves as data source
      */
     protected IncludedStructuralElement(IncludedStructuralElement source) {
+        super(source);
         label = source.label;
         link = source.link;
-        metadata = source.metadata;
-        order = source.order;
-        orderlabel = source.orderlabel;
-        subincludedStructuralElements = source.subincludedStructuralElements;
-        type = source.type;
         views = source.views;
-    }
-
-    /**
-     * Returns the sub-included structural elements associated with this
-     * included structural element.
-     *
-     * @return the sub-included structural elements
-     */
-    @Override
-    public List<IncludedStructuralElement> getChildren() {
-        return subincludedStructuralElements;
     }
 
     /**
@@ -158,71 +103,6 @@ public class IncludedStructuralElement extends Division<IncludedStructuralElemen
     }
 
     /**
-     * Returns the metadata on this included structural element.
-     *
-     * @return the metadata
-     */
-    public Collection<Metadata> getMetadata() {
-        return metadata;
-    }
-
-    /**
-     * Get order.
-     *
-     * @return value of order
-     */
-    public int getOrder() {
-        return order;
-    }
-
-    /**
-     * Set order.
-     *
-     * @param order as int
-     */
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    /**
-     * Returns the order label of this included structural element.
-     *
-     * @return the order label
-     */
-    public String getOrderlabel() {
-        return orderlabel;
-    }
-
-    /**
-     * Sets the order label of this included structural element.
-     *
-     * @param orderlabel
-     *            order label to set
-     */
-    public void setOrderlabel(String orderlabel) {
-        this.orderlabel = orderlabel;
-    }
-
-    /**
-     * Returns the type of this included structural element.
-     *
-     * @return the type
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Sets the type of this included structural element.
-     *
-     * @param type
-     *            type to set
-     */
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    /**
      * Returns the views associated with this included structural element.
      *
      * @return the views
@@ -233,7 +113,7 @@ public class IncludedStructuralElement extends Division<IncludedStructuralElemen
 
     @Override
     public String toString() {
-        return type + " \"" + label + "\"";
+        return getType() + " \"" + label + "\"";
     }
 
     @Override
@@ -241,17 +121,24 @@ public class IncludedStructuralElement extends Division<IncludedStructuralElemen
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (!super.equals(o)) {
             return false;
         }
-        IncludedStructuralElement that = (IncludedStructuralElement) o;
-        return order == that.order
-                && Objects.equals(label, that.label)
-                && Objects.equals(link, that.link)
-                && Objects.equals(metadata, that.metadata)
-                && Objects.equals(orderlabel, that.orderlabel)
-                && Objects.equals(subincludedStructuralElements, that.subincludedStructuralElements)
-                && Objects.equals(type, that.type)
-                && Objects.equals(views, that.views);
+        if (!(o instanceof IncludedStructuralElement)) {
+            return false;
+        }
+        IncludedStructuralElement other = (IncludedStructuralElement) o;
+        return Objects.equals(label, other.label) && Objects.equals(link, other.link)
+                && Objects.equals(views, other.views);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((label == null) ? 0 : label.hashCode());
+        result = prime * result + ((link == null) ? 0 : link.hashCode());
+        result = prime * result + ((views == null) ? 0 : views.hashCode());
+        return result;
     }
 }
