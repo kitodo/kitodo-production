e4ef0ed5bb55718b7721f7b4f6d6524473b2df34
Kathrin Huber <kathrin.huber@slub-dresden.de>
2021-01-14 18:45:20 +0100

    fix import

--- Kitodo/src/main/java/org/kitodo/production/forms/createprocess/FileUploadDialog.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/createprocess/FileUploadDialog.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
