f116aac9446be582d07c38c585b6ded9c3dc0ec9
Arved Solth <solth@effective-webwork.de>
2021-02-24 17:41:07 +0100

    Update Kitodo/src/main/java/org/kitodo/production/controller/SecurityAccessController.java

--- Kitodo/src/main/java/org/kitodo/production/controller/SecurityAccessController.java
+++ Kitodo/src/main/java/org/kitodo/production/controller/SecurityAccessController.java
@@ -425,7 +425,7 @@ public class SecurityAccessController {
     /**
      * Check if the current user has the authority to unassign all tasks in progress of a user.
      *
-     * @return true if the current user has the authority to to unassign all tasks in progress of a user
+     * @return true if the current user has the authority to unassign all tasks in progress of a user
      */
     public boolean hasAuthorityToUnassignTasks() {
         return securityAccessService.hasAuthorityToUnassignTasks();
