0ef945729797cdb6ce2eea6bb5e71c1f9eb4f8fa
matthias.ronge <matthias.ronge@MIK-NB-10691.mikservices.local>
2021-02-25 09:52:07 +0100

    Fix spaces

--- Kitodo/src/main/java/org/kitodo/production/services/command/CommandService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/command/CommandService.java
@@ -27,8 +27,8 @@ import org.kitodo.production.services.ServiceManager;
 import org.kitodo.serviceloader.KitodoServiceLoader;
 
 public class CommandService {
-	private static final CommandService commandService = ServiceManager.getCommandService();
-	private final ArrayList<CommandResult> finishedCommandResults = new ArrayList<>();
+    private static final CommandService commandService = ServiceManager.getCommandService();
+    private final ArrayList<CommandResult> finishedCommandResults = new ArrayList<>();
     private final Random random = new Random(1000000);
     /**
      * Method executes a script string.
