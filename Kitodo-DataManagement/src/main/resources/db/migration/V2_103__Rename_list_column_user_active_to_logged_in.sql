9918396ce990e664c48966118add0816586fb33e
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2021-04-20 08:43:35 +0200

    Fix flyway migration

--- Kitodo-DataManagement/src/main/resources/db/migration/V2_103__Rename_list_column_user_active_to_logged_in.sql
+++ Kitodo-DataManagement/src/main/resources/db/migration/V2_103__Rename_list_column_user_active_to_logged_in.sql
@@ -9,6 +9,14 @@
 -- GPL3-License.txt file that was distributed with this source code.
 --
 
--- Rename user list column "active" to "loggedIn"
+-- 1. Switch off safe updates
+--
+SET SQL_SAFE_UPDATES = 0;
 
+-- 2. Rename user list column "active" to "loggedIn"
+--
 UPDATE listColumn SET title = "user.loggedIn" WHERE title = "user.active";
+
+-- 3. Switch on safe updates
+--
+SET SQL_SAFE_UPDATES = 1;
