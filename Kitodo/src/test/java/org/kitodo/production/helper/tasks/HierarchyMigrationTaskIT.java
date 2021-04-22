171f6cf310482b2ae8fbb5bca6c9707c5a7143e9
Kathrin Huber <kathrin.huber@slub-dresden.de>
2021-04-22 08:45:42 +0200

    format

--- Kitodo/src/test/java/org/kitodo/production/helper/tasks/HierarchyMigrationTaskIT.java
+++ Kitodo/src/test/java/org/kitodo/production/helper/tasks/HierarchyMigrationTaskIT.java
@@ -43,6 +43,9 @@ public class HierarchyMigrationTaskIT {
     private static Project project;
     private static final File script = new File(ConfigCore.getParameter(ParameterCore.SCRIPT_CREATE_DIR_META));
 
+    /**
+     * prepares database.
+     */
     @BeforeClass
     public static void prepareDatabase() throws Exception {
         if (!SystemUtils.IS_OS_WINDOWS) {
@@ -59,6 +62,9 @@ public class HierarchyMigrationTaskIT {
         createTestMetaAnchorfile();
     }
 
+    /**
+     * cleans database.
+     */
     @AfterClass
     public static void cleanDatabase() throws Exception {
         if (!SystemUtils.IS_OS_WINDOWS) {
@@ -70,6 +76,9 @@ public class HierarchyMigrationTaskIT {
         cleanUp();
     }
 
+    /**
+     * Tests migration of hierarchies.
+     */
     @Test
     public void testHierarchyMigration() throws DAOException, ProcessGenerationException, CommandException,
             DataException, IOException, SAXException, ParserConfigurationException {
@@ -85,91 +94,69 @@ public class HierarchyMigrationTaskIT {
     }
 
     private static void createTestMetaAnchorfile() throws Exception {
-        List<String> lines = Arrays.asList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-                "<mets:mets xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:kitodo=\"http://meta.kitodo.org/v1/\" xmlns:mets=\"http://www.loc.gov/METS/\">\n" +
-                "    <mets:metsHdr CREATEDATE=\"2019-09-11T05:02:04\">\n" +
-                "        <mets:agent ROLE=\"CREATOR\" TYPE=\"OTHER\" OTHERTYPE=\"SOFTWARE\">\n" +
-                "            <mets:name>Kitodo - kitodo-ugh-2.1.3-kitodo-ugh-2.1.1-11-g4b06eaa - 30−July−2019</mets:name>\n" +
-                "            <mets:note>Kitodo</mets:note>\n" +
-                "            <mets:note>Converted by Kitodo - Data Editor - 3.2.1-SNAPSHOT (2021-03-29T07:53:32Z)</mets:note>\n" +
-                "        </mets:agent>\n" +
-                "        <mets:metsDocumentID></mets:metsDocumentID>\n" +
-                "    </mets:metsHdr>\n" +
-                "    <mets:dmdSec ID=\"DMDLOG_0000\">\n" +
-                "        <mets:mdWrap MDTYPE=\"OTHER\" OTHERMDTYPE=\"KITODO\">\n" +
-                "            <mets:xmlData>\n" +
-                "                <kitodo:kitodo version=\"1.0\">\n" +
-                "                    <kitodo:metadata name=\"CatalogIDDigital\">404810993</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"TSL_ATS\">JahrdeDeG</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"PlaceOfPublication\">Freiberg</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"slub_Finance\">LDP Sachsen</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"docType\">MultiVolumeWork</kitodo:metadata>\n" +
-                "                </kitodo:kitodo>\n" +
-                "            </mets:xmlData>\n" +
-                "        </mets:mdWrap>\n" +
-                "    </mets:dmdSec>\n" +
-                "    <mets:structMap TYPE=\"LOGICAL\">\n" +
-                "        <mets:div ID=\"LOG_0000\" DMDID=\"DMDLOG_0000\" TYPE=\"MultiVolumeWork\" ORDERLABEL=\"Jahresbericht der Deutschen Gerberschule zu Freiberg in Sachsen\" LABEL=\"Jahresbericht der Deutschen Gerberschule zu Freiberg in Sachsen\">\n" +
-                "            <mets:div ID=\"LOG_0001\" TYPE=\"PeriodicalVolume\">\n" +
-                "                <mets:mptr xlink:href=\"\" LOCTYPE=\"URL\"/>\n" +
-                "            </mets:div>\n" +
-                "        </mets:div>\n" +
-                "    </mets:structMap>\n" +
-                "</mets:mets>\n");
+        List<String> lines = Arrays.asList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                + "<mets:mets xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:kitodo=\"http://meta.kitodo.org/v1/\" xmlns:mets=\"http://www.loc.gov/METS/\">\n"
+                + "    <mets:metsHdr CREATEDATE=\"2019-09-11T05:02:04\">\n"
+                + "        <mets:agent ROLE=\"CREATOR\" TYPE=\"OTHER\" OTHERTYPE=\"SOFTWARE\">\n"
+                + "            <mets:name>Kitodo - kitodo-ugh-2.1.3-kitodo-ugh-2.1.1-11-g4b06eaa - 30−July−2019</mets:name>\n"
+                + "            <mets:note>Kitodo</mets:note>\n"
+                + "            <mets:note>Converted by Kitodo - Data Editor - 3.2.1-SNAPSHOT (2021-03-29T07:53:32Z)</mets:note>\n"
+                + "        </mets:agent>\n" + "        <mets:metsDocumentID></mets:metsDocumentID>\n"
+                + "    </mets:metsHdr>\n" + "    <mets:dmdSec ID=\"DMDLOG_0000\">\n"
+                + "        <mets:mdWrap MDTYPE=\"OTHER\" OTHERMDTYPE=\"KITODO\">\n" + "            <mets:xmlData>\n"
+                + "                <kitodo:kitodo version=\"1.0\">\n"
+                + "                    <kitodo:metadata name=\"CatalogIDDigital\">404810993</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"TSL_ATS\">JahrdeDeG</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"PlaceOfPublication\">Freiberg</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"slub_Finance\">LDP Sachsen</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"docType\">MultiVolumeWork</kitodo:metadata>\n"
+                + "                </kitodo:kitodo>\n" + "            </mets:xmlData>\n" + "        </mets:mdWrap>\n"
+                + "    </mets:dmdSec>\n" + "    <mets:structMap TYPE=\"LOGICAL\">\n"
+                + "        <mets:div ID=\"LOG_0000\" DMDID=\"DMDLOG_0000\" TYPE=\"MultiVolumeWork\" ORDERLABEL=\"Jahresbericht der Deutschen Gerberschule zu Freiberg in Sachsen\" LABEL=\"Jahresbericht der Deutschen Gerberschule zu Freiberg in Sachsen\">\n"
+                + "            <mets:div ID=\"LOG_0001\" TYPE=\"PeriodicalVolume\">\n"
+                + "                <mets:mptr xlink:href=\"\" LOCTYPE=\"URL\"/>\n" + "            </mets:div>\n"
+                + "        </mets:div>\n" + "    </mets:structMap>\n" + "</mets:mets>\n");
         File processHome = new File(ConfigCore.getKitodoDataDirectory(), "2");
         FileUtils.writeLines(new File(processHome, "meta_anchor.xml"), "UTF-8", lines);
     }
 
     private static void createTestMetafile() throws Exception {
-        List<String> lines = Arrays.asList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-                "<mets:mets xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:kitodo=\"http://meta.kitodo.org/v1/\" xmlns:mets=\"http://www.loc.gov/METS/\">\n" +
-                "    <mets:metsHdr CREATEDATE=\"2019-09-11T05:02:04\">\n" +
-                "        <mets:agent ROLE=\"CREATOR\" TYPE=\"OTHER\" OTHERTYPE=\"SOFTWARE\">\n" +
-                "            <mets:name>Kitodo - kitodo-ugh-2.1.3-kitodo-ugh-2.1.1-11-g4b06eaa - 30−July−2019</mets:name>\n" +
-                "            <mets:note>Kitodo</mets:note>\n" +
-                "            <mets:note>Converted by Kitodo - Data Editor - 3.2.1-SNAPSHOT (2021-03-29T07:53:32Z)</mets:note>\n" +
-                "        </mets:agent>\n" +
-                "        <mets:metsDocumentID></mets:metsDocumentID>\n" +
-                "    </mets:metsHdr>\n" +
-                "    <mets:dmdSec ID=\"DMDLOG_0001\">\n" +
-                "        <mets:mdWrap MDTYPE=\"OTHER\" OTHERMDTYPE=\"KITODO\">\n" +
-                "            <mets:xmlData>\n" +
-                "                <kitodo:kitodo version=\"1.0\">\n" +
-                "                    <kitodo:metadata name=\"CatalogIDDigital\">404810993-19130000</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"CatalogIDDigitalAnchor\">404810993</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"CurrentNo\">25.1913/14</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"CurrentNoSorting\">19130000</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"slub_script\">Antiqua</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"DocLanguage\">ger</kitodo:metadata>\n" +
-                "                    <kitodo:metadata name=\"docType\">PeriodicalVolume</kitodo:metadata>\n" +
-                "                </kitodo:kitodo>\n" +
-                "            </mets:xmlData>\n" +
-                "        </mets:mdWrap>\n" +
-                "    </mets:dmdSec>\n" +
-                "    <mets:fileSec>\n" +
-                "        <mets:fileGrp USE=\"LOCAL\">\n" +
-                "            <mets:file ID=\"FILE_0001\" MIMETYPE=\"image/tiff\">\n" +
-                "                <mets:FLocat xlink:href=\"file:///home/goobi/work/daten/188237/images/scans_tif/00000001.tif\" LOCTYPE=\"URL\"/>\n" +
-                "            </mets:file>\n" +
-                "        </mets:fileGrp>\n" +
-                "    </mets:fileSec>\n" +
-                "    <mets:structMap TYPE=\"LOGICAL\">\n" +
-                "        <mets:div ID=\"LOG_0002\" TYPE=\"Periodical\">\n" +
-                "            <mets:mptr xlink:href=\"\" LOCTYPE=\"URL\"/>\n" +
-                "            <mets:div ID=\"LOG_0003\" DMDID=\"DMDLOG_0001\" TYPE=\"PeriodicalVolume\" ORDER=\"19130000\">\n" +
-                "                <mets:div ID=\"LOG_0004\" TYPE=\"TitlePage\"/>\n" +
-                "                <mets:div ID=\"LOG_0005\" DMDID=\"DMDLOG_0002\" TYPE=\"OtherDocStrct\">\n" +
-                "                    <mets:div ID=\"LOG_0006\" DMDID=\"DMDLOG_0003\" TYPE=\"OtherDocStrct\"/>\n" +
-                "                    <mets:div ID=\"LOG_0007\" DMDID=\"DMDLOG_0004\" TYPE=\"OtherDocStrct\"/>\n" +
-                "                    <mets:div ID=\"LOG_0008\" DMDID=\"DMDLOG_0005\" TYPE=\"OtherDocStrct\"/>\n" +
-                "                    <mets:div ID=\"LOG_0009\" DMDID=\"DMDLOG_0006\" TYPE=\"OtherDocStrct\"/>\n" +
-                "                    <mets:div ID=\"LOG_0010\" DMDID=\"DMDLOG_0007\" TYPE=\"OtherDocStrct\"/>\n" +
-                "                </mets:div>\n" +
-                "                <mets:div ID=\"LOG_0011\" DMDID=\"DMDLOG_0008\" TYPE=\"Article\"/>\n" +
-                "            </mets:div>\n" +
-                "        </mets:div>\n" +
-                "    </mets:structMap>\n" +
-                "</mets:mets>\n");
+        List<String> lines = Arrays.asList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                + "<mets:mets xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:kitodo=\"http://meta.kitodo.org/v1/\" xmlns:mets=\"http://www.loc.gov/METS/\">\n"
+                + "    <mets:metsHdr CREATEDATE=\"2019-09-11T05:02:04\">\n"
+                + "        <mets:agent ROLE=\"CREATOR\" TYPE=\"OTHER\" OTHERTYPE=\"SOFTWARE\">\n"
+                + "            <mets:name>Kitodo - kitodo-ugh-2.1.3-kitodo-ugh-2.1.1-11-g4b06eaa - 30−July−2019</mets:name>\n"
+                + "            <mets:note>Kitodo</mets:note>\n"
+                + "            <mets:note>Converted by Kitodo - Data Editor - 3.2.1-SNAPSHOT (2021-03-29T07:53:32Z)</mets:note>\n"
+                + "        </mets:agent>\n" + "        <mets:metsDocumentID></mets:metsDocumentID>\n"
+                + "    </mets:metsHdr>\n" + "    <mets:dmdSec ID=\"DMDLOG_0001\">\n"
+                + "        <mets:mdWrap MDTYPE=\"OTHER\" OTHERMDTYPE=\"KITODO\">\n" + "            <mets:xmlData>\n"
+                + "                <kitodo:kitodo version=\"1.0\">\n"
+                + "                    <kitodo:metadata name=\"CatalogIDDigital\">404810993-19130000</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"CatalogIDDigitalAnchor\">404810993</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"CurrentNo\">25.1913/14</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"CurrentNoSorting\">19130000</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"slub_script\">Antiqua</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"DocLanguage\">ger</kitodo:metadata>\n"
+                + "                    <kitodo:metadata name=\"docType\">PeriodicalVolume</kitodo:metadata>\n"
+                + "                </kitodo:kitodo>\n" + "            </mets:xmlData>\n" + "        </mets:mdWrap>\n"
+                + "    </mets:dmdSec>\n" + "    <mets:fileSec>\n" + "        <mets:fileGrp USE=\"LOCAL\">\n"
+                + "            <mets:file ID=\"FILE_0001\" MIMETYPE=\"image/tiff\">\n"
+                + "                <mets:FLocat xlink:href=\"file:///home/goobi/work/daten/188237/images/scans_tif/00000001.tif\" LOCTYPE=\"URL\"/>\n"
+                + "            </mets:file>\n" + "        </mets:fileGrp>\n" + "    </mets:fileSec>\n"
+                + "    <mets:structMap TYPE=\"LOGICAL\">\n" + "        <mets:div ID=\"LOG_0002\" TYPE=\"Periodical\">\n"
+                + "            <mets:mptr xlink:href=\"\" LOCTYPE=\"URL\"/>\n"
+                + "            <mets:div ID=\"LOG_0003\" DMDID=\"DMDLOG_0001\" TYPE=\"PeriodicalVolume\" ORDER=\"19130000\">\n"
+                + "                <mets:div ID=\"LOG_0004\" TYPE=\"TitlePage\"/>\n"
+                + "                <mets:div ID=\"LOG_0005\" DMDID=\"DMDLOG_0002\" TYPE=\"OtherDocStrct\">\n"
+                + "                    <mets:div ID=\"LOG_0006\" DMDID=\"DMDLOG_0003\" TYPE=\"OtherDocStrct\"/>\n"
+                + "                    <mets:div ID=\"LOG_0007\" DMDID=\"DMDLOG_0004\" TYPE=\"OtherDocStrct\"/>\n"
+                + "                    <mets:div ID=\"LOG_0008\" DMDID=\"DMDLOG_0005\" TYPE=\"OtherDocStrct\"/>\n"
+                + "                    <mets:div ID=\"LOG_0009\" DMDID=\"DMDLOG_0006\" TYPE=\"OtherDocStrct\"/>\n"
+                + "                    <mets:div ID=\"LOG_0010\" DMDID=\"DMDLOG_0007\" TYPE=\"OtherDocStrct\"/>\n"
+                + "                </mets:div>\n"
+                + "                <mets:div ID=\"LOG_0011\" DMDID=\"DMDLOG_0008\" TYPE=\"Article\"/>\n"
+                + "            </mets:div>\n" + "        </mets:div>\n" + "    </mets:structMap>\n" + "</mets:mets>\n");
         File processHome = new File(ConfigCore.getKitodoDataDirectory(), "2");
         FileUtils.writeLines(new File(processHome, "meta.xml"), "UTF-8", lines);
     }
