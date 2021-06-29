d6828fa0f1ff1110e1de28a9ba4f914182d47af3
Arved Solth <solth@effective-webwork.de>
2021-06-29 12:27:12 +0200

    Refactor code

--- Kitodo/src/main/java/org/kitodo/production/services/dataeditor/DataEditorService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/dataeditor/DataEditorService.java
@@ -15,8 +15,10 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.kitodo.api.Metadata;
@@ -77,41 +79,25 @@ public class DataEditorService {
      * @return title value of given element
      */
     public static String getTitleValue(IncludedStructuralElement element) {
-        Metadata metadata;
         for (final String titleKey : getTitleKeys()) {
-            metadata = null;
-            if (titleKey.contains("@")) {
-                String[] metadataPath = titleKey.split("@");
-                int i = 0;
-                while (i < metadataPath.length) {
-                    final String pathPart = metadataPath[i];
-                    if (i == 0) {
-                        // _first_ metadata child elements must be taken from structure element
-                        metadata = element.getMetadata().stream()
-                                .filter(m -> m.getKey().equals(pathPart)).findFirst().orElse(null);
-                    } else if (i == metadataPath.length - 1 && metadata instanceof MetadataGroup) {
-                        // _last_ metadata element must be MetadataEntry (must not have children)
-                        metadata = ((MetadataGroup)metadata).getGroup().stream()
-                                .filter(m -> m.getKey().equals(pathPart) && m instanceof MetadataEntry)
-                                .findFirst().orElse(null);
-                    } else {
-                        // all intermediate path parts must represent metadata groups
-                        metadata = ((MetadataGroup)metadata).getGroup().stream()
-                                .filter(m -> m.getKey().equals(pathPart)).findFirst().orElse(null);
-                    }
-                    // skip current title key if no matching metadata could be found for current title key path part
-                    if (Objects.isNull(metadata)) {
-                        break;
-                    }
-                    i++;
-                }
-            } else {
-                // if title key did not contain an "@" sign, use the whole string as metadata key instead
-                metadata = element.getMetadata().stream()
-                        .filter(m -> m.getKey().equals(titleKey)).findFirst().orElse(null);
+            String[] metadataPath = titleKey.split("@");
+            int lastIndex = metadataPath.length - 1;
+            Collection<Metadata> metadata = element.getMetadata();
+            for (int i = 0; i < lastIndex; i++) {
+                final String metadataKey = metadataPath[i];
+                metadata = metadata.stream()
+                        .filter(currentMetadata -> Objects.equals(currentMetadata.getKey(), metadataKey))
+                        .filter(MetadataGroup.class::isInstance).map(MetadataGroup.class::cast)
+                        .flatMap(metadataGroup -> metadataGroup.getGroup().stream())
+                        .collect(Collectors.toList());
             }
-            if (metadata instanceof MetadataEntry && !((MetadataEntry) metadata).getValue().isEmpty()) {
-                return " - " + ((MetadataEntry) metadata).getValue();
+            Optional<String> metadataTitle = metadata.stream().filter(currentMetadata -> Objects.equals(currentMetadata.getKey(), metadataPath[lastIndex]))
+                    .filter(MetadataEntry.class::isInstance).map(MetadataEntry.class::cast)
+                    .map(MetadataEntry::getValue)
+                    .filter(value -> !value.isEmpty())
+                    .findFirst();
+            if (metadataTitle.isPresent()) {
+                return " - ".concat(metadataTitle.get());
             }
         }
         return "";
