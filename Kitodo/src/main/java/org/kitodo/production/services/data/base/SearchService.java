e3bdc71c826568b1442dfa4031813c3e2c9aef01
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2022-02-03 11:59:59 +0100

    mark the index status of base beans within a transaction as done without using the base type derivation

--- Kitodo/src/main/java/org/kitodo/production/services/data/base/SearchService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/base/SearchService.java
@@ -209,14 +209,7 @@ public abstract class SearchService<T extends BaseIndexedBean, S extends BaseDTO
         if (!baseIndexedBeans.isEmpty()) {
             indexer.performMultipleRequests(baseIndexedBeans, type, true);
         }
-        setIndexColumToIndexed(baseIndexedBeans);
-    }
-
-    private void setIndexColumToIndexed(List<T> baseIndexedBeans) throws DAOException {
-        for (T baseIndexedBean : baseIndexedBeans) {
-            baseIndexedBean.setIndexAction(IndexAction.DONE);
-            saveToDatabase(baseIndexedBean);
-        }
+        saveAsIndexed(baseIndexedBeans);
     }
 
     /**
