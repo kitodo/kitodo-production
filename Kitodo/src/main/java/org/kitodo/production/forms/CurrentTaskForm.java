b81325626b5660d3d4efe6795146f9e6d9226f61
BartChris <christoph.bartmann@gmx.de>
2022-08-26 14:06:02 +0200

    change redirect to task list and add test

--- Kitodo/src/main/java/org/kitodo/production/forms/CurrentTaskForm.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/CurrentTaskForm.java
@@ -72,7 +72,7 @@ public class CurrentTaskForm extends BaseForm {
     private final WorkflowControllerService workflowControllerService = new WorkflowControllerService();
     private List<Property> properties;
     private Property property;
-    private final String tasksPath = MessageFormat.format(REDIRECT_PATH, "desktop");
+    private final String tasksPath = MessageFormat.format(REDIRECT_PATH, "tasks");
     private final String taskEditPath = MessageFormat.format(REDIRECT_PATH, "currentTasksEdit");
     private final String taskBatchEditPath = MessageFormat.format(REDIRECT_PATH, "taskBatchEdit");
 
