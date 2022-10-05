96ad75364769c39dd87384ac9609c3e21f285c7e
Thomas Low <code@thomaslow.com>
2022-10-05 11:09:13 +0000

    Make authorities table sortable.

--- Kitodo/src/main/java/org/kitodo/production/services/data/AuthorityService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/AuthorityService.java
@@ -15,16 +15,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.kitodo.data.database.beans.Authority;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.database.persistence.AuthorityDAO;
+import org.kitodo.production.helper.Helper;
 import org.kitodo.production.services.data.base.SearchDatabaseService;
 import org.primefaces.model.SortOrder;
 
 public class AuthorityService extends SearchDatabaseService<Authority, AuthorityDAO> {
 
+    private static final Logger logger = LogManager.getLogger(AuthorityService.class);
+
     private static volatile AuthorityService instance = null;
+
+    /**
+     * The maximum number of authorities to load from the database such that sorting can be done
+     * in this service instead of the database (which doesn't know how to sort translated authority titles).
+     */
+    private static final Integer MAX_SORT_BUFFER_SIZE = 1000;
 
     private static final String GLOBAL_AUTHORITY_SUFFIX = "_globalAssignable";
     private static final String CLIENT_AUTHORITY_SUFFIX = "_clientAssignable";
@@ -97,7 +110,34 @@ public class AuthorityService extends SearchDatabaseService<Authority, Authority
     @Override
     @SuppressWarnings("unchecked")
     public List<Authority> loadData(int first, int pageSize, String sortField, SortOrder sortOrder, Map filters) {
-        return dao.getByQuery("FROM Authority"  + getSort(sortField, sortOrder), filters, first, pageSize);
+        // query all authorities from database
+        List<Authority> authorities = dao.getByQuery("FROM Authority", filters, 0, MAX_SORT_BUFFER_SIZE);
+        if (authorities.size() >= MAX_SORT_BUFFER_SIZE) {
+            // print warning but still return authorities directly from database with database sorting
+            // that can not consider translated authority titles
+            logger.warn("Can't sort more than " + MAX_SORT_BUFFER_SIZE + " authorities!");            
+            return dao.getByQuery("FROM Authority"  + getSort(sortField, sortOrder), filters, first, pageSize);
+        }
+
+        // do sorting
+        Integer sortFactor = sortOrder.equals(SortOrder.DESCENDING) ? -1 : 1;
+        if (sortField.equals("title")) {
+            authorities.sort((Authority a, Authority b) -> 
+                Helper.getTranslation(a.getTitleWithoutSuffix()).toLowerCase().compareTo(
+                    Helper.getTranslation(b.getTitleWithoutSuffix()).toLowerCase()
+                ) * sortFactor
+            );
+        }
+        if (sortField.equals("type")) {
+            authorities.sort((Authority a, Authority b) -> 
+                Helper.getTranslation(a.getType()).compareTo(Helper.getTranslation(b.getType())) * sortFactor
+            );
+        }
+
+        // extract authorities of current page
+        int fromIndex = first;
+        int toIndex = Math.min(authorities.size(), first + pageSize);
+        return IntStream.range(fromIndex, toIndex).mapToObj(authorities::get).collect(Collectors.toList());
     }
 
     /**
