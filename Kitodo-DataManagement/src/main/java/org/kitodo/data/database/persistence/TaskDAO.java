35ea25a0273e5f32c0f25e027858446a10bc3ffd
Thomas Low <thomas.low@knopflogik.de>
2022-10-19 12:26:19 +0200

    Remove unused import.
    Co-authored-by: Arved Solth <solth@effective-webwork.de>

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/persistence/TaskDAO.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/persistence/TaskDAO.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import javax.persistence.PersistenceException;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
