0889c06777d783a7ebe221ea6fbae6313d653306
Pontus Österdahl <85453963+pontus-osterdahl@users.noreply.github.com>
2022-10-21 13:03:22 +0200

    Refactored nested code

--- Kitodo/src/main/java/org/kitodo/production/services/schema/SchemaService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/schema/SchemaService.java
@@ -144,14 +144,10 @@ public class SchemaService {
                         int lastSeparator = mediaFileForMediaVariant.getValue().toString().lastIndexOf('/');
                         String lastSegment = mediaFileForMediaVariant.getValue().toString()
                                 .substring(lastSeparator + 1);
-                        if (variableReplacer.containsFiles(folder.getUrlStructure())) {
-                            mediaFileForMediaVariant
-                                .setValue(new URI(variableReplacer.replaceWithFilenames(folder.getUrlStructure(), lastSegment)));
-                        }
-                        else {
-                            mediaFileForMediaVariant
-                                .setValue(new URI(variableReplacer.replace(folder.getUrlStructure() + lastSegment)));
-                        }
+                        String mediaFile = variableReplacer.containsFiles(folder.getUrlStructure())
+                                ? variableReplacer.replaceWithFilename(folder.getUrlStructure(), lastSegment)
+                                : variableReplacer.replace(folder.getUrlStructure() + lastSegment);
+                        mediaFileForMediaVariant.setValue(new URI(mediaFile));
                     }
                 }
             }
