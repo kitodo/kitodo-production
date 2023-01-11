d5c1a3a7f2641756955c11e3c30f5c5b97c4dd8e
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2023-01-11 17:10:49 +0100

    Improve try block

--- Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/GalleryMediaContent.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/GalleryMediaContent.java
@@ -237,8 +237,7 @@ public class GalleryMediaContent {
          * we are NOT ALLOWED TO close the InputStream at this point. Faces does
          * that after transferring the data.
          */
-        try {
-            InputStream viewData = ServiceManager.getFileService().read(uri);
+        try (InputStream viewData = ServiceManager.getFileService().read(uri)) {
             return DefaultStreamedContent.builder().stream(() -> viewData).contentType(mimeType)
                     .name(Paths.get(uri.getPath()).getFileName().toString()).contentLength(viewData.available())
                     .build();
