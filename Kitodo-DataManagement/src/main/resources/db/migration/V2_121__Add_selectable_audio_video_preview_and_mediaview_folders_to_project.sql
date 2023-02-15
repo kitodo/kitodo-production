8fb40d56736302a92451245fdadc83b29a6f203a
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2023-02-15 18:22:32 +0100

    Rename prefix constraint names to FK_

--- Kitodo-DataManagement/src/main/resources/db/migration/V2_121__Add_selectable_audio_video_preview_and_mediaview_folders_to_project.sql
+++ Kitodo-DataManagement/src/main/resources/db/migration/V2_121__Add_selectable_audio_video_preview_and_mediaview_folders_to_project.sql
@@ -14,17 +14,17 @@
 ALTER TABLE project
   ADD preview_video_folder_id int(11) DEFAULT NULL
         COMMENT 'media to use for video gallery preview',
-  ADD CONSTRAINT CK_preview_video_folder_id
+  ADD CONSTRAINT FK_preview_video_folder_id
         FOREIGN KEY (preview_video_folder_id) REFERENCES folder (id),
   ADD mediaView_video_folder_id int(11) DEFAULT NULL
         COMMENT 'media to use for single video view',
-  ADD CONSTRAINT CK_project_mediaView_video_folder_id
+  ADD CONSTRAINT FK_project_mediaView_video_folder_id
         FOREIGN KEY (mediaView_video_folder_id) REFERENCES folder (id),
   ADD preview_audio_folder_id int(11) DEFAULT NULL
         COMMENT 'media to use for audio gallery preview',
-  ADD CONSTRAINT CK_preview_audio_folder_id
+  ADD CONSTRAINT FK_preview_audio_folder_id
         FOREIGN KEY (preview_audio_folder_id) REFERENCES folder (id),
   ADD mediaView_audio_folder_id int(11) DEFAULT NULL
         COMMENT 'media to use for single audio view',
-  ADD CONSTRAINT CK_project_mediaView_audio_folder_id
+  ADD CONSTRAINT FK_project_mediaView_audio_folder_id
         FOREIGN KEY (mediaView_audio_folder_id) REFERENCES folder (id);
