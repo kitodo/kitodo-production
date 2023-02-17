9cdbee64a639067e4181cc2099fadbd95d179216
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2023-02-17 16:12:47 +0100

    Do not render the media view image if no selected gallery media content is available

--- Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
@@ -227,8 +227,10 @@ function initializeImage() {
     resetNorth();
     hideCanvas();
     let image = $("#imagePreviewForm\\:mediaViewData");
-    addListener(image);
-    image[0].src = image[0].src.replace(/&uuid=[a-z0-9]+/i, "") + "&uuid=" + random(8);
+    if ( image.length > 0 ) {
+        addListener(image);
+        image[0].src = image[0].src.replace(/&uuid=[a-z0-9]+/i, "") + "&uuid=" + random(8);
+    }
 }
 
 function changeToMapView() {
