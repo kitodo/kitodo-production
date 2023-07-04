bf81f346942699d90ad6d3c260fb1d9a7ada44a5
Pontus Österdahl <85453963+pontus-osterdahl@users.noreply.github.com>
2023-07-04 11:30:29 +0200

    Pontus osterdahl patch 5 (#13)
    * Add editCommentDialog.xhtml
    
    * Enable removal of Comment in CommentService and CommentsForm
    
    * Add integration test for CommentService
    
    * Changed properties of comments association in Process bean
    
    * Add edit comment messages
    
    * Update kitodo.css

--- Kitodo/src/main/java/org/kitodo/production/forms/CommentForm.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/CommentForm.java
@@ -43,6 +43,7 @@ import org.kitodo.production.services.workflow.WorkflowControllerService;
 public class CommentForm extends BaseForm {
     private static final Logger logger = LogManager.getLogger(CommentForm.class);
     private boolean correctionComment = false;
+    private Comment editedComment = null;
     private String commentMessage;
     private String correctionTaskId;
     private Task currentTask;
@@ -57,6 +58,19 @@ public class CommentForm extends BaseForm {
      */
     public Process getProcess() {
         return process;
+    }
+
+    /**
+     * Remove a given comment.
+     * 
+     * @param comment to be removed.
+     */
+    public void removeComment(Comment comment) {
+        try {
+            ServiceManager.getCommentService().removeComment(comment);
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -103,6 +117,24 @@ public class CommentForm extends BaseForm {
     }
 
     /**
+     * Set's edited comment.
+     * 
+     * @param comment to be set as editedComment.
+     */
+    public void setEditedComment(Comment comment) {
+        this.editedComment = comment;
+    }
+    
+    /**
+     * Returns edited comment.
+     * 
+     * @return edited comment.
+     */
+    public Comment getEditedComment() {
+        return this.editedComment;
+    }
+    
+    /**
      * Add a new comment to the process.
      */
     public String addComment() {
@@ -140,6 +172,21 @@ public class CommentForm extends BaseForm {
             reportProblem(comment);
             return MessageFormat.format(REDIRECT_PATH, "tasks");
         }
+        return null;
+    }
+
+    /**
+     * Saves the edited comment to database.
+     */
+    public String saveEditedComment() {
+        if (Objects.nonNull(this.editedComment) && this.editedComment.getType().equals(CommentType.INFO)) {
+            try {
+                ServiceManager.getCommentService().saveToDatabase(this.editedComment);
+            } catch (DAOException e) {
+                Helper.setErrorMessage(ERROR_SAVING, logger, e);
+            }
+        }
+        this.editedComment = null;
         return null;
     }
 
