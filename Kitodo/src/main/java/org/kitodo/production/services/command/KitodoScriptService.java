0ad7b5cfaaeb7b4ae0ee18c75e837135d1a74ca9
Matthias Ronge <matthias.ronge@mik-center.de>
2023-07-25 16:36:17 +0200

    Create a base for the Kitodo script

--- Kitodo/src/main/java/org/kitodo/production/services/command/KitodoScriptService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/command/KitodoScriptService.java
@@ -229,6 +229,17 @@ public class KitodoScriptService {
             case "searchForMedia":
                 searchForMedia(processes);
                 break;
+            case "importProcesses":
+                String indir = parameters.get("indir");
+                String template = parameters.get("template");
+                String errors = parameters.get("errors");
+                try {
+                    TaskManager.addTask(new ImportProcesses(indir, template, errors));
+                    Helper.setMessage("kitodoScript.importProcesses.executesInTaskManager");
+                } catch (IllegalArgumentException e) {
+                    Helper.setErrorMessage(e.getMessage());
+                }
+                break;
             default:
                 Helper.setErrorMessage("Unknown action",
                     " - use: 'action:addRole, action:setTaskProperty, action:setStepStatus, "
