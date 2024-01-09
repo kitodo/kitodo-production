3079c44865e1d43bafdec2317070c03eb7fd9458
Markus Weigelt <markus.weigelt@slub-dresden.de>
2024-01-09 18:31:54 +0100

    Revert view

--- Kitodo-API/src/main/java/org/kitodo/api/dataformat/View.java
+++ Kitodo-API/src/main/java/org/kitodo/api/dataformat/View.java
@@ -63,7 +63,7 @@ public class View {
         if (this == o) {
             return true;
         }
-        if (o == null || !getClass().isInstance(o)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         View view = (View) o;
