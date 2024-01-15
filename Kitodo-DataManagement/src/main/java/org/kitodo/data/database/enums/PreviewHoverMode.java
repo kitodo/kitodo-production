24c14120ed56ced6475f6bc43ca76896c448225f
Markus Weigelt <markus.weigelt@slub-dresden.de>
2024-01-15 09:16:54 +0100

    Add javadoc

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/enums/PreviewHoverMode.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/enums/PreviewHoverMode.java
@@ -11,11 +11,23 @@
 
 package org.kitodo.data.database.enums;
 
+/**
+ * Configuration modes when hovering over item in list view (preview item).
+ */
 public enum PreviewHoverMode {
 
+    /**
+     * Show an overlay directly over the item.
+     */
     OVERLAY,
 
+    /**
+     * Show a tooltip beside the item with the preview as content.
+     */
     TOOLTIP_PREVIEW,
 
+    /**
+     * Show a tooltip beside the item with the mediaview as content.
+     */
     TOOLTIP_MEDIAVIEW
 }
