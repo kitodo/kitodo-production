6ace832190477d3cc1f3b04e2e8c48a9d27fb9f7
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2024-02-26 11:09:49 +0100

    Move random string generation to one place

---
+++ Kitodo/src/test/java/org/kitodo/production/helper/HelperTest.java
@@ -0,0 +1,38 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+package org.kitodo.production.helper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+class HelperTest {
+
+    @Test
+    void generateRandomStringOnlyLetters() {
+        String randomString = Helper.generateRandomString(10);
+        assertTrue(StringUtils.isAlpha(randomString), "Generated string contains not only letters");
+    }
+
+    @Test
+    void generateRandomStringOnlyNumbers() {
+        String randomString = Helper.generateRandomString(10, false, true);
+        assertTrue(StringUtils.isNumeric(randomString), "Generated string contains not only numbers");
+    }
+
+    @Test
+    void generateRandomStringLettersAndNumbers() {
+        String randomString = Helper.generateRandomString(10, true, true);
+        assertTrue(StringUtils.isAlphanumeric(randomString), "Generated string contains not only letters and numbers");
+    }
+}
