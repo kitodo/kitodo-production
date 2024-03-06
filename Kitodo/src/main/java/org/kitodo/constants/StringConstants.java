97a5c373481f22b1bdb20946f4fcc05d4c08e00f
Arved Solth <solth@effective-webwork.de>
2024-03-06 08:08:34 +0100

    Update Kitodo/src/main/java/org/kitodo/constants/StringConstants.java
    Co-authored-by: Stefan Weil <sw@weilnetz.de>

--- Kitodo/src/main/java/org/kitodo/constants/StringConstants.java
+++ Kitodo/src/main/java/org/kitodo/constants/StringConstants.java
@@ -13,7 +13,7 @@ package org.kitodo.constants;
 
 public class StringConstants {
 
-    public static String COMMA_DELIMITER = ", ";
-    public static String SEMICOLON_DELIMITER = "; ";
+    public static final String COMMA_DELIMITER = ", ";
+    public static final String SEMICOLON_DELIMITER = "; ";
 
 }
