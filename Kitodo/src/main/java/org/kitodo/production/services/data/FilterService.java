kitodo/kitodo-production

commit 6c6f5cff47b0c16885c54fbcab7b924f52e2cfd4

adjust property search, so legacy filters do work

Kitodo/src/main/java/org/kitodo/production/services/data/FilterService.java
@@ -155,6 +155,7 @@ public class FilterService extends SearchService<Filter, FilterDTO, FilterDAO> {
     public BoolQueryBuilder queryBuilder(String filter, ObjectType objectType, Boolean onlyOpenTasks,
             Boolean onlyUserAssignedTasks) throws DataException {
 
+        filter = replaceLegacyFilters(filter);
         BoolQueryBuilder query = new BoolQueryBuilder();
 
         // this is needed if we filter task
@@ -222,6 +223,13 @@ public class FilterService extends SearchService<Filter, FilterDTO, FilterDAO> {
             }
         }
         return query;
+    }
+
+    private String replaceLegacyFilters(String filter) {
+        filter.replace("processproperty","property");
+        filter.replace("workpiece","property");
+        filter.replace("template","property");
+        return filter;
     }
 
     private BoolQueryBuilder buildTaskQuery(Boolean onlyOpenTasks, Boolean onlyUserAssignedTasks) {
@@ -481,7 +489,7 @@ public class FilterService extends SearchService<Filter, FilterDTO, FilterDAO> {
             BoolQueryBuilder propertyQuery = new BoolQueryBuilder();
             Set<String> strings = filterValuesAsStrings(filter, FilterString.PROPERTY);
             for (String string : strings) {
-                String[] split = string.split("=");
+                String[] split = string.split(":");
                 propertyQuery.should(ServiceManager.getProcessService().createPropertyQuery(split[0], split[1]));
             }
             return propertyQuery;

Kitodo/src/main/java/org/kitodo/production/enums/FilterString.java
@@ -21,11 +21,9 @@ public enum FilterString {
     TASKDONETITLE("stepdonetitle:", "abgeschlossenerschritttitel:"),
     TASKDONEUSER("stepdoneuser:", "abgeschlossenerschrittbenutzer:"),
     PROJECT("project:", "projekt:"),
-    TEMPLATE("template:", "vorlage:"),
     ID("id:", "id:"),
     PARENTPROCESSID("parentprocessid:", "elternprozessid:"),
     PROCESS("process:", "prozess:"),
-    WORKPIECE("workpiece:", "werkstueck:"),
     BATCH("batch:", "gruppe:"),
     TASKAUTOMATIC("stepautomatic:", "schrittautomatisch:"),
     PROPERTY("property:","eigenschaft" );

Kitodo/src/main/java/org/kitodo/production/forms/SearchForm.java
@@ -304,7 +304,7 @@ public class SearchForm {
                     + FilterString.TASKDONETITLE.getFilterEnglish() + this.stepdonetitle + "\" ";
         }
         if (StringUtils.isNotBlank(this.processPropertyTitle) && StringUtils.isNotBlank(this.processPropertyValue)) {
-            search += "\"" + FilterString.PROPERTY.getFilterEnglish() + this.processPropertyTitle + "=" + this.processPropertyValue + "\" ";
+            search += "\"" + FilterString.PROPERTY.getFilterEnglish() + this.processPropertyTitle + ":" + this.processPropertyValue + "\" ";
         }
         return search;
     }

commit 8fe0ad49dc19e2607e335bca02d8a8ce970b031d

Fix CID 415245 (Useless call)
Fixes: 6c6f5cff47b0c168 ("adjust property search, so legacy filters do work")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Kitodo/src/main/java/org/kitodo/production/services/data/FilterService.java
@@ -261,9 +261,9 @@ public class FilterService extends SearchService<Filter, FilterDTO, FilterDAO> {
     }
 
     private String replaceLegacyFilters(String filter) {
-        filter.replace("processproperty","property");
-        filter.replace("workpiece","property");
-        filter.replace("template","property");
+        filter = filter.replace("processproperty","property");
+        filter = filter.replace("workpiece","property");
+        filter = filter.replace("template","property");
         return filter;
     }
 