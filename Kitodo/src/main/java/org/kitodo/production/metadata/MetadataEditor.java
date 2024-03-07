kitodo/kitodo-production

commit 7a612faa77101b32b99b1b13759ab942385a9b28

Update MetadataEditor.java
Added methods for adding metadataentries and metadatagroups sepcifically

Kitodo/src/main/java/org/kitodo/production/metadata/MetadataEditor.java
@@ -32,8 +32,10 @@ import org.apache.logging.log4j.Logger;
 import org.kitodo.api.MdSec;
 import org.kitodo.api.Metadata;
 import org.kitodo.api.MetadataEntry;
+import org.kitodo.api.MetadataGroup;
 import org.kitodo.api.dataeditor.rulesetmanagement.Domain;
 import org.kitodo.api.dataeditor.rulesetmanagement.SimpleMetadataViewInterface;
+import org.kitodo.api.dataeditor.rulesetmanagement.MetadataViewInterface;
 import org.kitodo.api.dataformat.Division;
 import org.kitodo.api.dataformat.LogicalDivision;
 import org.kitodo.api.dataformat.PhysicalDivision;
@@ -62,10 +64,10 @@ public class MetadataEditor {
 
     /**
      * Connects two processes by means of a link. The link is sorted as a linked
-     * logical division in a logical division of the
-     * parent process. The order is based on the order number specified by the
-     * user. This method does not create a link between the two processes in the
-     * database, this must and can only happen when saving.
+     * logical division in a logical division of the parent process. The order
+     * is based on the order number specified by the user. This method does not
+     * create a link between the two processes in the database, this must and
+     * can only happen when saving.
      *
      * @param process
      *            the parent process in which the link is to be added
@@ -83,8 +85,7 @@ public class MetadataEditor {
         LogicalDivision logicalDivision = workpiece.getLogicalStructure();
         for (int index = 0; index < indices.size(); index++) {
             if (index < indices.size() - 1) {
-                logicalDivision = logicalDivision.getChildren()
-                        .get(Integer.parseInt(indices.get(index)));
+                logicalDivision = logicalDivision.getChildren().get(Integer.parseInt(indices.get(index)));
             } else {
                 addLink(logicalDivision, Integer.parseInt(indices.get(index)), childProcessId);
             }
@@ -95,14 +96,14 @@ public class MetadataEditor {
 
     /**
      * Connects two processes by means of a link. The link is sorted as a linked
-     * logical division in a logical division of the
-     * parent process. The order is based on the order number specified by the
-     * user. This method does not create a link between the two processes in the
-     * database, this must and can only happen when saving.
+     * logical division in a logical division of the parent process. The order
+     * is based on the order number specified by the user. This method does not
+     * create a link between the two processes in the database, this must and
+     * can only happen when saving.
      *
      * @param parentLogicalDivision
-     *            document logical division of the parent process in
-     *            which the link is to be added
+     *            document logical division of the parent process in which the
+     *            link is to be added
      * @param childProcessId
      *            Database ID of the child process to be linked
      */
@@ -110,8 +111,7 @@ public class MetadataEditor {
         addLink(parentLogicalDivision, -1, childProcessId);
     }
 
-    private static void addLink(LogicalDivision parentLogicalDivision, int index,
-            int childProcessId) {
+    private static void addLink(LogicalDivision parentLogicalDivision, int index, int childProcessId) {
 
         LinkedMetsResource link = new LinkedMetsResource();
         link.setLoctype(INTERNAL_LOCTYPE);
@@ -128,11 +128,16 @@ public class MetadataEditor {
     }
 
     /**
-     * Remove link to process with ID 'childProcessId' from workpiece of Process 'parentProcess'.
+     * Remove link to process with ID 'childProcessId' from workpiece of Process
+     * 'parentProcess'.
      *
-     * @param parentProcess Process from which link is removed
-     * @param childProcessId ID of process whose link will be removed from workpiece of parent process
-     * @throws IOException thrown if meta.xml could not be loaded
+     * @param parentProcess
+     *            Process from which link is removed
+     * @param childProcessId
+     *            ID of process whose link will be remove from workpiece of
+     *            parent process
+     * @throws IOException
+     *             thrown if meta.xml could not be loaded
      */
     public static void removeLink(Process parentProcess, int childProcessId) throws IOException {
         URI metadataFileUri = ServiceManager.getProcessService().getMetadataFileUri(parentProcess);
@@ -160,7 +165,8 @@ public class MetadataEditor {
                 }
             }
         }
-        // no need to check if 'linkElement' is Null since it is set in the same place as 'parentElement'!
+        // no need to check if 'linkElement' is Null since it is set in the same
+        // place as 'parentElement'!
         if (Objects.nonNull(parentElement)) {
             parentElement.getChildren().remove(linkElement);
             return true;
@@ -190,20 +196,102 @@ public class MetadataEditor {
      * @param metadataValue
      *            value of the first metadata entry
      */
-    public static void addMultipleStructures(int number, String type, Workpiece workpiece, LogicalDivision structure,
+    public static void addMultipleStructuresWithMetadataEntry(int number, String type, Workpiece workpiece, LogicalDivision structure,
             InsertionPosition position, String metadataKey, String metadataValue) {
-
         for (int i = 0; i < number; i++) {
-            LogicalDivision newStructure = addLogicalDivision(type, workpiece, structure, position, Collections.emptyList());
+            LogicalDivision newStructure = addLogicalDivision(type, workpiece, structure, position,
+                Collections.emptyList());
             if (Objects.isNull(newStructure) || metadataKey.isEmpty()) {
                 continue;
             }
-            MetadataEntry metadataEntry = new MetadataEntry();
-            metadataEntry.setKey(metadataKey);
-            metadataEntry.setValue(metadataValue + " " + (number - i));
-            newStructure.getMetadata().add(metadataEntry);
+            if(!metadataKey.isEmpty()) {
+                MetadataEntry metadataEntry = new MetadataEntry();
+                metadataEntry.setKey(metadataKey);
+                metadataEntry.setValue(metadataValue + " " + (number - i));
+                newStructure.getMetadata().add(metadataEntry);
+            }
         }
     }
+
+    /**
+     * Creates a given number of new structures and inserts them into the
+     * workpiece. The insertion position is given relative to an existing
+     * structure. In addition, you can specify metadata, which is assigned to
+     * the structures consecutively with a counter.
+     *
+     * @param number
+     *            number of structures to create
+     * @param type
+     *            type of new structure
+     * @param workpiece
+     *            workpiece to which the new structure is to be added
+     * @param structure
+     *            structure relative to which the new structure is to be
+     *            inserted
+     * @param position
+     *            relative insertion position
+     * @param metadataKey
+     *            key of the metadata to create
+     * @param metadataValue
+     *            value of the first metadata entry
+     */
+    public static void addMultipleStructuresWithMetadataGroup(int number, String type, Workpiece workpiece, LogicalDivision structure,
+            InsertionPosition position, String metadataKey) {
+
+        for (int i = 0; i < number; i++) {
+            LogicalDivision newStructure = addLogicalDivision(type, workpiece, structure, position,
+                Collections.emptyList());
+            if (Objects.isNull(newStructure) || metadataKey.isEmpty() || metadataKey == null) {
+                continue;
+            }
+            MetadataGroup metadataGroup = new MetadataGroup();
+            metadataGroup.setKey(metadataKey);
+            newStructure.getMetadata().add(metadataGroup);
+        }
+    }
+
+    /**
+     * Creates a given number of new structures and inserts them into the
+     * workpiece. The insertion position is given relative to an existing
+     * structure. In addition, you can specify metadata, which is assigned to
+     * the structures consecutively with a counter.
+     *
+     * @param number
+     *            number of structures to create
+     * @param type
+     *            type of new structure
+     * @param workpiece
+     *            workpiece to which the new structure is to be added
+     * @param structure
+     *            structure relative to which the new structure is to be
+     *            inserted
+     * @param position
+     *            relative insertion position
+     * @param metadataViewInterface
+     *            interface of the metadata to be added
+     * @param metadataValue
+     *            value of the first metadata entry
+     */
+    public static void addMultipleStructuresWithMetadata(int number, String type, Workpiece workpiece, LogicalDivision structure,
+            InsertionPosition position, MetadataViewInterface metadataViewInterface, String metadataValue) {
+
+        String metadataKey = metadataViewInterface.getId();
+        
+        if (metadataViewInterface.isComplex()) {
+            addMultipleStructuresWithMetadataGroup(number, type, workpiece, structure, position, metadataKey);
+        } else {
+            addMultipleStructuresWithMetadataEntry(number, type, workpiece, structure, position, metadataKey, metadataValue);
+        }
+    }
+    
+    public static void addMultipleStructures(int number, String type, Workpiece workpiece, LogicalDivision structure,
+            InsertionPosition position) {
+        for (int i = 0; i < number; i++) {
+            LogicalDivision newStructure = addLogicalDivision(type, workpiece, structure, position,
+                Collections.emptyList());
+        }
+    }
+    
 
     /**
      * Creates a new structure and inserts it into a workpiece. The insertion

commit 4cdcd5fbb223be9a85701045ef03e240aa1fdbb9

Fix CID 415251 (Dereference before null check)
Fixes: 7a612faa77101b32 ("Update MetadataEditor.java")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

Kitodo/src/main/java/org/kitodo/production/metadata/MetadataEditor.java
@@ -198,7 +198,7 @@ public class MetadataEditor {
         for (int i = 0; i < number; i++) {
             LogicalDivision newStructure = addLogicalDivision(type, workpiece, structure, position,
                 Collections.emptyList());
-            if (Objects.isNull(newStructure) || metadataKey.isEmpty() || metadataKey == null) {
+            if (Objects.isNull(newStructure) || metadataKey == null || metadataKey.isEmpty()) {
                 continue;
             }
             MetadataGroup metadataGroup = new MetadataGroup();