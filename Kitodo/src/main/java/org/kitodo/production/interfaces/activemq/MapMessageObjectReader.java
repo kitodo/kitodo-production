a2c8b5e2fce3e0f32940cc56f1703895b011845c
Matthias Ronge <matthias.ronge@mik-center.de>
2024-03-26 12:08:07 +0100

    Prevent NumberFormatException for blank input string

--- Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
+++ Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
@@ -27,6 +27,7 @@ import javax.jms.MapMessage;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Strings;
 import org.kitodo.utils.Guard;
 
 public class MapMessageObjectReader {
@@ -128,8 +129,8 @@ public class MapMessageObjectReader {
     public Collection<Integer> getCollectionOfInteger(String key) throws JMSException {
         Collection<String> collectionOfString = getCollectionOfString(key);
         List<Integer> collectionOfInteger = collectionOfString.stream()
-                .flatMap(string -> Arrays.stream(string.split("\\D+"))).map(Integer::valueOf)
-                .collect(Collectors.toList());
+                .flatMap(string -> Arrays.stream(string.split("\\D+"))).filter(Strings::isNotBlank)
+                .map(Integer::valueOf).collect(Collectors.toList());
         return collectionOfInteger;
     }
 
