043a170c179c31d7b53699b1271c877219fb3afc
Arved Solth <solth@effective-webwork.de>
2024-04-12 13:08:26 +0200

    Set specific language for tests

--- Kitodo/src/test/java/org/kitodo/production/helper/XMLUtilsTest.java
+++ Kitodo/src/test/java/org/kitodo/production/helper/XMLUtilsTest.java
@@ -11,7 +11,9 @@
 
 package org.kitodo.production.helper;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.kitodo.production.model.bibliography.course.Course;
 import org.w3c.dom.Document;
@@ -24,6 +26,7 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
 public class XMLUtilsTest {
 
@@ -33,7 +36,19 @@ public class XMLUtilsTest {
     private static final String TEST_CHILD_ELEMENT = "testChildElement";
     private static final String XML_STRING = "<document><element>Text</element></document>";
     private static final String EXPECTED_EXCEPTION_MESSAGE = "javax.xml.transform.TransformerException: "
-            + "Nach dem Token \"/\" oder \"//\" wurde ein Verzeichnisschritt erwartet.";
+            + "A location step was expected following the '/' or '//' token.";
+    private static Locale originalDefaultLocale;
+
+    @BeforeAll
+    public static void setLocaleToEnglish() {
+        originalDefaultLocale = Locale.getDefault();
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @AfterAll
+    public static void resetLocaleToOriginalDefault() {
+        Locale.setDefault(originalDefaultLocale);
+    }
 
     @Test
     public void shouldConvertDocumentToByteArray() throws IOException, TransformerException {
