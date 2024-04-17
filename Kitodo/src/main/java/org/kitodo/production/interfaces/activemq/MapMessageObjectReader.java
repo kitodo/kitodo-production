59db3c0cd8fe5dce6e61fc310b3ae3df38790e99
Matthias Ronge <matthias.ronge@mik-center.de>
2024-04-17 10:33:19 +0200

    Update Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
    Co-authored-by: Arved Solth <solth@effective-webwork.de>

--- Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
+++ Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
@@ -25,9 +25,9 @@ import java.util.stream.Collectors;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 
 public class MapMessageObjectReader {
 
