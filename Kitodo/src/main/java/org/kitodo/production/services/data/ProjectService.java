78b6227125a1380fb2ccb19808b068d263dca564
Matthias Ronge <matthias.ronge@mik-center.de>
2024-05-28 14:24:44 +0200

    Replace LIMIT and OFFSET with the corresponding Hibernate commands (5)

--- Kitodo/src/main/java/org/kitodo/production/services/data/ProjectService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/ProjectService.java
@@ -107,10 +107,8 @@ public class ProjectService extends SearchDatabaseService<Project, ProjectDAO>
         parameters.put("sessionClientId", ServiceManager.getUserService().getSessionClientId());
         parameters.put("sortBy", SORT_FIELD_MAPPING.get(sortField));
         parameters.put("direction", SORT_ORDER_MAPPING.get(sortOrder));
-        parameters.put("limit", pageSize);
-        parameters.put("offset", first);
-        return getByQuery("FROM Project WHERE client_id = :sessionClientId "
-                + "ORDER BY :sortBy :direction LIMIT :limit OFFSET :offset", parameters);
+        return getByQuery("FROM Project WHERE client_id = :sessionClientId ORDER BY :sortBy :direction", parameters,
+            first, pageSize);
     }
 
     @Override
