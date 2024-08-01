c083105e29c6c93132976ab7e718963dc6536e72
Matthias Ronge <matthias.ronge@mik-center.de>
2024-08-01 12:33:51 +0200

    Parse ticket

--- Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/CreateNewProcessesProcessor.java
+++ Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/CreateNewProcessesProcessor.java
@@ -11,21 +11,12 @@
 
 package org.kitodo.production.interfaces.activemq;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import javax.jms.JMSException;
 
 import org.kitodo.config.ConfigCore;
 import org.kitodo.config.enums.ParameterCore;
-import org.kitodo.data.database.beans.Comment;
-import org.kitodo.data.database.beans.Property;
-import org.kitodo.data.database.enums.CommentType;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.exceptions.ProcessorException;
-import org.kitodo.production.forms.CurrentTaskForm;
-import org.kitodo.production.services.ServiceManager;
 
 /**
  * An Active MQ service interface to create new processes.
@@ -48,54 +39,15 @@ public class CreateNewProcessesProcessor extends ActiveMQProcessor {
     @Override
     protected void process(MapMessageObjectReader ticket) throws ProcessorException, JMSException {
         try {
-            Integer projectId = ticket.getMandatoryInteger("project");
-            Integer templateId = ticket.getMandatoryInteger("template");
-            List<?> imports = ticket.getList("import"); // may be null
-            String title = ticket.getString("title"); // may be null
-            Integer parent = ticket.getInteger("parent"); // may be null
-            Map<String, ?> metadata = ticket.getMapOfString("metadata"); // may
-                                                                         // be
-                                                                         // null
-            
-//            Integer stepID = ticket.getMandatoryInteger("id");
-//            dialog.setCurrentTask(ServiceManager.getTaskService().getById(stepID));
-//
-//            if (ticket.hasField("properties")) {
-//                updateProperties(dialog, ticket.getMapOfStringToString("properties"));
-//            }
-//            if (ticket.hasField("message")) {
-//                Comment comment = new Comment();
-//                comment.setProcess(dialog.getCurrentTask().getProcess());
-//                comment.setMessage(ticket.getString("message"));
-//                comment.setAuthor(ServiceManager.getUserService().getCurrentUser());
-//                comment.setType(CommentType.INFO);
-//                comment.setCreationDate(new Date());
-//                ServiceManager.getCommentService().saveToDatabase(comment);
-//            }
-//            dialog.closeTaskByUser();
-        } catch (DAOException e) {
-            throw new ProcessorException(e);
-        }
-    }
+            CreateNewProcessOrder order = new CreateNewProcessOrder(ticket);
 
-    /**
-     * Transfers the properties to set into Production’s data model.
-     *
-     * @param dialog
-     *            The CurrentTaskForm that we work with
-     * @param propertiesToSet
-     *            A Map with the properties to set
-     */
-    private void updateProperties(CurrentTaskForm dialog, Map<String, String> propertiesToSet) {
-        List<Property> availableProperties = dialog.getProperties();
-        for (Property property : availableProperties) {
-            String key = property.getTitle();
-            if (propertiesToSet.containsKey(key)) {
-                String desiredValue = propertiesToSet.get(key);
-                property.setValue(desiredValue);
-                dialog.setProperty(property);
-                dialog.saveCurrentProperty();
-            }
+
+        } catch (IllegalArgumentException e) {
+            // ticket.getMandatory... value not found (null)
+            throw new ProcessorException(e);
+        } catch (DAOException e) {
+            // importconfiguration not found
+            throw new ProcessorException(e);
         }
     }
 }
--- Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
+++ Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/MapMessageObjectReader.java
@@ -11,11 +11,14 @@
 
 package org.kitodo.production.interfaces.activemq;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -29,12 +32,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kitodo.utils.Guard;
 
-import edu.umd.cs.findbugs.annotations.CheckForNull;
-
 public class MapMessageObjectReader {
 
     private MapMessage ticket;
     private static final Logger logger = LogManager.getLogger(MapMessageObjectReader.class);
+    private static final String MANDATORY_ARGUMENT = "Mandatory argument ";
     private static final String MISSING_ARGUMENT = "Missing mandatory argument: \"";
     private static final String WRONG_TYPE = "\" was not found to be of type ";
 
@@ -104,7 +106,8 @@ public class MapMessageObjectReader {
      *             in case that getObject returns null or the returned string is
      *             of length “0”.
      * @throws JMSException
-     *             can be thrown by MapMessage.getString(String)
+     *             thrown by MapMessage.getString(String) if the string is a
+     *             byte[]
      */
     public String getMandatoryString(String key) throws JMSException {
         String mandatoryString = ticket.getString(key);
@@ -160,6 +163,32 @@ public class MapMessageObjectReader {
     }
 
     /**
+     * Fetches a String from a map. This is a strict implementation that
+     * requires the string not to be null and not to be empty.
+     *
+     * @param key
+     *            the name of the string to return
+     * @return the string requested
+     * @throws IllegalArgumentException
+     *             in case that get returns null, an inappropriate object, or
+     *             the returned string is of length “0”.
+     */
+    public static String getMandatoryString(Map<?, ?> data, String key) {
+        Object value = data.get(key);
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException(MISSING_ARGUMENT + key + "\"");
+        }
+        if (!(value instanceof String)) {
+            throw new IllegalArgumentException(MANDATORY_ARGUMENT + key + " is not a string");
+        }
+        String mandatoryString = (String) value;
+        if (mandatoryString.isEmpty()) {
+            throw new IllegalArgumentException(MISSING_ARGUMENT + key + "\"");
+        }
+        return mandatoryString;
+    }
+
+    /**
      * Fetches a String from a MapMessage. This is an access forward to the
      * native function of the MapMessage. You may consider to use
      * getMandatoryString() instead.
@@ -181,18 +210,45 @@ public class MapMessageObjectReader {
      * implementation that requires the Integer not to be null.
      *
      * @param key
-     *            the name of the string to return
+     *            the name of the integer to return
      * @return the string requested
      * @throws IllegalArgumentException
      *             in case that getObject returns null
      * @throws JMSException
-     *             can be thrown by MapMessage.getString(String)
+     *             in case that getObject returns an unmatching object type
      */
     public Integer getMandatoryInteger(String key) throws JMSException {
         if (!ticket.itemExists(key)) {
             throw new IllegalArgumentException(MISSING_ARGUMENT + key + "\"");
         }
         return ticket.getInt(key);
+    }
+
+    /**
+     * Fetches an Integer object from a map. This is a strict implementation
+     * that requires the Integer not to be null.
+     *
+     * @param data
+     *            the data map
+     * @param key
+     *            the name of the integer to return
+     * @return the string requested
+     * @throws IllegalArgumentException
+     *             in case that there is no such key, or get returns an
+     *             unmatching object type
+     */
+    public static Integer getMandatoryInteger(Map<?, ?> data, String key) {
+        if (!data.containsKey(key)) {
+            throw new IllegalArgumentException(MISSING_ARGUMENT + key + "\"");
+        }
+        Object value = data.get(key);
+        if (value instanceof Integer) {
+            return (Integer) value;
+        }
+        if (value instanceof String) {
+            return Integer.valueOf((String) value);
+        }
+        throw new IllegalArgumentException(MANDATORY_ARGUMENT + key + " is not an integer");
     }
 
     /**
@@ -241,6 +297,21 @@ public class MapMessageObjectReader {
         }
 
         return mapOfStringToString;
+    }
+
+    @CheckForNull
+    public List<?> getList(String key) {
+        return null;
+    }
+
+    @CheckForNull
+    public Integer getInteger(String key) {
+        return null;
+    }
+
+    @CheckForNull
+    public Map<String, ?> getMapOfString(String string) {
+        return null;
     }
 
     /**
