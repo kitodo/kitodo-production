a9dbc1c91f0a8a5e25290fe2e05f78ab90c6fbb2
Matthias Ronge <matthias.ronge@mik-center.de>
2024-08-05 14:04:00 +0200

    * changes

--- Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/CreateNewProcessesProcessor.java
+++ Kitodo/src/main/java/org/kitodo/production/interfaces/activemq/CreateNewProcessesProcessor.java
@@ -11,17 +11,30 @@
 
 package org.kitodo.production.interfaces.activemq;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 import javax.jms.JMSException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kitodo.api.dataformat.Workpiece;
 import org.kitodo.config.ConfigCore;
 import org.kitodo.config.enums.ParameterCore;
 import org.kitodo.data.database.exceptions.DAOException;
+import org.kitodo.exceptions.ProcessGenerationException;
 import org.kitodo.exceptions.ProcessorException;
+import org.kitodo.production.helper.TempProcess;
+import org.kitodo.production.process.ProcessGenerator;
+import org.kitodo.production.services.ServiceManager;
+import org.kitodo.production.services.data.ProcessService;
 
 /**
  * An Active MQ service interface to create new processes.
  */
 public class CreateNewProcessesProcessor extends ActiveMQProcessor {
+    private static final Logger logger = LogManager.getLogger(CreateNewProcessesProcessor.class);
+    private final ProcessService processService = ServiceManager.getProcessService();
 
     /**
      * The default constructor looks up the queue name to use in
@@ -41,13 +54,22 @@ public class CreateNewProcessesProcessor extends ActiveMQProcessor {
         try {
             CreateNewProcessOrder order = new CreateNewProcessOrder(ticket);
 
+            // init
+            ProcessGenerator processGenerator = new ProcessGenerator();
+            boolean generated = processGenerator.generateProcess(order.getTemplateId(), order.getProjectId());
+            if (!generated) {
+                throw new ProcessGenerationException("Process was not generated");
+            }
+            Workpiece workpiece = new Workpiece();
+            TempProcess tp = new TempProcess(processGenerator.getGeneratedProcess(), workpiece);
 
-        } catch (IllegalArgumentException e) {
-            // ticket.getMandatory... value not found (null)
-            throw new ProcessorException(e);
-        } catch (DAOException e) {
-            // importconfiguration not found
-            throw new ProcessorException(e);
+            if (order.getImports().isEmpty()) {
+                // create process without import
+            } else {
+                // create process with import
+            }
+        } catch (DAOException /* | DataException */ | ProcessGenerationException e) {
+            throw new ProcessorException(e.getMessage());
         }
     }
 }
