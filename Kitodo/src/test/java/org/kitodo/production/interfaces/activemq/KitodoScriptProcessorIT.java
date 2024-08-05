6130bff66ba80a9b0992b0f53fdfb780bd5bc135
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2024-08-05 15:05:40 +0200

    Use Mockito power to mock the KitodoScript execution

--- Kitodo/src/test/java/org/kitodo/production/interfaces/activemq/KitodoScriptProcessorIT.java
+++ Kitodo/src/test/java/org/kitodo/production/interfaces/activemq/KitodoScriptProcessorIT.java
@@ -8,27 +8,45 @@
  * For the full copyright and license information, please read the
  * GPL3-License.txt file that was distributed with this source code.
  */
+
 package org.kitodo.production.interfaces.activemq;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.kitodo.MockDatabase;
 import org.kitodo.SecurityTestUtils;
 import org.kitodo.data.database.beans.Process;
 import org.kitodo.production.services.ServiceManager;
 import org.kitodo.production.services.command.KitodoScriptService;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class KitodoScriptProcessorIT {
+
+    @Captor
+    private ArgumentCaptor<List<Process>> processCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> scriptCaptor;
+
     @BeforeEach
     public void prepare() throws Exception {
         MockDatabase.startNode();
@@ -54,25 +72,31 @@ public class KitodoScriptProcessorIT {
         // the object to be tested
         KitodoScriptProcessor underTest = new KitodoScriptProcessor();
 
-        // organize return of results
-        List<String> scriptResult = new ArrayList<>();
-        List<Process> processesResult = new ArrayList<>();
-        Field serviceField = KitodoScriptProcessor.class.getDeclaredField("kitodoScriptService");
-        serviceField.setAccessible(true);
-        serviceField.set(underTest, new KitodoScriptService() {
-            @Override
-            public void execute(List<Process> processes, String script) {
-                scriptResult.add(script);
-                processesResult.addAll(processes);
-            }
-        });
+        // manipulate static field to insert a mocked service
+        // using MockStatic or other options did not work or too less knowdlegde to manipulate a static field
+        KitodoScriptService kitodoScriptService = mock(KitodoScriptService.class);
+        Field field = ReflectionUtils
+                .findFields(
+                        KitodoScriptProcessor.class, f -> f.getName().equals("kitodoScriptService"),
+                        ReflectionUtils.HierarchyTraversalMode.TOP_DOWN)
+                .get(0);
+        field.setAccessible(true);
+        field.set(underTest, kitodoScriptService);
+
+        // capture the method parameters of the execute method
+        doNothing().when(kitodoScriptService).execute(processCaptor.capture(), scriptCaptor.capture());
 
         // carry out test
         underTest.process(mockedMappedMessageObjectReader);
 
+        // check executed mocks
+        verify(mockedMappedMessageObjectReader, times(1)).getMandatoryString("script");
+        verify(mockedMappedMessageObjectReader, times(1)).getCollectionOfInteger("processes");
+        verify(kitodoScriptService, times(1)).execute(anyList(), anyString());
+
         // check results
-        assertEquals(scriptResult.get(0), "action:test", "should have passed the script to be executed");
-        assertEquals(processesResult.size(), 1, "should have passed one process");
-        assertEquals(processesResult.get(0).getId(), 1, "should have passed process 1");
+        assertEquals("action:test", scriptCaptor.getValue(), "should have passed the script to be executed");
+        assertEquals(1, processCaptor.getAllValues().size(), "should have passed one process");
+        assertEquals(1, processCaptor.getAllValues().get(0).get(0).getId(), "should have passed process 1");
     }
 }
