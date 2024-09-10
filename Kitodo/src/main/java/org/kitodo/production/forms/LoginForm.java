07ba672faed07d944a002a2d1f4359c5ec2eff30
Matthias Ronge <matthias.ronge@mik-center.de>
2024-09-10 14:40:05 +0200

    Remove unnecessary throws of DAOException

--- Kitodo/src/main/java/org/kitodo/production/forms/LoginForm.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/LoginForm.java
@@ -95,7 +95,7 @@ public class LoginForm implements Serializable {
     /**
      * Check if index is up-to-date and if user has multiple clients and display corresponding notification dialogs.
      */
-    public void performPostLoginChecks() throws DAOException, IOException {
+    public void performPostLoginChecks() throws IOException {
 
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
         SessionClientController controller = new SessionClientController();
