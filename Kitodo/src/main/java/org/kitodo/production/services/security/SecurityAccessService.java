68eb29a1e696922c58cd1bc6e83d103d71b99744
Arved Solth <solth@effective-webwork.de>
2024-09-23 14:07:42 +0200

    Add missing JavaDoc

--- Kitodo/src/main/java/org/kitodo/production/services/security/SecurityAccessService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/security/SecurityAccessService.java
@@ -1114,10 +1114,20 @@ public class SecurityAccessService extends SecurityAccess {
         return hasAnyAuthorityForClient("useMassImport");
     }
 
+    /**
+     * Check if the current user has the permission to set import configurations for processes.
+     *
+     * @return true if the current user has the permission to set import configurations for processes.
+     */
     public boolean hasAuthorityToSetImportConfiguration() {
         return hasAuthorityForClient("setImportConfiguration");
     }
 
+    /**
+     * Check if the current user has the permission to re-import metadata in the metadata editor.
+     *
+     * @return true if the current user has the permission to re-import metadata in the metadata editor.
+     */
     public boolean hasAuthorityToReimportMetadata() {
         return hasAuthorityForClient("reimportMetadata");
     }
