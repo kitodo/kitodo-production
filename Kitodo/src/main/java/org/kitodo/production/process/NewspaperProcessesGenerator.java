2da06c2d98cb819427a70feffac407e1a869fe57
Arved Solth <solth@effective-webwork.de>
2025-02-10 20:28:22 +0100

    Sort newspaper issues by 'ORDERLABEL'

--- Kitodo/src/main/java/org/kitodo/production/process/NewspaperProcessesGenerator.java
+++ Kitodo/src/main/java/org/kitodo/production/process/NewspaperProcessesGenerator.java
@@ -22,6 +22,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale.LanguageRange;
@@ -786,6 +787,7 @@ public class NewspaperProcessesGenerator extends ProcessGenerator {
         createdChild.setType(childType);
         MetadataEditor.writeMetadataEntry(createdChild, identifierMetadata, identifierMetadataValue);
         logicalDivision.getChildren().add(createdChild);
+        logicalDivision.getChildren().sort(Comparator.comparing(LogicalDivision::getOrderlabel));
         return createdChild;
     }
 
