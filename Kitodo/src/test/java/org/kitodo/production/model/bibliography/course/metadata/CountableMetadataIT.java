796e91d6c9bdac65f99644cf8787c87375aecd50
Matthias Ronge <matthias.ronge@mik-center.de>
2025-05-05 15:02:42 +0200

    Declare exception in test function signature

--- Kitodo/src/test/java/org/kitodo/production/model/bibliography/course/metadata/CountableMetadataIT.java
+++ Kitodo/src/test/java/org/kitodo/production/model/bibliography/course/metadata/CountableMetadataIT.java
@@ -35,6 +35,7 @@ import org.kitodo.SecurityTestUtils;
 import org.kitodo.data.database.beans.User;
 import org.kitodo.data.database.exceptions.DAOException;
 import org.kitodo.data.exceptions.DataException;
+import org.kitodo.exceptions.InvalidMetadataValueException;
 import org.kitodo.production.forms.createprocess.ProcessDetail;
 import org.kitodo.production.model.bibliography.course.Block;
 import org.kitodo.production.model.bibliography.course.Course;
@@ -121,7 +122,7 @@ public class CountableMetadataIT {
     }
 
     @Test
-    public void shouldGetValue() throws DAOException, DataException {
+    public void shouldGetValue() throws DAOException, DataException, InvalidMetadataValueException {
         List<ProcessDetail> metadataTypes = getMetadataTypes();
         countableMetadata.setMetadataDetail(metadataTypes.get(0));
         countableMetadata.setStartValue(METADATA_START_VALUE);
@@ -131,7 +132,7 @@ public class CountableMetadataIT {
     }
 
     @Test
-    public void shouldMatch() throws DAOException, DataException {
+    public void shouldMatch() throws DAOException, DataException, InvalidMetadataValueException {
         List<ProcessDetail> metadataTypes = getMetadataTypes();
         ProcessDetail firstMetadataType = metadataTypes.get(0);
         countableMetadata.setMetadataDetail(firstMetadataType);
