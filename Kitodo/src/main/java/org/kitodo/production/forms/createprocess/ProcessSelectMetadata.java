45768f9f67908486dd03544478edb90a996da33e
Matthias Ronge <matthias.ronge@mik-center.de>
2025-05-06 14:44:33 +0200

    Remove unused import

--- Kitodo/src/main/java/org/kitodo/production/forms/createprocess/ProcessSelectMetadata.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/createprocess/ProcessSelectMetadata.java
@@ -19,7 +19,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 
 import javax.faces.model.SelectItem;
