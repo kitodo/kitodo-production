ed547f4953f9463b20643278427e22d9ce30d458
Matthias Ronge <matthias.ronge@mik-center.de>
2025-05-21 14:36:21 +0200

    Update Kitodo/src/main/java/org/kitodo/production/services/data/BatchService.java
    Co-authored-by: Arved Solth <solth@effective-webwork.de>

--- Kitodo/src/main/java/org/kitodo/production/services/data/BatchService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/BatchService.java
@@ -13,7 +13,6 @@ package org.kitodo.production.services.data;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
