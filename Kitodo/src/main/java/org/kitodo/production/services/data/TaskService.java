c23b735b8eaadee51a55efdfd048b558e8efcde6
Matthias Ronge <matthias.ronge@mik-center.de>
2025-05-22 11:04:05 +0200

    Revert "Update sort helper status if saving a task changes it"
    This reverts commit 7f55bee754fabb2178cbe836c7af174b06a761a9.

--- Kitodo/src/main/java/org/kitodo/production/services/data/TaskService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/TaskService.java
@@ -891,16 +891,6 @@ public class TaskService extends BaseBeanService<Task, TaskDAO> {
     @Override
     public void save(Task taskBean) throws DAOException {
         super.save(taskBean);
-
-        Process processBean = taskBean.getProcess();
-        if (Objects.nonNull(processBean)) {
-            String sortHelperStatus = processBean.getSortHelperStatus();
-            WorkflowControllerService.updateProcessSortHelperStatus(processBean);
-            if (!Objects.equals(sortHelperStatus, processBean.getSortHelperStatus())) {
-                ServiceManager.getProcessService().save(processBean);
-            }
-        }
-
         taskBean.getRoles().parallelStream().forEach(role -> role.setUsedInWorkflow(true));
     }
 }
