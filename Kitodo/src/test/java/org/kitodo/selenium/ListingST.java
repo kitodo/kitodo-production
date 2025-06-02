95b413a3b93eb912ad3244649d4f9252b2675be2
Arved Solth <solth@effective-webwork.de>
2025-06-02 14:58:43 +0200

    Reset ListinST test to original state

--- Kitodo/src/test/java/org/kitodo/selenium/ListingST.java
+++ Kitodo/src/test/java/org/kitodo/selenium/ListingST.java
@@ -228,9 +228,9 @@ public class ListingST extends BaseTestSelenium {
         int usersDisplayed = usersPage.countListedUsers();
         assertEquals(usersInDatabase, usersDisplayed, "Displayed wrong number of users");
 
-        int clientRolesInDatabase = ServiceManager.getRoleService().getNumberOfRolesOfCurrentClient();
+        int rolesInDatabase = ServiceManager.getRoleService().getAll().size();
         int rolesDisplayed = usersPage.countListedRoles();
-        assertEquals(clientRolesInDatabase, rolesDisplayed, "Displayed wrong number of roles");
+        assertEquals(rolesInDatabase, rolesDisplayed, "Displayed wrong number of roles");
 
         int clientsInDatabase = ServiceManager.getClientService().getAll().size();
         int clientsDisplayed = usersPage.countListedClients();
