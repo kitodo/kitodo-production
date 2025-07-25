59f76543ecea8ab5c45bda303a1be2d1e3140b33
Arved Solth <solth.arved@gmail.com>
2025-07-25 16:30:55 +0200

    Ead import default namespace (#6620)
    * Add processing of XML default namespace during EAD import with SAX parser
    
    * Add missing xpath-default-namespace to EAD test import mapping file

--- Kitodo/src/main/java/org/kitodo/production/thread/ImportEadProcessesThread.java
+++ Kitodo/src/main/java/org/kitodo/production/thread/ImportEadProcessesThread.java
@@ -264,6 +264,8 @@ public class ImportEadProcessesThread extends EmptyTask {
             if (StringUtils.isNotBlank(prefix)) {
                 content = content.replace(">", " xmlns:" + namespace.getPrefix() + "=\""
                         + namespace.getNamespaceURI() + "\">");
+            } else {
+                content = content.replace(">", " xmlns=\"" + namespace.getNamespaceURI() + "\">");
             }
         }
         return removeDefaultNamespaceUri(content);
