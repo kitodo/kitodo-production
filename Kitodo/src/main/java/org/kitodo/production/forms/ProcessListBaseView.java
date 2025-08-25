f18e9030d0047da5ee94d4477a429739ae440302
BartChris <christoph.bartmann@gmx.de>
2025-08-25 20:06:15 +0200

    Optimization of the process list

--- Kitodo/src/main/java/org/kitodo/production/forms/ProcessListBaseView.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/ProcessListBaseView.java
@@ -617,17 +617,17 @@ public class ProcessListBaseView extends BaseForm {
     /**
      * Check and return whether process with given ID can be exported or not.
      *
-     * @param processId process ID
+     * @param process the process
      * @return whether process with given ID can be exported or not
      */
-    public boolean canBeExported(int processId) {
-        Stopwatch stopwatch = new Stopwatch(this.getClass(), processId, "canBeExported");
+    public boolean canBeExported(Process process) {
+        Stopwatch stopwatch = new Stopwatch(this.getClass(), process, "canBeExported");
         try {
-            if (!exportable.containsKey(processId)) {
-                exportable.put(processId, ProcessService.canBeExported(processId));
+            if (!exportable.containsKey(process.getId())) {
+                exportable.put(process.getId(), ProcessService.canBeExported(process));
             }
-            return stopwatch.stop(exportable.get(processId));
-        } catch (IOException | DAOException e) {
+            return stopwatch.stop(exportable.get(process.getId()));
+        } catch (DAOException e) {
             Helper.setErrorMessage(e);
             return stopwatch.stop(false);
         }
