3c81bef035c7ecc9733510eb1feb790ffd72d994
BartChris <christoph.bartmann@gmx.de>
2025-10-15 15:01:02 +0200

    Make index condition more explicit

--- Kitodo/src/main/java/org/kitodo/production/services/data/BeanQuery.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/BeanQuery.java
@@ -30,6 +30,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.kitodo.data.database.beans.BaseBean;
 import org.kitodo.data.database.beans.Process;
 import org.kitodo.data.database.beans.Role;
+import org.kitodo.data.database.beans.Task;
 import org.kitodo.production.enums.ProcessState;
 import org.kitodo.production.services.ServiceManager;
 import org.kitodo.production.services.index.IndexingService;
@@ -313,7 +314,8 @@ public class BeanQuery {
      */
     public void restrictWithUserFilterString(String filterString) {
         int userFilterCount = 0;
-        for (var groupFilter : filterService.parse(filterString, true)
+        boolean indexed = beanClass.isAssignableFrom(Process.class) || beanClass.isAssignableFrom(Task.class);
+        for (var groupFilter : filterService.parse(filterString, indexed)
                 .entrySet()) {
             List<String> groupFilters = new ArrayList<>();
             for (UserSpecifiedFilter searchFilter : groupFilter.getValue()) {
