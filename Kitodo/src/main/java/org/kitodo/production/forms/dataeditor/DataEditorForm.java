6d3803c1f110e3795911e137f0116950dc091eff
Arved Solth <solth@effective-webwork.de>
2025-12-19 14:51:14 +0100

    Implement change requests

--- Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/DataEditorForm.java
+++ Kitodo/src/main/java/org/kitodo/production/forms/dataeditor/DataEditorForm.java
@@ -1359,6 +1359,11 @@ public class DataEditorForm implements MetadataTreeTableInterface, RulesetSetupI
         return taskLayoutLoaded;
     }
 
+    /**
+     * Check whether access conditions to metadata of given process are fulfilled.
+     * @param process process to check access conditions for
+     * @param linkedProcess flag indicating whether the process is a linked process or not
+     */
     private void checkProcessMetadataAccessConditions(Process process, boolean linkedProcess) {
         linkedProcessId = null;
         errorMessage = null;
@@ -1382,7 +1387,7 @@ public class DataEditorForm implements MetadataTreeTableInterface, RulesetSetupI
      * - the currently selected structure tree node represents a linked process
      * - the linked process belongs to a project assigned to the current user
      * - the linked process is currently opened in the metadata editor by another user
-     * Sets properties containing error message and error title used in corresponding popup dialog accordingly.
+     * Sets properties containing error message and error title used in the corresponding popup dialog accordingly.
      */
     public void checkConditionsForOpeningLinkedProcessInMetadataEditor() {
         linkedProcessClicked = true;
@@ -1398,6 +1403,30 @@ public class DataEditorForm implements MetadataTreeTableInterface, RulesetSetupI
         } catch (DAOException e) {
             Helper.setErrorMessage(e.getLocalizedMessage(), logger, e);
         }
+    }
+
+    /**
+     * Checks and returns whether
+     * - the currently selected structure tree node represents a linked process
+     * - the linked process belongs to a project assigned to the current user
+     * - the linked process is currently opened in the metadata editor by another user
+     */
+    public boolean canLinkedProcessBeOpenedInMetadataEditor() {
+        Optional<LogicalDivision> divisionOptional = structurePanel.getSelectedStructure();
+        if (divisionOptional.isPresent()) {
+            try {
+                Integer processId = ServiceManager.getDataEditorService().getLinkedProcessId(divisionOptional.get());
+                if (Objects.nonNull(processId)) {
+                    Process linkedProcess = ServiceManager.getProcessService().getById(processId);
+                    User blockingUser = MetadataLock.getLockUser(linkedProcess.getId());
+                    return (ServiceManager.getUserService().getCurrentUser().getProjects().contains(linkedProcess.getProject())
+                            && (Objects.isNull(blockingUser) || blockingUser.equals(this.user)));
+                }
+            } catch (DAOException e) {
+                Helper.setErrorMessage(e.getLocalizedMessage(), logger, e);
+            }
+        }
+        return false;
     }
 
     /**
