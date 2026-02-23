8b9528ff9a4fefc38fe64c2e7205559bbeee44b4
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2026-02-23 14:35:11 +0100

    Replace Arrays.asList() with List.of()

--- Kitodo-LongTermPreservationValidation/src/test/java/org/kitodo/longtermpreservationvalidation/LongTermPreservationValidationIT.java
+++ Kitodo-LongTermPreservationValidation/src/test/java/org/kitodo/longtermpreservationvalidation/LongTermPreservationValidationIT.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.kitodo.api.validation.longtermpreservation.FileType;
@@ -339,7 +340,7 @@ public class LongTermPreservationValidationIT {
         LongTermPreservationValidationInterface validator = new LongTermPreservationValidation();
 
         LtpValidationCondition wrongValuesCondition = new LtpValidationCondition("imageWidth",
-                LtpValidationConditionOperation.NON_EMPTY, Arrays.asList("60"),
+                LtpValidationConditionOperation.NON_EMPTY, List.of("60"),
                 LtpValidationConditionSeverity.ERROR);
 
         LtpValidationCondition validCondition = new LtpValidationCondition("imageWidth",
@@ -348,11 +349,11 @@ public class LongTermPreservationValidationIT {
 
         assertEquals(LtpValidationResultState.ERROR,
                 validator.validate(TIF_URI, FileType.TIFF,
-                        Arrays.asList(wrongValuesCondition)).getState());
+                        List.of(wrongValuesCondition)).getState());
 
         assertEquals(LtpValidationResultState.VALID,
                 validator.validate(TIF_URI, FileType.TIFF,
-                        Arrays.asList(validCondition)).getState());
+                        List.of(validCondition)).getState());
     }
 
     private LtpValidationResultState simpleValidateFile(URI file, FileType fileType) {
