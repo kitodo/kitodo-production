3a29939f87a9b008c0290a5544d4d2f05addbcbd
BartChris <christoph.bartmann@gmx.de>
2026-07-28 10:00:33 +0200

    Remove empty line

--- Kitodo/src/test/java/org/kitodo/production/services/data/ProjectServiceIT.java
+++ Kitodo/src/test/java/org/kitodo/production/services/data/ProjectServiceIT.java
@@ -17,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import java.util.Collections;
 import java.util.List;
 
