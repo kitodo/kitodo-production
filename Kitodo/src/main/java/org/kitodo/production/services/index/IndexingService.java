52807f554a1d1d6f4972d56c0c4a5c380f5d7230
Thomas Low <code@thomaslow.com>
2026-09-08 20:13:30 +0000

    Fix codeql issue.

--- Kitodo/src/main/java/org/kitodo/production/services/index/IndexingService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/index/IndexingService.java
@@ -201,7 +201,7 @@ public class IndexingService {
         BeanQuery beanQuery = new BeanQuery(Process.class);
         Long totalCount = ServiceManager.getProcessService().count(beanQuery.formCountQuery(), beanQuery
                 .getQueryParameters());
-        Long indexedCount = getAllIndexed();
+        long indexedCount = getAllIndexed();
         if (totalCount != indexedCount) {
             logger.warn("index is considered corrupted with {} of {} processes indexed", indexedCount, totalCount);
         }
