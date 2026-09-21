56f94452e2f6b44f234f29115382d634f498417e
Arved Solth <solth.arved@gmail.com>
2026-09-21 08:13:20 +0200

    Add loading symbol to metadata editor gallery detail view (#7231)
    * Add loading symbol to metadata editor gallery detail view
    
    * Update Kitodo/src/main/webapp/WEB-INF/resources/css/kitodo.css
    
    Co-authored-by: Oliver Stöhr <stoehr@effective-webwork.de>
    
    * Implement change requests
    
    ---------
    
    Co-authored-by: Oliver Stöhr <stoehr@effective-webwork.de>

--- Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
+++ Kitodo/src/main/webapp/WEB-INF/resources/js/ol_custom.js
@@ -250,10 +250,8 @@ class KitodoDetailMap {
      */
     hideCanvas() {
         let canvas = document.querySelector("#map canvas");
-        let loadingIcon = document.querySelector("#map > .fa-spinner");
         if (canvas) {
             canvas.style.opacity = 0;
-            loadingIcon.style.opacity = 1;
         }
     }
     
@@ -262,10 +260,8 @@ class KitodoDetailMap {
      */
     showCanvas() {
         let canvas = document.querySelector("#map canvas");
-        let loadingIcon = document.querySelector("#map > .fa-spinner");
         if (canvas) {
             canvas.style.opacity = 1;
-            loadingIcon.style.opacity = 0;
         }
     }
 
@@ -423,6 +419,7 @@ class KitodoDetailMap {
     onRenderComplete() {
         this.showCanvas();
         this.saveCurrentView();
+        $("#imagePreviewForm\\:loadingSymbol").hide();
     }
 
     /**
@@ -482,6 +479,7 @@ class KitodoDetailMap {
      */
     update() {
         this.registerImageLoadEvent();
+        $("#imagePreviewForm\\:loadingSymbol").show();
     }
 }
 
