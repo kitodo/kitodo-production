cd706f4bc871000d3cc8318fdcead02711749b30
Thomas Low <code@thomaslow.com>
2026-09-23 09:21:10 +0000

    Disable old user password hash methods MD5 and SHA-1. Make salted SHA-1 new default.

--- Kitodo-DataManagement/src/main/java/org/kitodo/data/database/enums/PasswordEncryption.java
+++ Kitodo-DataManagement/src/main/java/org/kitodo/data/database/enums/PasswordEncryption.java
@@ -16,27 +16,29 @@ package org.kitodo.data.database.enums;
  * database.
  */
 public enum PasswordEncryption {
-    SHA(0, "SHA-1"),
-    MD5(1, "MD5"),
+    SHA(0, "SHA-1", true),
+    MD5(1, "MD5", true),
 
     // Salted SHA-1, see https://man7.org/linux/man-pages/man5/slapd.conf.5.html
-    SSHA(2, "SHA-1 with Salt"),
+    SSHA(2, "SHA-1 with Salt", false),
 
     // Salted SHA-256 via Crypt, see https://man7.org/linux/man-pages/man3/crypt.3.html
-    CRYPT_5(3, "SHA-256 with Salt (Linux only, libcrypt required)"),
+    CRYPT_5(3, "SHA-256 with Salt (Linux only, libcrypt required)", false),
 
     // Salted SHA-512 via Crypt, see https://man7.org/linux/man-pages/man3/crypt.3.html
-    CRYPT_6(4, "SHA-512 with Salt (Linux only, libcrypt required)");
+    CRYPT_6(4, "SHA-512 with Salt (Linux only, libcrypt required)", false);
 
     private final int value;
     private final String title;
+    private final boolean deprecated;
 
     /**
      * Private constructor, initializes integer value.
      */
-    PasswordEncryption(int value, String title) {
+    PasswordEncryption(int value, String title, boolean deprecated) {
         this.value = value;
         this.title = title;
+        this.deprecated = deprecated;
     }
 
     /**
@@ -55,6 +57,15 @@ public enum PasswordEncryption {
      */
     public String getTitle() {
         return this.title;
+    }
+
+    /**
+     * Return true if this method is considered unsecure, and should not be used anymore.
+     * 
+     * @return true if deprecated
+     */
+    public boolean isDeprecated() {
+        return deprecated;
     }
 
     /**
